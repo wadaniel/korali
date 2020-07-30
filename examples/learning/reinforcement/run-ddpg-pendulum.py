43093afad70874e11bdf4a29899c591c37db0c20
Sergio Martin <martiser@ethz.ch>
2020-07-30 11:38:51 +0200

    Advancing DDPG

--- examples/learning/reinforcement/run-ddpg-pendulum.py
+++ examples/learning/reinforcement/run-ddpg-pendulum.py
@@ -6,15 +6,15 @@ import gym
 
 ######## Defining Environment Storage
 
-car = gym.make('MountainCarContinuous-v0').unwrapped
-maxSteps = 1000
+pendulum = gym.make('Pendulum-v0').unwrapped
+maxSteps = 100
 
 ####### Defining Problem's environment
 
 def env(s):
 
  # Initializing environment
- s["State"] = car.reset().tolist()
+ s["State"] = pendulum.reset().tolist()
  step = 0
  done = False
 
@@ -22,12 +22,12 @@ def env(s):
 
   # Getting new action
   s.update()
-
+  
   # Reading action
   action = s["Action"] 
     
   # Performing the action
-  state, reward, done, info = car.step(action)
+  state, reward, done, info = pendulum.step(action)
 
   # Storing Reward
   s["Reward"] = reward
@@ -47,17 +47,24 @@ e = korali.Experiment()
 e["Problem"]["Type"] = "Reinforcement Learning"
 e["Problem"]["Environment Function"] = env
 
-e["Variables"][0]["Name"] = "Car Position"
+e["Variables"][0]["Name"] = "X"
 e["Variables"][0]["Type"] = "State"
 
-e["Variables"][1]["Name"] = "Car Speed"
+e["Variables"][1]["Name"] = "Y"
 e["Variables"][1]["Type"] = "State"
 
-e["Variables"][2]["Name"] = "Force"
-e["Variables"][2]["Type"] = "Action"
-e["Variables"][2]["Exploration Noise"]["Type"] = "Univariate/Normal"
-e["Variables"][2]["Exploration Noise"]["Mean"] = 0.0
-e["Variables"][2]["Exploration Noise"]["Standard Deviation"] = 0.01
+e["Variables"][2]["Name"] = "Omega"
+e["Variables"][2]["Type"] = "State"
+
+e["Variables"][3]["Name"] = "Torque"
+e["Variables"][3]["Type"] = "Action"
+
+### Defining noise to add to the action
+
+e["Variables"][3]["Exploration Noise"]["Random Variable"]["Type"] = "Univariate/Normal"
+e["Variables"][3]["Exploration Noise"]["Random Variable"]["Mean"] = 0.0
+e["Variables"][3]["Exploration Noise"]["Random Variable"]["Standard Deviation"] = 0.25
+e["Variables"][3]["Exploration Noise"]["Theta"] = 0.15
 
 ### Configuring DQN hyperparameters
 
@@ -65,43 +72,41 @@ e["Solver"]["Type"] = "Agent/DDPG"
 
 ### Defining Mini-batch and DDPG configuration 
 
-e["Solver"]["Episodes Per Generation"] = 10
-e["Solver"]["Optimization Steps Per Generation"] = 10
-
-e["Solver"]["Agent History Size"] = 1000
-e["Solver"]["Mini Batch Size"] = 32
-e["Solver"]["Batch Normalization"]["Enabled"] = False
+e["Solver"]["Episodes Per Generation"] = 1
+e["Solver"]["Optimization Steps Per Generation"] = 1
+e["Solver"]["Agent History Size"] = maxSteps
+e["Solver"]["Mini Batch Size"] = 64
+e["Solver"]["Batch Normalization"]["Enabled"] = True
 e["Solver"]["Batch Normalization"]["Correction Steps"] = 32
-
 e["Solver"]["Discount Factor"] = 0.99
-e["Solver"]["Adoption Rate"] = 0.0001
+e["Solver"]["Adoption Rate"] = 0.001
 
 ### Defining the configuration of replay memory
- 
-e["Solver"]["Replay Memory"]["Start Size"] = 5000
-e["Solver"]["Replay Memory"]["Maximum Size"] = 50000
+
+e["Solver"]["Replay Memory"]["Start Size"] = 2000
+e["Solver"]["Replay Memory"]["Maximum Size"] = 500000
 e["Solver"]["Replay Memory"]["Replacement Policy"] = "Least Recently Added"
 
 ## Defining Actor and Critic optimizers
 
 e["Solver"]["Actor Optimizer"]["Type"] = "Optimizer/Adam"
-e["Solver"]["Actor Optimizer"]["Eta"] = 0.05
+e["Solver"]["Actor Optimizer"]["Eta"] = 0.0001
 
 e["Solver"]["Critic Optimizer"]["Type"] = "Optimizer/Adam"
-e["Solver"]["Critic Optimizer"]["Eta"] = 0.05
+e["Solver"]["Critic Optimizer"]["Eta"] = 0.001
 
 ### Defining the shape of the critic neural network
 
 e["Solver"]["Critic Neural Network"]["Layers"][0]["Type"] = "Input"
-e["Solver"]["Critic Neural Network"]["Layers"][0]["Node Count"] = 3
+e["Solver"]["Critic Neural Network"]["Layers"][0]["Node Count"] = 4
 e["Solver"]["Critic Neural Network"]["Layers"][0]["Activation Function"]["Type"] = "Identity"
 
 e["Solver"]["Critic Neural Network"]["Layers"][1]["Type"] = "Dense"
-e["Solver"]["Critic Neural Network"]["Layers"][1]["Node Count"] = 32
+e["Solver"]["Critic Neural Network"]["Layers"][1]["Node Count"] = 64
 e["Solver"]["Critic Neural Network"]["Layers"][1]["Activation Function"]["Type"] = "Tanh"
 
 e["Solver"]["Critic Neural Network"]["Layers"][2]["Type"] = "Dense"
-e["Solver"]["Critic Neural Network"]["Layers"][2]["Node Count"] = 32
+e["Solver"]["Critic Neural Network"]["Layers"][2]["Node Count"] = 64
 e["Solver"]["Critic Neural Network"]["Layers"][2]["Activation Function"]["Type"] = "Tanh"
 
 e["Solver"]["Critic Neural Network"]["Layers"][3]["Type"] = "Output"
@@ -111,42 +116,44 @@ e["Solver"]["Critic Neural Network"]["Layers"][3]["Activation Function"]["Type"]
 ### Defining the shape of the actor neural network
 
 e["Solver"]["Actor Neural Network"]["Layers"][0]["Type"] = "Input"
-e["Solver"]["Actor Neural Network"]["Layers"][0]["Node Count"] = 2
+e["Solver"]["Actor Neural Network"]["Layers"][0]["Node Count"] = 1
 e["Solver"]["Actor Neural Network"]["Layers"][0]["Activation Function"]["Type"] = "Identity"
 
 e["Solver"]["Actor Neural Network"]["Layers"][1]["Type"] = "Dense"
-e["Solver"]["Actor Neural Network"]["Layers"][1]["Node Count"] = 32
+e["Solver"]["Actor Neural Network"]["Layers"][1]["Node Count"] = 64
 e["Solver"]["Actor Neural Network"]["Layers"][1]["Activation Function"]["Type"] = "Tanh"
 
 e["Solver"]["Actor Neural Network"]["Layers"][2]["Type"] = "Dense"
-e["Solver"]["Actor Neural Network"]["Layers"][2]["Node Count"] = 32
+e["Solver"]["Actor Neural Network"]["Layers"][2]["Node Count"] = 64
 e["Solver"]["Actor Neural Network"]["Layers"][2]["Activation Function"]["Type"] = "Tanh"
 
 e["Solver"]["Actor Neural Network"]["Layers"][3]["Type"] = "Output"
 e["Solver"]["Actor Neural Network"]["Layers"][3]["Node Count"] = 1
-e["Solver"]["Actor Neural Network"]["Layers"][3]["Activation Function"]["Type"] = "Logistic" 
+e["Solver"]["Actor Neural Network"]["Layers"][3]["Activation Function"]["Type"] = "Identity" 
 
 ### Defining Termination Criteria
 
-e["Solver"]["Termination Criteria"]["Target Average Reward"] = 900
+e["Solver"]["Termination Criteria"]["Target Average Reward"] = -400
 
 ### Setting file output configuration
 
-e["File Output"]["Frequency"] = 1
+e["File Output"]["Frequency"] = 0
 
 ### Running Experiment
 
+k["Conduit"]["Type"] = "Concurrent"
+k["Conduit"]["Concurrent Jobs"] = 4
 k.run(e)
 
-###### Now running the car experiment with Korali's help
+###### Now running the pendulum experiment with Korali's help
 
-state = car.reset().tolist()
+state = pendulum.reset().tolist()
 step = 0
 done = False
 
 while not done and step < maxSteps:
- action = int(e.getAction(state)[0])
+ action = e.getAction(state)
  print('Step ' + str(step) + ' - State: ' + str(state) + ' - Action: ' + str(action), end = '')
- state, reward, done, info = car.step(action)
+ state, reward, done, info = pendulum.step(action)
  print('- Reward: ' + str(reward))
  step = step + 1
--- examples/learning/reinforcement/run-dqn-pendulum.py
+++ examples/learning/reinforcement/run-dqn-pendulum.py
@@ -6,15 +6,15 @@ import gym
 
 ######## Defining Environment Storage
 
-cart = gym.make('CartPole-v1').unwrapped
-maxSteps = 3000
+pendulum = gym.make('Pendulum-v0').unwrapped
+maxSteps = 200
 
 ####### Defining Problem's environment
 
 def env(s):
 
  # Initializing environment
- s["State"] = cart.reset().tolist()
+ s["State"] = pendulum.reset().tolist()
  step = 0
  done = False
 
@@ -24,10 +24,10 @@ def env(s):
   s.update()
   
   # Reading action
-  action = s["Action"][0] 
+  action = s["Action"] 
     
   # Performing the action
-  state, reward, done, info = cart.step(action)
+  state, reward, done, info = pendulum.step(action)
 
   # Storing Reward
   s["Reward"] = reward
@@ -47,21 +47,18 @@ e = korali.Experiment()
 e["Problem"]["Type"] = "Reinforcement Learning"
 e["Problem"]["Environment Function"] = env
 
-e["Variables"][0]["Name"] = "Cart Position"
+e["Variables"][0]["Name"] = "X"
 e["Variables"][0]["Type"] = "State"
 
-e["Variables"][1]["Name"] = "Cart Velocity"
+e["Variables"][1]["Name"] = "Y"
 e["Variables"][1]["Type"] = "State"
 
-e["Variables"][2]["Name"] = "Pole Angle"
+e["Variables"][2]["Name"] = "Omega"
 e["Variables"][2]["Type"] = "State"
 
-e["Variables"][3]["Name"] = "Pole Angular Velocity"
-e["Variables"][3]["Type"] = "State"
-
-e["Variables"][4]["Name"] = "Push Direction"
-e["Variables"][4]["Type"] = "Action"
-e["Variables"][4]["Values"] = [ 0.0, 1.0 ]
+e["Variables"][3]["Name"] = "Torque"
+e["Variables"][3]["Type"] = "Action"
+e["Variables"][3]["Values"] = [ -2.0, -1.5, -1.0, -0.5, 0.0, 0.5, 1.0, 1.5, 2.0 ]
 
 ### Configuring DQN hyperparameters
 
@@ -69,10 +66,10 @@ e["Solver"]["Type"] = "Agent/DQN"
 
 ### Defining Mini-batch and Q-Training configuration
  
-e["Solver"]["Episodes Per Generation"] = 10
-e["Solver"]["Optimization Steps Per Generation"] = 10
-e["Solver"]["Agent History Size"] = 1000
-e["Solver"]["Mini Batch Size"] = 32
+e["Solver"]["Episodes Per Generation"] = 5
+e["Solver"]["Optimization Steps Per Generation"] = 5
+e["Solver"]["Agent History Size"] = maxSteps
+e["Solver"]["Mini Batch Size"] = 64
 e["Solver"]["Batch Normalization"]["Enabled"] = True
 e["Solver"]["Batch Normalization"]["Correction Steps"] = 32
 e["Solver"]["Discount Factor"] = 0.99
@@ -80,7 +77,7 @@ e["Solver"]["Discount Factor"] = 0.99
 ### Defining the configuration of replay memory
  
 e["Solver"]["Replay Memory"]["Start Size"] = 5000
-e["Solver"]["Replay Memory"]["Maximum Size"] = 150000
+e["Solver"]["Replay Memory"]["Maximum Size"] = 100000
 e["Solver"]["Replay Memory"]["Replacement Policy"] = "Least Recently Added"
 
 ### Defining Epsilon (the probability of taking a random action) configuration
@@ -91,31 +88,27 @@ e["Solver"]["Epsilon"]["Decrease Rate"] = 0.05
 
 ## Defining Q-Weight and Action-selection optimizers
 
-e["Solver"]["Action Optimizer"]["Type"] = "Optimizer/Grid Search" 
+e["Solver"]["Action Optimizer"]["Type"] = "Optimizer/Grid Search"
 e["Solver"]["Weight Optimizer"]["Type"] = "Optimizer/Adam"
-e["Solver"]["Weight Optimizer"]["Eta"] = 0.1
+e["Solver"]["Weight Optimizer"]["Eta"] = 0.05
 
 ### Defining the shape of the neural network
 
 e["Solver"]["Neural Network"]["Layers"][0]["Type"] = "Input"
-e["Solver"]["Neural Network"]["Layers"][0]["Node Count"] = 5
+e["Solver"]["Neural Network"]["Layers"][0]["Node Count"] = 4
 e["Solver"]["Neural Network"]["Layers"][0]["Activation Function"]["Type"] = "Identity"
 
 e["Solver"]["Neural Network"]["Layers"][1]["Type"] = "Dense"
 e["Solver"]["Neural Network"]["Layers"][1]["Node Count"] = 32
 e["Solver"]["Neural Network"]["Layers"][1]["Activation Function"]["Type"] = "Tanh"
 
-e["Solver"]["Neural Network"]["Layers"][2]["Type"] = "Dense"
-e["Solver"]["Neural Network"]["Layers"][2]["Node Count"] = 32
-e["Solver"]["Neural Network"]["Layers"][2]["Activation Function"]["Type"] = "Tanh"
-
-e["Solver"]["Neural Network"]["Layers"][3]["Type"] = "Output"
-e["Solver"]["Neural Network"]["Layers"][3]["Node Count"] = 1
-e["Solver"]["Neural Network"]["Layers"][3]["Activation Function"]["Type"] = "Identity" 
+e["Solver"]["Neural Network"]["Layers"][2]["Type"] = "Output"
+e["Solver"]["Neural Network"]["Layers"][2]["Node Count"] = 1
+e["Solver"]["Neural Network"]["Layers"][2]["Activation Function"]["Type"] = "Identity" 
 
 ### Defining Termination Criteria
 
-e["Solver"]["Termination Criteria"]["Target Average Reward"] = 0.9*maxSteps
+e["Solver"]["Termination Criteria"]["Target Average Reward"] = -400
 
 ### Setting file output configuration
 
@@ -123,19 +116,17 @@ e["File Output"]["Frequency"] = 1
 
 ### Running Experiment
 
-#k["Conduit"]["Type"] = "Concurrent"
-#k["Conduit"]["Concurrent Jobs"] = 5
 k.run(e)
 
-###### Now running the cartpole experiment with Korali's help
+###### Now running the pendulumpole experiment with Korali's help
 
-state = cart.reset().tolist()
+state = pendulum.reset().tolist()
 step = 0
 done = False
 
 while not done and step < maxSteps:
- action = int(e.getAction(state)[0])
+ action = e.getAction(state)
  print('Step ' + str(step) + ' - State: ' + str(state) + ' - Action: ' + str(action), end = '')
- state, reward, done, info = cart.step(action)
+ state, reward, done, info = pendulum.step(action)
  print('- Reward: ' + str(reward))
  step = step + 1

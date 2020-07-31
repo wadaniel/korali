4a9281088771c558913580c28e88f66dde733f61
Sergio Martin <martiser@ethz.ch>
2020-07-31 15:27:44 +0200

    Simplified initialization for NNs, fixing the problem with openMP+Concurrent Conduit with big NNs

--- examples/learning/supervised/run-sin.py
+++ examples/learning/supervised/run-sin.py
@@ -57,8 +57,6 @@ e["Random Seed"] = 0xC0FFEE
 ### Training the neural network
 
 e["Solver"]["Termination Criteria"]["Max Generations"] = 10
-k["Conduit"]["Type"] = "Concurrent"
-k["Conduit"]["Concurrent Jobs"] = 1
 k.run(e)
 
 ### Obtaining inferred results from the NN and comparing them to the actual solution

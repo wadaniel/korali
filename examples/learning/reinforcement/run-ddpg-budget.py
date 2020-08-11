dc90a067fd176b0f30d80468f4e0d7ddd29dce7c
Sergio Martin <martiser@ethz.ch>
2020-08-11 08:01:20 +0200

    DDPG working with multiple variables and changing state

--- examples/learning/reinforcement/run-ddpg-budget.py
+++ examples/learning/reinforcement/run-ddpg-budget.py
@@ -6,18 +6,22 @@ import gym
 
 ######## Defining Environment Storage
 
-maxSteps = 10
+maxSteps = 5
+initialBudget = 25
 
 ####### Defining Problem's environment
 
 def env(s):
 
  # Initializing environment
- s["State"] = [ 3.0 ]
+ budget = initialBudget
  step = 0
 
  while step < maxSteps:
 
+  # Storing current budget as state
+  s["State"] = [ budget ]
+  
   # Getting new action
   s.update()
   
@@ -27,9 +31,18 @@ def env(s):
   print('Python Action: ' + str(action0) + ' ' + str(action1) ) 
   
   # Reward = action, if not bigger than state
-  val0 = action0 - s["State"][0]
-  val1 = - action1 - s["State"][0]  
+  val0 = action0 - 5.0
+  val1 = action1 - 2.5
   reward = -val0*val0 -val1*val1
+  
+  # Calculating remaining budget
+  spenditure = val0 + val1
+  budget = budget - spenditure
+  
+  # If budget is negative (overdraft), set to zero and punish
+  if (budget < 0): 
+    reward = reward - abs(budget)
+    budget = 0
     
   # Storing Reward
   s["Reward"] = reward
@@ -49,12 +62,12 @@ e["Problem"]["Environment Function"] = env
 e["Variables"][0]["Name"] = "Budget"
 e["Variables"][0]["Type"] = "State"
 
-e["Variables"][1]["Name"] = "Spenditure 0"
+e["Variables"][1]["Name"] = "Spenditure"
 e["Variables"][1]["Type"] = "Action"
 e["Variables"][1]["Lower Bound"] = -6.0
 e["Variables"][1]["Upper Bound"] = +6.0
 
-e["Variables"][2]["Name"] = "Spenditure 1"
+e["Variables"][2]["Name"] = "Spenditure"
 e["Variables"][2]["Type"] = "Action"
 e["Variables"][2]["Lower Bound"] = -6.0
 e["Variables"][2]["Upper Bound"] = +6.0
@@ -67,15 +80,11 @@ e["Variables"][1]["Exploration Noise"]["Distribution"]["Mean"] = 0.0
 e["Variables"][1]["Exploration Noise"]["Distribution"]["Standard Deviation"] = 0.1
 e["Variables"][1]["Exploration Noise"]["Theta"] = 0.05
 
-### Defining noise to add to the action
-
 e["Variables"][2]["Exploration Noise"]["Enabled"] = True
 e["Variables"][2]["Exploration Noise"]["Distribution"]["Type"] = "Univariate/Normal"
 e["Variables"][2]["Exploration Noise"]["Distribution"]["Mean"] = 0.0
 e["Variables"][2]["Exploration Noise"]["Distribution"]["Standard Deviation"] = 0.1
 e["Variables"][2]["Exploration Noise"]["Theta"] = 0.05
-
-### Configuring DQN hyperparameters
 
 e["Solver"]["Type"] = "Agent/DDPG"
 
@@ -90,7 +99,7 @@ e["Solver"]["Adoption Rate"] = 0.001
 
 ### Defining the configuration of replay memory
 
-e["Solver"]["Replay Memory"]["Start Size"] = 5000
+e["Solver"]["Replay Memory"]["Start Size"] = 32
 e["Solver"]["Replay Memory"]["Maximum Size"] = 100000
 e["Solver"]["Replay Memory"]["Replacement Policy"] = "Least Recently Added"
 
@@ -98,7 +107,7 @@ e["Solver"]["Replay Memory"]["Replacement Policy"] = "Least Recently Added"
 
 e["Solver"]["Actor Optimizer"]["Type"] = "Optimizer/Adam"
 e["Solver"]["Actor Optimizer"]["Termination Criteria"]["Min Gradient Norm"] = -1.0
-e["Solver"]["Actor Optimizer"]["Eta"] = 0.0001
+e["Solver"]["Actor Optimizer"]["Eta"] = 0.000001
 
 e["Solver"]["Critic Optimizer"]["Type"] = "Optimizer/Adam"
 e["Solver"]["Critic Optimizer"]["Eta"] = 0.005
@@ -106,19 +115,19 @@ e["Solver"]["Critic Optimizer"]["Eta"] = 0.005
 ### Defining the shape of the critic neural network
 
 e["Solver"]["Critic Neural Network"]["Layers"][0]["Type"] = "Layer/Dense"
-e["Solver"]["Critic Neural Network"]["Layers"][0]["Node Count"] = 2
+e["Solver"]["Critic Neural Network"]["Layers"][0]["Node Count"] = 3
 e["Solver"]["Critic Neural Network"]["Layers"][0]["Activation Function"]["Type"] = "Linear"
-e["Solver"]["Critic Neural Network"]["Layers"][0]["Batch Normalization"]["Enabled"] = True
+e["Solver"]["Critic Neural Network"]["Layers"][0]["Batch Normalization"]["Enabled"] = False
 
 e["Solver"]["Critic Neural Network"]["Layers"][1]["Type"] = "Layer/Dense"
 e["Solver"]["Critic Neural Network"]["Layers"][1]["Node Count"] = 32
 e["Solver"]["Critic Neural Network"]["Layers"][1]["Activation Function"]["Type"] = "Tanh"
-e["Solver"]["Critic Neural Network"]["Layers"][1]["Batch Normalization"]["Enabled"] = True
+e["Solver"]["Critic Neural Network"]["Layers"][1]["Batch Normalization"]["Enabled"] = False
 
 e["Solver"]["Critic Neural Network"]["Layers"][2]["Type"] = "Layer/Dense"
 e["Solver"]["Critic Neural Network"]["Layers"][2]["Node Count"] = 32
 e["Solver"]["Critic Neural Network"]["Layers"][2]["Activation Function"]["Type"] = "Tanh"
-e["Solver"]["Critic Neural Network"]["Layers"][2]["Batch Normalization"]["Enabled"] = True
+e["Solver"]["Critic Neural Network"]["Layers"][2]["Batch Normalization"]["Enabled"] = False
 
 e["Solver"]["Critic Neural Network"]["Layers"][3]["Type"] = "Layer/Dense"
 e["Solver"]["Critic Neural Network"]["Layers"][3]["Node Count"] = 1
@@ -132,18 +141,25 @@ e["Solver"]["Batch Normalization"]["Correction Steps"] = 32
 e["Solver"]["Actor Neural Network"]["Layers"][0]["Type"] = "Layer/Dense"
 e["Solver"]["Actor Neural Network"]["Layers"][0]["Node Count"] = 1
 e["Solver"]["Actor Neural Network"]["Layers"][0]["Activation Function"]["Type"] = "Linear"
-e["Solver"]["Actor Neural Network"]["Layers"][0]["Batch Normalization"]["Enabled"] = True
+e["Solver"]["Actor Neural Network"]["Layers"][0]["Batch Normalization"]["Enabled"] = False
 
 e["Solver"]["Actor Neural Network"]["Layers"][1]["Type"] = "Layer/Dense"
 e["Solver"]["Actor Neural Network"]["Layers"][1]["Node Count"] = 32
-e["Solver"]["Actor Neural Network"]["Layers"][1]["Activation Function"]["Type"] = "Tanh"
-e["Solver"]["Actor Neural Network"]["Layers"][1]["Batch Normalization"]["Enabled"] = True
+e["Solver"]["Actor Neural Network"]["Layers"][1]["Activation Function"]["Type"] = "ReLU"
+e["Solver"]["Actor Neural Network"]["Layers"][1]["Activation Function"]["Alpha"] = 0.0
+e["Solver"]["Actor Neural Network"]["Layers"][1]["Batch Normalization"]["Enabled"] = False
 
 e["Solver"]["Actor Neural Network"]["Layers"][2]["Type"] = "Layer/Dense"
-e["Solver"]["Actor Neural Network"]["Layers"][2]["Node Count"] = 1
-e["Solver"]["Actor Neural Network"]["Layers"][2]["Activation Function"]["Type"] = "Logistic" 
+e["Solver"]["Actor Neural Network"]["Layers"][2]["Node Count"] = 32
+e["Solver"]["Actor Neural Network"]["Layers"][2]["Activation Function"]["Type"] = "ReLU"
+e["Solver"]["Actor Neural Network"]["Layers"][2]["Activation Function"]["Alpha"] = 0.0
 e["Solver"]["Actor Neural Network"]["Layers"][2]["Batch Normalization"]["Enabled"] = False
-e["Solver"]["Actor Neural Network"]["Layers"][2]["Weight Initialization Scaling"] = 0.0000001
+
+e["Solver"]["Actor Neural Network"]["Layers"][3]["Type"] = "Layer/Dense"
+e["Solver"]["Actor Neural Network"]["Layers"][3]["Node Count"] = 2
+e["Solver"]["Actor Neural Network"]["Layers"][3]["Activation Function"]["Type"] = "Logistic" 
+e["Solver"]["Actor Neural Network"]["Layers"][3]["Batch Normalization"]["Enabled"] = False
+e["Solver"]["Actor Neural Network"]["Layers"][3]["Weight Initialization Scaling"] = 0.0000001
 
 e["Solver"]["Actor Neural Network"]["Output Scaling"] = [ 10.0, 10.0 ]
 
@@ -153,7 +169,8 @@ e["Solver"]["Termination Criteria"]["Target Average Reward"] = 0
 
 ### Setting file output configuration
 
-e["File Output"]["Frequency"] = 1000
+e["File Output"]["Frequency"] = 10000
+#e["Console Output"]["Verbosity"] = "Silent"
 
 ### Running Experiment
 

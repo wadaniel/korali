d59950a92923931b319ff4c3a89d2b8faa36cc27
Susi <kellersu@student.ethz.ch>
2020-08-19 15:16:20 +0200

    remove features-9 from tests

--- features/9.latent.variables.cxx/.run_test.sh
+++ features/9.latent.variables.cxx/.run_test.sh
@@ -2,20 +2,20 @@
 
 ###### Auxiliar Functions and Variables #########
 
-source ../../tests/functions.sh
+#source ../../tests/functions.sh
 
 ##### Deleting Previous Results
 
-echo "  + Deleting previous results..." 
-rm -rf _korali_result*; check_result
+#echo "  + Deleting previous results..."
+#rm -rf _korali_result*; check_result
 
 ##### Recompiling C++
 
-make clean; check_result
-make -j4; check_result
+#make clean; check_result
+#make -j4; check_result
 
 ##### Running Tests
 
-./run-saem; check_result
-./run-saem-gaussian-mixture; check_result
+#./run-saem; check_result
+#./run-saem-gaussian-mixture; check_result
 

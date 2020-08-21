947144861d65e924e039c91e9b995b4a1c5a82ea
wadaniel <wadaniel@ethz.ch>
2020-08-21 16:29:47 +0200

    lmcma cleanup

--- examples/optimization/stochastic/run-dea.py
+++ examples/optimization/stochastic/run-dea.py
@@ -18,7 +18,7 @@ e = korali.Experiment()
 
 # Configuring Problem
 e["Problem"]["Type"] = "Optimization"
-e["Problem"]["Objective Function"] = model
+e["Problem"]["Objective Function"] = negative_rosenbrock
 
 # Configuring Solver
 e["Solver"]["Type"] = "Optimizer/DEA"
@@ -26,13 +26,18 @@ e["Solver"]["Population Size"] = 32
 e["Solver"]["Termination Criteria"]["Min Value Difference Threshold"] = 1e-12
 e["Solver"]["Termination Criteria"]["Max Generations"] = 100
 
+dim = 3
+
 # Defining the problem's variables and their bounds.
-e["Variables"][0]["Name"] = "X"
-e["Variables"][0]["Lower Bound"] = -10.0
-e["Variables"][0]["Upper Bound"] = +10.0
+for i in range(dim):
+    e["Variables"][i]["Name"] = "X" + str(i)
+    e["Variables"][i]["Lower Bound"] = -25.0
+    e["Variables"][i]["Upper Bound"] = +25.0
+
 
 # Configuring results path
 e["File Output"]["Path"] = '_korali_result_dea'
+e["File Output"]["Frequency"] = 1
 
 # Running Korali
 k.run(e)

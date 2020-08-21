36a9fdfbe318ccbc64fa3d471ac02d54c20c2ac2
Susi <kellersu@student.ethz.ch>
2020-08-21 17:47:34 +0200

    tiny change to example

--- examples/hierarchical.bayesian/12.hierarchical.latent.variables/estimate-FIM-normal.py
+++ examples/hierarchical.bayesian/12.hierarchical.latent.variables/estimate-FIM-normal.py
@@ -79,7 +79,7 @@ def main():
 
   # Configure how results will be stored to a file:
   e["File Output"]["Frequency"] = 1
-  e["File Output"]["Path"] = "_korali_result_FIM/"
+  e["File Output"]["Path"] = "_korali_result_FIM_normal/"
   # We choose a non-default output directory -
   # for plotting results, we can later set the directory with:
   #   python3 -m korali.plotter --dir _korali_result_logistic/

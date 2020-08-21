816512c2cf250dab61a306118f729a8b1fd519e1
wadaniel <wadaniel@ethz.ch>
2020-08-21 03:29:30 +0200

    lmcma wip

--- examples/optimization/stochastic/_model/model.py
+++ examples/optimization/stochastic/_model/model.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 
-
 # Single function evaluation
 def model(p):
   x = p["Parameters"][0]
   p["F(x)"] = -0.5 * x * x
 
+def negative_sphere(p):
+    x = p["Parameters"]
+    dim = len(x)
+    res = 0.
+    for i in range(dim):
+        res += x[i]**2
+
+    p["F(x)"] = -res
 
 def negative_rosenbrock(p):
     x = p["Parameters"]

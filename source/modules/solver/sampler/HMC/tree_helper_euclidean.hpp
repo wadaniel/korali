8900a55cd4654e0572556e44f84dbd17a009d161
Isidor Schoch <isschoch@ethz.ch>
2020-08-22 16:13:26 +0200

    Now using abstract helper object for dynamic build criterion

---
+++ source/modules/solver/sampler/HMC/tree_helper_euclidean.hpp
@@ -0,0 +1,32 @@
+#ifndef TREE_HELPER_EUCLIDEAN
+#define TREE_HELPER_EUCLIDEAN
+
+#include "tree_helper_base.hpp"
+namespace korali
+{
+namespace solver
+{
+namespace sampler
+{
+/**
+* \struct TreeHelperEuclidean
+* @brief Euclidean child of helper class for long argument list of buildTree
+*/
+struct TreeHelperEuclidean : public TreeHelper
+{
+  /**
+    * @brief Computes No U-Turn Sampling (NUTS) criterion
+    * @param hamiltonian Hamiltonian object of system
+    * @return Returns of tree should be built further.
+    */
+  bool computeCriterion(Hamiltonian *hamiltonian) override
+  {
+    return hamiltonian->computeStandardCriterion(qLeftOut, pLeftOut, qRightOut, pRightOut);
+  }
+};
+
+} // namespace sampler
+} // namespace solver
+} // namespace korali
+
+#endif

9cf95513071792a612037fd9bb7298df514cf345
wadaniel <wadaniel@ethz.ch>
2020-09-08 13:09:03 +0200

    style

--- examples/bayesian.inference/reference/run-hmc-euclidean.py
+++ examples/bayesian.inference/reference/run-hmc-euclidean.py
@@ -32,34 +32,34 @@ e["Solver"]["Hamiltonian Verbosity"] = False
 e["Distributions"][0]["Name"] = "Uniform 0"
 e["Distributions"][0]["Type"] = "Univariate/Uniform"
 e["Distributions"][0]["Minimum"] = 0.0
-e["Distributions"][0]["Maximum"] = +50.0
+e["Distributions"][0]["Maximum"] = +5.0
 
 e["Distributions"][1]["Name"] = "Uniform 1"
 e["Distributions"][1]["Type"] = "Univariate/Uniform"
 e["Distributions"][1]["Minimum"] = 0.0
-e["Distributions"][1]["Maximum"] = +50.0
+e["Distributions"][1]["Maximum"] = +5.0
 
 e["Distributions"][2]["Name"] = "Uniform 2"
 e["Distributions"][2]["Type"] = "Univariate/Uniform"
 e["Distributions"][2]["Minimum"] = 0.0
-e["Distributions"][2]["Maximum"] = +50.0
+e["Distributions"][2]["Maximum"] = +5.0
 
 # Configuring the problem's variables and their prior distributions
 e["Variables"][0]["Name"] = "a"
 e["Variables"][0]["Prior Distribution"] = "Uniform 0"
-e["Variables"][0]["Initial Mean"] = 25.0
+e["Variables"][0]["Initial Mean"] = 2.50
 e["Variables"][0]["Initial Standard Deviation"] = 1.0
 
 
 e["Variables"][1]["Name"] = "b"
 e["Variables"][1]["Prior Distribution"] = "Uniform 1"
-e["Variables"][1]["Initial Mean"] = 25.0
+e["Variables"][1]["Initial Mean"] = 2.50
 e["Variables"][1]["Initial Standard Deviation"] = 1.0
 
 
 e["Variables"][2]["Name"] = "[Sigma]"
 e["Variables"][2]["Prior Distribution"] = "Uniform 2"
-e["Variables"][2]["Initial Mean"] = 25.0
+e["Variables"][2]["Initial Mean"] = 2.50
 e["Variables"][2]["Initial Standard Deviation"] = 1.0
 
 

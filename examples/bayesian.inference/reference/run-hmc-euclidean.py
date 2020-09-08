9a617a69112bc70f731ee62b6b2842bbe005b922
wadaniel <wadaniel@ethz.ch>
2020-09-08 13:02:41 +0200

    fix

--- examples/bayesian.inference/reference/run-hmc-euclidean.py
+++ examples/bayesian.inference/reference/run-hmc-euclidean.py
@@ -23,7 +23,8 @@ e["Problem"]["Computational Model"] = lambda sampleData: modelWithGradients(
 # Configuring HMC parameters
 e["Solver"]["Type"] = "Sampler/HMC"
 e["Solver"]["Burn In"] = 500
-e["Solver"]["Termination Criteria"]["Max Samples"] = 50000
+e["Solver"]["Termination Criteria"]["Max Samples"] = 25000
+e["Solver"]["Use NUTS"] = True
 e["Solver"]["Integrator Verbosity"] = False
 e["Solver"]["Hamiltonian Verbosity"] = False
 
@@ -31,34 +32,34 @@ e["Solver"]["Hamiltonian Verbosity"] = False
 e["Distributions"][0]["Name"] = "Uniform 0"
 e["Distributions"][0]["Type"] = "Univariate/Uniform"
 e["Distributions"][0]["Minimum"] = 0.0
-e["Distributions"][0]["Maximum"] = +5.0
+e["Distributions"][0]["Maximum"] = +50.0
 
 e["Distributions"][1]["Name"] = "Uniform 1"
 e["Distributions"][1]["Type"] = "Univariate/Uniform"
 e["Distributions"][1]["Minimum"] = 0.0
-e["Distributions"][1]["Maximum"] = +5.0
+e["Distributions"][1]["Maximum"] = +50.0
 
 e["Distributions"][2]["Name"] = "Uniform 2"
 e["Distributions"][2]["Type"] = "Univariate/Uniform"
 e["Distributions"][2]["Minimum"] = 0.0
-e["Distributions"][2]["Maximum"] = +5.0
+e["Distributions"][2]["Maximum"] = +50.0
 
 # Configuring the problem's variables and their prior distributions
 e["Variables"][0]["Name"] = "a"
 e["Variables"][0]["Prior Distribution"] = "Uniform 0"
-e["Variables"][0]["Initial Mean"] = 3.0
+e["Variables"][0]["Initial Mean"] = 25.0
 e["Variables"][0]["Initial Standard Deviation"] = 1.0
 
 
 e["Variables"][1]["Name"] = "b"
 e["Variables"][1]["Prior Distribution"] = "Uniform 1"
-e["Variables"][1]["Initial Mean"] = 3.0
+e["Variables"][1]["Initial Mean"] = 25.0
 e["Variables"][1]["Initial Standard Deviation"] = 1.0
 
 
 e["Variables"][2]["Name"] = "[Sigma]"
 e["Variables"][2]["Prior Distribution"] = "Uniform 2"
-e["Variables"][2]["Initial Mean"] = 3.0
+e["Variables"][2]["Initial Mean"] = 25.0
 e["Variables"][2]["Initial Standard Deviation"] = 1.0
 
 
--- examples/bayesian.inference/reference/run-hmc-riemannian.py
+++ examples/bayesian.inference/reference/run-hmc-riemannian.py
@@ -22,8 +22,8 @@ e["Problem"]["Computational Model"] = lambda sampleData: modelWithGradientsAndHe
 
 # Configuring HMC parameters
 e["Solver"]["Type"] = "Sampler/HMC"
-e["Solver"]["Burn In"] = 10
-e["Solver"]["Termination Criteria"]["Max Samples"] = 1000
+e["Solver"]["Burn In"] = 500
+e["Solver"]["Termination Criteria"]["Max Samples"] = 25000
 e["Solver"]["Version"] = "Riemannian"
 e["Solver"]["Use NUTS"] = True
 e["Solver"]["Inverse Regularization Parameter"] = 1
@@ -34,34 +34,34 @@ e["Solver"]["Hamiltonian Verbosity"] = False
 e["Distributions"][0]["Name"] = "Uniform 0"
 e["Distributions"][0]["Type"] = "Univariate/Uniform"
 e["Distributions"][0]["Minimum"] = 0.0
-e["Distributions"][0]["Maximum"] = 5.0
+e["Distributions"][0]["Maximum"] = 50.0
 
 e["Distributions"][1]["Name"] = "Uniform 1"
 e["Distributions"][1]["Type"] = "Univariate/Uniform"
 e["Distributions"][1]["Minimum"] = 0.0
-e["Distributions"][1]["Maximum"] = 5.0
+e["Distributions"][1]["Maximum"] = 50.0
 
 e["Distributions"][2]["Name"] = "Uniform 2"
 e["Distributions"][2]["Type"] = "Univariate/Uniform"
 e["Distributions"][2]["Minimum"] = 0.0
-e["Distributions"][2]["Maximum"] = 5.0
+e["Distributions"][2]["Maximum"] = 50.0
 
 # Configuring the problem's variables and their prior distributions
 e["Variables"][0]["Name"] = "a"
 e["Variables"][0]["Prior Distribution"] = "Uniform 0"
-e["Variables"][0]["Initial Mean"] = 3.0
+e["Variables"][0]["Initial Mean"] = 25.0
 e["Variables"][0]["Initial Standard Deviation"] = 1.0
 
 
 e["Variables"][1]["Name"] = "b"
 e["Variables"][1]["Prior Distribution"] = "Uniform 1"
-e["Variables"][1]["Initial Mean"] = 3.0
+e["Variables"][1]["Initial Mean"] = 25.0
 e["Variables"][1]["Initial Standard Deviation"] = 1.0
 
 
 e["Variables"][2]["Name"] = "[Sigma]"
 e["Variables"][2]["Prior Distribution"] = "Uniform 2"
-e["Variables"][2]["Initial Mean"] = 3.0
+e["Variables"][2]["Initial Mean"] = 25.0
 e["Variables"][2]["Initial Standard Deviation"] = 1.0
 
 

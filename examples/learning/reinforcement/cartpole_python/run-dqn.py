2aa6bfecb09b0b4d92a7e2581c8158b40b97f9a0
Sergio Martin <martiser@ethz.ch>
2020-10-01 10:33:22 +0200

    Adding c++ versions of cartpole

--- examples/learning/reinforcement/cartpole_python/run-dqn.py
+++ examples/learning/reinforcement/cartpole_python/run-dqn.py
@@ -2,37 +2,7 @@
 import os
 import sys
 sys.path.append('./_model')
-from cartpole import *
-
-######## Defining Environment Storage
-
-cart = CartPole()
-maxSteps = 500
-
-def env(s):
-
- # Initializing environment
- cart.reset()
- s["State"] = cart.getState().tolist()
- step = 0
- done = False
-
- while not done and step < maxSteps:
-
-  # Getting new action
-  s.update()
-  
-  # Performing the action
-  done = cart.advance(s["Action"])
-  
-  # Getting Reward
-  s["Reward"] = cart.getReward()
-   
-  # Storing New State
-  s["State"] = cart.getState().tolist()
-  
-  # Advancing step counter
-  step = step + 1
+from env import *
 
 ####### Defining Korali Problem
 
@@ -126,16 +96,3 @@ e["File Output"]["Frequency"] = 100
 ### Running Experiment
 
 k.run(e)
-
-###### Now running the cartpole experiment with Korali's help
-
-state = cart.reset().tolist()
-step = 0
-done = False
-
-while not done and step < maxSteps:
- action = int(e.getAction(state)[0])
- print('Step ' + str(step) + ' - State: ' + str(state) + ' - Action: ' + str(action), end = '')
- state, reward, done, info = cart.step(action)
- print('- Reward: ' + str(reward))
- step = step + 1

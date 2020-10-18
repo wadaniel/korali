9dcadbff11c9758d4ae869182d99c92f4611ebf9
wadaniel <wadaniel@ethz.ch>
2020-10-18 11:21:06 +0200

    added double pendulum

--- examples/learning/reinforcement/double_pendulum/run-dqn.py
+++ examples/learning/reinforcement/double_pendulum/run-dqn.py
@@ -15,7 +15,7 @@ e["Problem"]["Possible Actions"] = [ [ -10.0 ], [ -9.0 ], [ -8.0 ], [ -7.0 ], [ 
                                      [  10.0 ], [  9.0 ], [  8.0 ], [  7.0 ], [  6.0 ], [  5.0 ], [  4.0 ], [  3.0 ], [  2.0 ], [  1.0 ]  ]
 e["Problem"]["Environment Function"] = env
 e["Problem"]["Action Repeat"] = 1
-e["Problem"]["Actions Between Policy Updates"] = 1
+e["Problem"]["Actions Between Policy Updates"] = 10
 
 e["Variables"][0]["Name"] = "Cart Position"
 e["Variables"][0]["Type"] = "State"
@@ -23,27 +23,30 @@ e["Variables"][0]["Type"] = "State"
 e["Variables"][1]["Name"] = "Cart Velocity"
 e["Variables"][1]["Type"] = "State"
 
-e["Variables"][2]["Name"] = "Pole Omega"
+e["Variables"][2]["Name"] = "Angle 1"
 e["Variables"][2]["Type"] = "State"
 
-e["Variables"][3]["Name"] = "Pole Cos(Angle)"
+e["Variables"][3]["Name"] = "Angular Velocity 1"
 e["Variables"][3]["Type"] = "State"
 
-e["Variables"][4]["Name"] = "Pole Sin(Angle)"
+e["Variables"][4]["Name"] = "Angle 2"
 e["Variables"][4]["Type"] = "State"
 
-e["Variables"][5]["Name"] = "Force"
-e["Variables"][5]["Type"] = "Action"
+e["Variables"][5]["Name"] = "Angular Velocity 2"
+e["Variables"][5]["Type"] = "State"
+
+e["Variables"][6]["Name"] = "Force"
+e["Variables"][6]["Type"] = "Action"
 
 ### Configuring DQN hyperparameters
 
 e["Solver"]["Type"] = "Agent / Discrete / DQN"
-e["Solver"]["Experiences Between Updates"] = 10
+e["Solver"]["Experiences Between Updates"] = 100
 e["Solver"]["Optimization Steps Per Update"] = 1
 
 ### Defining Experience Replay configuration
 
-e["Solver"]["Experience Replay"]["Start Size"] = 500
+e["Solver"]["Experience Replay"]["Start Size"] = 10000
 e["Solver"]["Experience Replay"]["Maximum Size"] = 150000
 
 ### Defining probability of taking a random action (epsilon)
@@ -56,7 +59,7 @@ e["Solver"]["Random Action Probability"]["Decrease Rate"] = 0.10
 
 e["Solver"]["Critic"]["Mini Batch Size"] = 32
 e["Solver"]["Critic"]["Learning Rate"] = 0.01
-e["Solver"]["Critic"]["Discount Factor"] = 0.9999
+e["Solver"]["Critic"]["Discount Factor"] = 0.95
 
 ### Defining the shape of the neural network
 

2393147cd2a23e9083827771bb11a81c4250f7d9
Sergio Martin <martiser@ethz.ch>
2020-10-18 17:11:16 +0200

    Nek5000 environment working

--- examples/study.cases/nek5000/_model/model.hpp
+++ examples/study.cases/nek5000/_model/model.hpp
@@ -1,20 +1,37 @@
-//  Korali model for CubismUP_2D For Fish Following Experiment
+//  Korali environment for Nek5000
 //  Copyright (c) 2020 CSE-Lab, ETH Zurich, Switzerland.
+
+#ifndef _NEK5000_MODEL_HPP
+#define _NEK5000_MODEL_HPP
+
+#define STATE_SIZE 6
+#define ACTION_SIZE 1
 
 #include "korali.hpp"
 #include <random>
 
-void runEnvironment(korali::Sample &s);
+extern void runEnvironment(korali::Sample &s);
+extern void updateAgent();
+
+extern korali::Sample* sample;
+extern double _reward;
+extern double _state[STATE_SIZE];
+extern double _action[ACTION_SIZE];
 
 extern "C"
 {
  void nek_init_(int* comm);
  void nek_solve_();
  void nek_end_();
- void updateagent_(double* state, double* reward, double* action);
+ void setreward_(double* reward);
+ void setstate_(double state[STATE_SIZE]);
+ void getaction_(double action[ACTION_SIZE]);
+ void updateagent_();
 }
 
-void updateAgent(double* state, double reward, double* action);
-void updateagent_(double* state, double* reward, double* action) { updateAgent(state, *reward, action); }
+void setreward_(double* reward) { _reward = *reward; }
+void setstate_(double state[STATE_SIZE]) { for (int i = 0; i < STATE_SIZE; i++) _state[i] = state[i]; }
+void getaction_(double action[ACTION_SIZE]) { for (int i = 0; i < ACTION_SIZE; i++) _action[i] = action[i]; }
+void updateagent_() { updateAgent(); }
 
-extern korali::Sample* sample;
+#endif
--- examples/study.cases/nek5000/install_deps.sh
+++ examples/study.cases/nek5000/install_deps.sh
@@ -15,13 +15,19 @@ fi
 export NEK5000_DIR=${PWD}/_deps/nek5000
 export NEK_SOURCE_ROOT=${NEK5000_DIR}
 
-# Getting code
+# Getting source code
 rm -rf _deps
 git clone --depth 1 --recursive https://github.com/Nek5000/Nek5000.git ${NEK5000_DIR}
 
+# Modifying nek5000 as to not exit on success
+pushd ${NEK5000_DIR}/core
+cat drive1.f | sed -e 's/call exitt0()/! call exitt0()/g' > drive1.f.tmp
+mv drive1.f.tmp drive1.f
+popd
+
 # Building turbChannel
 cp _config/* ${NEK5000_DIR}/examples/turbChannel
-cp _model/AGENT _model/*.usr ${NEK5000_DIR}/examples/turbChannel
+cp _model/turbChannel.usr ${NEK5000_DIR}/examples/turbChannel
 pushd ${NEK5000_DIR}/examples/turbChannel
 MPI=0 FFLAGS='-O3 -g' CFLAGS='-O3 -g' ${NEK5000_DIR}/bin/nekconfig -build-dep;
 MPI=0 FFLAGS='-O3 -g' CFLAGS='-O3 -g' ${NEK5000_DIR}/bin/nekconfig;

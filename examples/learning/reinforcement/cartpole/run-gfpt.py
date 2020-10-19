26fbdbe54d8b8e4c1996e866b0f912d8ef1ec111
Sergio Martin <martiser@ethz.ch>
2020-10-19 18:17:00 +0200

    Fixing target for the gfpt example

--- examples/learning/reinforcement/cartpole/run-gfpt.py
+++ examples/learning/reinforcement/cartpole/run-gfpt.py
@@ -86,7 +86,7 @@ e["Solver"]["Critic"]["Neural Network"]["Layers"][3]["Batch Normalization"]["Ena
 ## Defining Policy Configuration
 
 e["Solver"]["Policy"]["Learning Rate"] = 0.001
-e["Solver"]["Policy"]["Mini Batch Size"] = 8 
+e["Solver"]["Policy"]["Mini Batch Size"] = 16
 e["Solver"]["Policy"]["Target Accuracy"] = 0.1
 e["Solver"]["Policy"]["Normalization Steps"] = 32
 
@@ -116,7 +116,7 @@ e["Solver"]["Policy"]["Neural Network"]["Output Scaling"] = [ 10.0 ]
 
 e["Solver"]["Training Reward Threshold"] = 400
 e["Solver"]["Policy Testing Episodes"] = 1
-e["Solver"]["Termination Criteria"]["Target Average Testing Reward"] = 0
+e["Solver"]["Termination Criteria"]["Target Average Testing Reward"] = 400
 
 ### Setting file output configuration
 

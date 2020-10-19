ec2a72829f2e70612f5f5b019b51174cd28ad2e6
wadaniel <wadaniel@ethz.ch>
2020-10-19 15:12:35 +0200

    wip

--- examples/study.cases/nek5000/_environment/agent.cpp
+++ examples/study.cases/nek5000/_environment/agent.cpp
@@ -2,36 +2,35 @@
 //  Copyright (c) 2020 CSE-Lab, ETH Zurich, Switzerland.
 
 #include "agent.hpp"
-#include "unistd.h"
 #include "stdio.h"
+#include "unistd.h"
 
-korali::Sample* sample;
+korali::Sample *sample;
 double _reward;
 double _state[STATE_SIZE];
 double _action[ACTION_SIZE];
 
 void updateAgent()
 {
- // Setting states
- for (size_t i = 0; i < STATE_SIZE; i++)
-  (*sample)["State"][i] = _state[i];
+  // Setting states
+  for (size_t i = 0; i < STATE_SIZE; i++)
+    (*sample)["State"][i] = _state[i];
 
- // Setting Reward
- (*sample)["Reward"] = _reward;
+  // Setting Reward
+  (*sample)["Reward"] = _reward;
 
- // Getting new action
- sample->update();
+  // Getting new action
+  sample->update();
 
- // Reading new action
- for (size_t i = 0; i < ACTION_SIZE; i++)
-  _action[i] = (*sample)["Action"][i];
+  // Reading new action
+  for (size_t i = 0; i < ACTION_SIZE; i++)
+    _action[i] = (*sample)["Action"][i];
 
- // Printing step info
- printf("[Korali] ----------------------------------------------------\n");
- printf("[Korali] Step Information:\n");
- printf("[Korali] State: [ %f, %f, %f, %f, %f, %f ]\n", _state[0], _state[1], _state[2], _state[3], _state[4], _state[5]);
- printf("[Korali] Reward: %f\n", _reward);
- printf("[Korali] Action: %f\n", _action[0]);
- printf("[Korali] ----------------------------------------------------\n");
+  // Printing step info
+  printf("[Korali] ----------------------------------------------------\n");
+  printf("[Korali] Step Information:\n");
+  printf("[Korali] State: [ %f, %f, %f, %f, %f, %f ]\n", _state[0], _state[1], _state[2], _state[3], _state[4], _state[5]);
+  printf("[Korali] Reward: %f\n", _reward);
+  printf("[Korali] Action: %f\n", _action[0]);
+  printf("[Korali] ----------------------------------------------------\n");
 }
-

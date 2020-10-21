1fa68dff01474fc68c72bd4e8f14b9a48990cf9a
Sergio Martin <martiser@ethz.ch>
2020-10-21 16:18:46 +0200

    Fixing documentation

--- source/auxiliar/cbuffer.hpp
+++ source/auxiliar/cbuffer.hpp
@@ -9,18 +9,51 @@
 
 #include <memory>
 
+/**
+* \namespace korali
+* @brief The Korali namespace includes all Korali-specific functions, variables, and modules.
+*/
 namespace korali
 {
+
+/**
+* \class cBuffer
+* @brief This class defines a circular buffer with overwrite policy on add
+*/
 template <typename T>
 class cBuffer
 {
+
+ /**
+  * @brief Size of buffer container
+  */
   size_t _max_size;
+
+  /**
+  * @brief Number of elements already added
+  */
   size_t _size;
+
+  /**
+  * @brief Container for data
+  */
   std::unique_ptr<T[]> _data;
+
+  /**
+   * @brief Position of the start of the buffer
+   */
   size_t _start;
+
+  /**
+   * @brief Position of the end of the buffer
+   */
   size_t _end;
 
   public:
+
+  /**
+   * @brief Default constructor
+   */
   cBuffer()
   {
     _max_size = 0;
@@ -29,13 +62,25 @@ class cBuffer
     _end = 0;
   };
 
+  /**
+   * @brief Constructor with a specific size
+   * @param size The buffer size
+   */
   cBuffer(size_t size)
   {
     resize(size);
   };
 
+  /**
+  * @brief Returns the current number of elements in the buffer
+  * @return The number of elements
+  */
   size_t size() { return _size; };
 
+  /**
+  * @brief Returns the current number of elements in the buffer
+  * @param size The buffer size
+  */
   void resize(size_t size)
   {
     _data = std::make_unique<T[]>(size);
@@ -46,6 +91,10 @@ class cBuffer
     _end = 0;
   }
 
+  /**
+  * @brief Adds an element to the buffer
+  * @param v The element to add
+  */
   void add(const T &v)
   {
     // Storing value
@@ -65,6 +114,11 @@ class cBuffer
     if (_start == _max_size) _start = 0;
   }
 
+  /**
+  * @brief Accesses an element at the required position
+  * @param pos The access position
+  * @return The element corresponding to the position
+  */
   T &operator[](size_t pos)
   {
     return _data[(_start + pos) % _max_size];

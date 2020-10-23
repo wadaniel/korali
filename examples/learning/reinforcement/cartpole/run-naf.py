3e659c2b84a2e549c2fce4fbc277570765a599bb
wadaniel <wadaniel@ethz.ch>
2020-10-23 12:54:40 +0200

    naf wip

--- examples/learning/reinforcement/cartpole/run-naf.py
+++ examples/learning/reinforcement/cartpole/run-naf.py
@@ -4,19 +4,18 @@ import sys
 sys.path.append('./_model')
 from env import *
 
-actions = [[i] for i in range(-10,10)]
-
 ####### Defining Korali Problem
 
 import korali
 k = korali.Engine()
 e = korali.Experiment()
 
+### Defining the Cartpole problem's configuration
+
 e["Problem"]["Type"] = "Reinforcement Learning / Continuous"
-e["Problem"]["Possible Actions"] = actions
 e["Problem"]["Environment Function"] = env
 e["Problem"]["Action Repeat"] = 1
-e["Problem"]["Actions Between Policy Updates"] = 1
+e["Problem"]["Actions Between Policy Updates"] = 10 
 
 e["Variables"][0]["Name"] = "Cart Position"
 e["Variables"][0]["Type"] = "State"
@@ -35,33 +34,32 @@ e["Variables"][4]["Type"] = "State"
 
 e["Variables"][5]["Name"] = "Force"
 e["Variables"][5]["Type"] = "Action"
+e["Variables"][5]["Lower Bound"] = -10.0
+e["Variables"][5]["Upper Bound"] = +10.0
+e["Variables"][5]["Exploration Sigma"] = 0.35
 
 ### Configuring NAF hyperparameters
 
 e["Solver"]["Type"] = "Agent / Continuous / NAF"
+e["Solver"]["Target Learning Rate"] = 0.3
 e["Solver"]["Optimization Steps Per Update"] = 1
 e["Solver"]["Experiences Between Agent Trainings"] = 1
-e["Solver"]["Experiences Between Target Network Updates"] = 50
+e["Solver"]["Experiences Between Target Network Updates"] = 1
+
+e["Solver"]["Random Action Probability"]["Initial Value"] = 0.5
+e["Solver"]["Random Action Probability"]["Target Value"] = 0.01
+e["Solver"]["Random Action Probability"]["Decrease Rate"] = 0.03
 
 ### Defining Experience Replay configuration
 
-e["Solver"]["Experience Replay"]["Start Size"] = 500
-e["Solver"]["Experience Replay"]["Maximum Size"] = 150000
+e["Solver"]["Experience Replay"]["Start Size"] =   1000
+e["Solver"]["Experience Replay"]["Maximum Size"] = 100000
 
-### Defining probability of taking a random action (epsilon)
+## Defining Q-Network
 
-e["Solver"]["Random Action Probability"]["Initial Value"] = 1.0
-e["Solver"]["Random Action Probability"]["Target Value"] = 0.05
-e["Solver"]["Random Action Probability"]["Decrease Rate"] = 0.10
-
-## Defining Q-Critic and Action-selection (policy) optimizers
-
-e["Solver"]["Critic"]["Mini Batch Size"] = 32
+e["Solver"]["Critic"]["Discount Factor"] = 0.99
 e["Solver"]["Critic"]["Learning Rate"] = 0.01
-e["Solver"]["Critic"]["Discount Factor"] = 0.9999
-
-### Defining the shape of the neural network
-
+e["Solver"]["Critic"]["Mini Batch Size"] = 32
 e["Solver"]["Critic"]["Normalization Steps"] = 32
 
 e["Solver"]["Critic"]["Neural Network"]["Layers"][0]["Type"] = "Layer/Dense"
@@ -80,17 +78,17 @@ e["Solver"]["Critic"]["Neural Network"]["Layers"][2]["Batch Normalization"]["Ena
 
 e["Solver"]["Critic"]["Neural Network"]["Layers"][3]["Type"] = "Layer/Dense"
 e["Solver"]["Critic"]["Neural Network"]["Layers"][3]["Activation Function"]["Type"] = "Elementwise/Linear"
-e["Solver"]["Critic"]["Neural Network"]["Layers"][3]["Batch Normalization"]["Enabled"] = True 
+e["Solver"]["Critic"]["Neural Network"]["Layers"][3]["Batch Normalization"]["Enabled"] = True
 
 ### Defining Termination Criteria
 
-e["Solver"]["Training Reward Threshold"] = 400
+e["Solver"]["Training Reward Threshold"] = 450
 e["Solver"]["Policy Testing Episodes"] = 20
-e["Solver"]["Termination Criteria"]["Target Average Testing Reward"] = 450
+e["Solver"]["Termination Criteria"]["Target Average Testing Reward"] = 0
 
 ### Setting file output configuration
 
-e["File Output"]["Frequency"] = 100
+e["File Output"]["Frequency"] = 100000
 
 ### Running Experiment
 

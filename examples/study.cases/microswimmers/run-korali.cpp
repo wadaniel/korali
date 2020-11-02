b21ab08e889f4053324214f8e8c28bbbb413396a
Sergio Martin <martiser@ethz.ch>
2020-11-02 13:51:57 +0100

    Updating gfpt to work with retrace

--- examples/study.cases/microswimmers/run-korali.cpp
+++ examples/study.cases/microswimmers/run-korali.cpp
@@ -77,58 +77,61 @@ int main(int argc, char *argv[])
   e["Solver"]["Random Action Probability"]["Target Value"] = 0.00;
   e["Solver"]["Random Action Probability"]["Decrease Rate"] = 0.05;
 
-  e["Solver"]["Experience Replay"]["Start Size"] = 1000;
-  e["Solver"]["Experience Replay"]["Maximum Size"] = 200000;
+  e["Solver"]["Experience Replay"]["Start Size"] = 1024;
+  e["Solver"]["Experience Replay"]["Maximum Size"] = 24576;
 
   //// Defining Critic Configuration
 
-  e["Solver"]["Critic"]["Learning Rate"] = 0.00001;
+  e["Solver"]["Critic"]["Learning Rate"] = 0.0001;
   e["Solver"]["Critic"]["Discount Factor"] = 0.99;
-  e["Solver"]["Critic"]["Mini Batch Size"] = 128;
+  e["Solver"]["Critic"]["Mini Batch Size"] = 64;
   e["Solver"]["Critic"]["Normalization Steps"] = 0;
+
+  e["Solver"]["Critic"]["Retrace"]["Enabled"] = false;
+  e["Solver"]["Critic"]["Retrace"]["Cache Persistence"] = 10;
 
   e["Solver"]["Critic"]["Neural Network"]["Layers"][0]["Type"] = "Layer/Dense";
   e["Solver"]["Critic"]["Neural Network"]["Layers"][0]["Activation Function"]["Type"] = "Elementwise/Linear";
-  e["Solver"]["Critic"]["Neural Network"]["Layers"][0]["Batch Normalization"]["Enabled"] = false;
+  e["Solver"]["Critic"]["Neural Network"]["Layers"][0]["Batch Normalization"]["Enabled"] = true;
 
   e["Solver"]["Critic"]["Neural Network"]["Layers"][1]["Type"] = "Layer/Dense";
-  e["Solver"]["Critic"]["Neural Network"]["Layers"][1]["Node Count"] = 128;
+  e["Solver"]["Critic"]["Neural Network"]["Layers"][1]["Node Count"] = 64;
   e["Solver"]["Critic"]["Neural Network"]["Layers"][1]["Activation Function"]["Type"] = "Elementwise/Tanh";
-  e["Solver"]["Critic"]["Neural Network"]["Layers"][1]["Batch Normalization"]["Enabled"] = false;
+  e["Solver"]["Critic"]["Neural Network"]["Layers"][1]["Batch Normalization"]["Enabled"] = true;
 
   e["Solver"]["Critic"]["Neural Network"]["Layers"][2]["Type"] = "Layer/Dense";
-  e["Solver"]["Critic"]["Neural Network"]["Layers"][2]["Node Count"] = 128;
+  e["Solver"]["Critic"]["Neural Network"]["Layers"][2]["Node Count"] = 64;
   e["Solver"]["Critic"]["Neural Network"]["Layers"][2]["Activation Function"]["Type"] = "Elementwise/Tanh";
-  e["Solver"]["Critic"]["Neural Network"]["Layers"][2]["Batch Normalization"]["Enabled"] = false;
+  e["Solver"]["Critic"]["Neural Network"]["Layers"][2]["Batch Normalization"]["Enabled"] = true;
 
   e["Solver"]["Critic"]["Neural Network"]["Layers"][3]["Type"] = "Layer/Dense";
   e["Solver"]["Critic"]["Neural Network"]["Layers"][3]["Activation Function"]["Type"] = "Elementwise/Linear";
-  e["Solver"]["Critic"]["Neural Network"]["Layers"][3]["Batch Normalization"]["Enabled"] = false;
+  e["Solver"]["Critic"]["Neural Network"]["Layers"][3]["Batch Normalization"]["Enabled"] = true;
 
   //// Defining Policy Configuration
 
-  e["Solver"]["Policy"]["Learning Rate"] = 0.00001;
+  e["Solver"]["Policy"]["Learning Rate"] = 0.0001;
   e["Solver"]["Policy"]["Mini Batch Size"] = 64;
-  e["Solver"]["Policy"]["Target Accuracy"] = 0.0001;
+  e["Solver"]["Policy"]["Target Accuracy"] = 0.001;
   e["Solver"]["Policy"]["Normalization Steps"] = 0;
 
   e["Solver"]["Policy"]["Neural Network"]["Layers"][0]["Type"] = "Layer/Dense";
   e["Solver"]["Policy"]["Neural Network"]["Layers"][0]["Activation Function"]["Type"] = "Elementwise/Linear";
-  e["Solver"]["Policy"]["Neural Network"]["Layers"][0]["Batch Normalization"]["Enabled"] = false;
+  e["Solver"]["Policy"]["Neural Network"]["Layers"][0]["Batch Normalization"]["Enabled"] = true;
 
   e["Solver"]["Policy"]["Neural Network"]["Layers"][1]["Type"] = "Layer/Dense";
-  e["Solver"]["Policy"]["Neural Network"]["Layers"][1]["Node Count"] = 128;
+  e["Solver"]["Policy"]["Neural Network"]["Layers"][1]["Node Count"] = 64;
   e["Solver"]["Policy"]["Neural Network"]["Layers"][1]["Activation Function"]["Type"] = "Elementwise/Tanh";
-  e["Solver"]["Policy"]["Neural Network"]["Layers"][1]["Batch Normalization"]["Enabled"] = false;
+  e["Solver"]["Policy"]["Neural Network"]["Layers"][1]["Batch Normalization"]["Enabled"] = true;
 
   e["Solver"]["Policy"]["Neural Network"]["Layers"][2]["Type"] = "Layer/Dense";
-  e["Solver"]["Policy"]["Neural Network"]["Layers"][2]["Node Count"] = 128;
+  e["Solver"]["Policy"]["Neural Network"]["Layers"][2]["Node Count"] = 64;
   e["Solver"]["Policy"]["Neural Network"]["Layers"][2]["Activation Function"]["Type"] = "Elementwise/Tanh";
-  e["Solver"]["Policy"]["Neural Network"]["Layers"][2]["Batch Normalization"]["Enabled"] = false;
+  e["Solver"]["Policy"]["Neural Network"]["Layers"][2]["Batch Normalization"]["Enabled"] = true;
 
   e["Solver"]["Policy"]["Neural Network"]["Layers"][3]["Type"] = "Layer/Dense";
   e["Solver"]["Policy"]["Neural Network"]["Layers"][3]["Activation Function"]["Type"] = "Elementwise/Tanh";
-  e["Solver"]["Policy"]["Neural Network"]["Layers"][3]["Batch Normalization"]["Enabled"] = false;
+  e["Solver"]["Policy"]["Neural Network"]["Layers"][3]["Batch Normalization"]["Enabled"] = true;
 
 
   ////// Defining Termination Criteria

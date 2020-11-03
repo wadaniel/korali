1730c30f6633179e1c9925e71536c13bed5284f7
Sergio Martin <martiser@ethz.ch>
2020-11-03 08:42:21 +0100

    Updating gfpt

--- examples/learning/reinforcement/cartpole/run-gfpt.py
+++ examples/learning/reinforcement/cartpole/run-gfpt.py
@@ -63,6 +63,9 @@ e["Solver"]["Critic"]["Learning Rate"] = 0.01
 e["Solver"]["Critic"]["Mini Batch Size"] = 32
 e["Solver"]["Critic"]["Normalization Steps"] = 0
 
+e["Solver"]["Critic"]["Retrace"]["Enabled"] = True
+e["Solver"]["Critic"]["Retrace"]["Cache Persistence"] = 5
+  
 e["Solver"]["Critic"]["Neural Network"]["Layers"][0]["Type"] = "Layer/Dense"
 e["Solver"]["Critic"]["Neural Network"]["Layers"][0]["Activation Function"]["Type"] = "Elementwise/Linear"
 e["Solver"]["Critic"]["Neural Network"]["Layers"][0]["Batch Normalization"]["Enabled"] = True

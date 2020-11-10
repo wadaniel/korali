1da070ec63e65ae4923c288d6be4530201d75074
wadaniel <wadaniel@ethz.ch>
2020-11-10 22:29:47 +0100

    cov scaling

--- examples/learning/reinforcement/dummycart/run-naf.py
+++ examples/learning/reinforcement/dummycart/run-naf.py
@@ -29,8 +29,9 @@ e["Variables"][1]["Exploration Sigma"] = 0.35
 ### Configuring NAF hyperparameters
 
 e["Solver"]["Type"] = "Agent / Continuous / NAF"
-e["Solver"]["Target Learning Rate"] = 0.0001
+e["Solver"]["Target Learning Rate"] = 0.001
 e["Solver"]["Optimization Steps Per Update"] = 100
+e["Solver"]["Covariance Scaling"] = 0.01
 e["Solver"]["Experiences Between Agent Trainings"] = 1
 e["Solver"]["Mini Batch Strategy"] = "Prioritized"
 
@@ -40,26 +41,26 @@ e["Solver"]["Random Action Probability"]["Decrease Rate"] = 0.03
 
 ### Defining Experience Replay configuration
 
-e["Solver"]["Experience Replay"]["Start Size"] =   1000
-e["Solver"]["Experience Replay"]["Maximum Size"] = 5000
+e["Solver"]["Experience Replay"]["Start Size"] =   100
+e["Solver"]["Experience Replay"]["Maximum Size"] = 1000
 
 ## Defining Q-Network
 
 e["Solver"]["Critic"]["Discount Factor"] = 0.99
-e["Solver"]["Critic"]["Learning Rate"] = 1e-3
-e["Solver"]["Critic"]["Mini Batch Size"] = 128
+e["Solver"]["Critic"]["Learning Rate"] = 1e-2
+e["Solver"]["Critic"]["Mini Batch Size"] = 64
 
 e["Solver"]["Critic"]["Neural Network"]["Layers"][0]["Type"] = "Layer/Dense"
 e["Solver"]["Critic"]["Neural Network"]["Layers"][0]["Activation Function"]["Type"] = "Elementwise/Linear"
 e["Solver"]["Critic"]["Neural Network"]["Layers"][0]["Batch Normalization"]["Enabled"] = False
 
 e["Solver"]["Critic"]["Neural Network"]["Layers"][1]["Type"] = "Layer/Dense"
-e["Solver"]["Critic"]["Neural Network"]["Layers"][1]["Node Count"] = 128
+e["Solver"]["Critic"]["Neural Network"]["Layers"][1]["Node Count"] = 16
 e["Solver"]["Critic"]["Neural Network"]["Layers"][1]["Activation Function"]["Type"] = "Elementwise/Tanh"
 e["Solver"]["Critic"]["Neural Network"]["Layers"][1]["Batch Normalization"]["Enabled"] = False
 
 e["Solver"]["Critic"]["Neural Network"]["Layers"][2]["Type"] = "Layer/Dense"
-e["Solver"]["Critic"]["Neural Network"]["Layers"][2]["Node Count"] = 128
+e["Solver"]["Critic"]["Neural Network"]["Layers"][2]["Node Count"] = 16
 e["Solver"]["Critic"]["Neural Network"]["Layers"][2]["Activation Function"]["Type"] = "Elementwise/Tanh"
 e["Solver"]["Critic"]["Neural Network"]["Layers"][2]["Batch Normalization"]["Enabled"] = False
 

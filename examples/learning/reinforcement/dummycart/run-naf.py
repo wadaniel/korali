b55e40c863913b3bbbf962b34fcfc01a4a7b313f
wadaniel <wadaniel@ethz.ch>
2020-11-11 12:51:00 +0100

    naf dummycart works

--- examples/learning/reinforcement/dummycart/run-naf.py
+++ examples/learning/reinforcement/dummycart/run-naf.py
@@ -22,8 +22,8 @@ e["Variables"][0]["Type"] = "State"
 
 e["Variables"][1]["Name"] = "Force"
 e["Variables"][1]["Type"] = "Action"
-e["Variables"][1]["Lower Bound"] = -3.0
-e["Variables"][1]["Upper Bound"] = +3.0
+e["Variables"][1]["Lower Bound"] = -1.0
+e["Variables"][1]["Upper Bound"] = +1.0
 e["Variables"][1]["Exploration Sigma"] = 0.35
 
 ### Configuring NAF hyperparameters
@@ -31,24 +31,24 @@ e["Variables"][1]["Exploration Sigma"] = 0.35
 e["Solver"]["Type"] = "Agent / Continuous / NAF"
 e["Solver"]["Target Learning Rate"] = 0.001
 e["Solver"]["Optimization Steps Per Update"] = 100
+e["Solver"]["Experiences Between Agent Trainings"] = 10
 e["Solver"]["Covariance Scaling"] = 0.01
-e["Solver"]["Experiences Between Agent Trainings"] = 1
 e["Solver"]["Mini Batch Strategy"] = "Prioritized"
 
-e["Solver"]["Random Action Probability"]["Initial Value"] = 0.5
-e["Solver"]["Random Action Probability"]["Target Value"] = 0.00
+e["Solver"]["Random Action Probability"]["Initial Value"] = 0.0
+e["Solver"]["Random Action Probability"]["Target Value"] = 0.0
 e["Solver"]["Random Action Probability"]["Decrease Rate"] = 0.03
 
 ### Defining Experience Replay configuration
 
-e["Solver"]["Experience Replay"]["Start Size"] =   100
-e["Solver"]["Experience Replay"]["Maximum Size"] = 1000
+e["Solver"]["Experience Replay"]["Start Size"] =   200
+e["Solver"]["Experience Replay"]["Maximum Size"] = 10000
 
 ## Defining Q-Network
 
 e["Solver"]["Critic"]["Discount Factor"] = 0.99
-e["Solver"]["Critic"]["Learning Rate"] = 1e-2
-e["Solver"]["Critic"]["Mini Batch Size"] = 64
+e["Solver"]["Critic"]["Learning Rate"] = 1e-5
+e["Solver"]["Critic"]["Mini Batch Size"] = 16
 
 e["Solver"]["Critic"]["Neural Network"]["Layers"][0]["Type"] = "Layer/Dense"
 e["Solver"]["Critic"]["Neural Network"]["Layers"][0]["Activation Function"]["Type"] = "Elementwise/Linear"
@@ -76,7 +76,7 @@ e["Solver"]["Termination Criteria"]["Target Average Testing Reward"] = 95
 
 ### Setting file output configuration
 
-e["File Output"]["Enabled"] = False
+e["File Output"]["Frequency"] = 100000
 
 ### Running Experiment
 

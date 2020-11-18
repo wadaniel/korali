0cfcaa40d12773c0489f7d3cec52a678eb7e6cf5
Sergio Martin <martiser@ethz.ch>
2020-11-18 13:40:36 +0100

    Added possibility to define result folder in abf2d

--- examples/learning/reinforcement/ABF2D/_model/env.py
+++ examples/learning/reinforcement/ABF2D/_model/env.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 from abf import *
+import os
 
 ######## Defining Environment Storage
 
+resDir = ''
 maxReward = -100000
 swimmers = Swimmers()
+
+def setResultsDir(dir):
+ if not os.path.exists(dir): os.makedirs(dir)
+ global resDir
+ resDir=dir
 
 def env(s):
 
@@ -41,11 +48,12 @@ def env(s):
   s["Termination"] = "Truncated"
   
  # Saving the last/best trajectories for visualization
+ global resDir
  global maxReward
  if (cumulativeReward > maxReward):
   maxReward = cumulativeReward
-  swimmers.dumpTrajectoryToCsv('_result/best.csv')
- swimmers.dumpTrajectoryToCsv('_result/last.csv')
+  swimmers.dumpTrajectoryToCsv(resDir + '/best.csv')
+ swimmers.dumpTrajectoryToCsv(resDir + '/last.csv')
  
  # Saving current trajectory
- swimmers.dumpTrajectoryToCsv('_result/trajectory' + str(sampleId).zfill(6) + '.csv')
+ swimmers.dumpTrajectoryToCsv(resDir + '/trajectory' + str(sampleId).zfill(6) + '.csv')

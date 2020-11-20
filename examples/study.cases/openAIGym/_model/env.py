d2e58f2871f2cc3d1518404498b9ca8653d378c1
Sergio Martin <martiser@ethz.ch>
2020-11-20 12:56:18 +0100

    Adding openAIgym example

---
+++ examples/study.cases/openAIGym/_model/env.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import gym
+
+######## Defining Environment Storage
+
+cart = gym.make('CartPole-v1').unwrapped
+maxSteps = 1000
+
+def env(s):
+
+ # Initializing environment
+ seed = s["Sample Id"]
+ cart.seed(seed)
+ 
+ # Resetting and getting initial state
+ s["State"] = cart.reset().tolist()
+ 
+ # Setting termination conditions
+ step = 0
+ done = False
+
+ # Running steps until termination is met
+ while not done and step < maxSteps:
+
+  # Getting new action
+  s.update()
+  
+  # Reading action
+  action = s["Action"][0] 
+    
+  # Performing the action
+  state, reward, done, info = cart.step(action)
+
+  # Storing Reward
+  s["Reward"] = reward
+   
+  # Storing New State
+  s["State"] = state.tolist()
+  
+  # Advancing step counter
+  step = step + 1
+
+ # Setting termination status
+ if (done):
+  s["Termination"] = "Normal"
+ else:
+  s["Termination"] = "Truncated"

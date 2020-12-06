f2f820da75bfdabd74712d1f5649435b1e2ae3a5
Sergio Martin <martiser@ethz.ch>
2020-12-06 15:29:09 +0100

    Improving fish experiment

--- examples/study.cases/fish/run_test.sh
+++ examples/study.cases/fish/run_test.sh
@@ -3,4 +3,5 @@
 # Set number of nodes here
 N=$SLURM_NNODES
 
-echo "OMP_NUM_THREADS=12 srun -N $N -n $N -c 12 ./run-korali -poissonType cosine -muteAll 1 -bpdx 32 -bpdy 16 -fdump 0 -tdump 0 -nu 0.000018 -tend 0 -shapes 'halfDisk_radius=.06_angle=20_xpos=.2_bForced=1_bFixed=1_xvel=0.15_tAccel=5,stefanfish_L=.2_xpos=.5'"
+rm -r _results
+OMP_NUM_THREADS=12 srun -N $N -n $N -c 12 ./run-korali -poissonType cosine -muteAll 1 -bpdx 32 -bpdy 16 -tdump 0 -nu 0.000018 -tend 0 -shapes 'halfDisk_radius=.06_angle=20_xpos=.2_bForced=1_bFixed=1_xvel=0.15_tAccel=5,stefanfish_L=.2_xpos=.5'

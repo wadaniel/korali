92c455838d6ee4777a1dec9316de900e07d91888
Sergio Martin <martiser@ethz.ch>
2020-12-08 17:14:19 +0100

    Added checkpoint test for agents and fixed bug in concurrent

--- examples/features/checkpoint.resume/.run_test.sh
+++ examples/features/checkpoint.resume/.run_test.sh
@@ -7,9 +7,12 @@ source ../../../tests/functions.sh
 ##### Deleting Previous Results
 
 echo "  + Deleting previous results..." 
-rm -rf _korali_result*; check_result
+rm -rf _result*; check_result
 
 ##### Running Tests
 
 python3 ./run-cmaes.py; check_result
+python3 ./run-cmaes.py; check_result
+python3 ./run-gfpt.py; check_result
+python3 ./run-gfpt.py; check_result
 

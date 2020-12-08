1b04115d447e594c0d78db1da1e16ea1aaeeb3c5
Sergio Martin <martiser@ethz.ch>
2020-12-08 11:39:27 +0100

    Adding persisting agents

--- examples/learning/reinforcement/cartpole/run-gfpt.py
+++ examples/learning/reinforcement/cartpole/run-gfpt.py
@@ -15,7 +15,7 @@ e = korali.Experiment()
 e["Problem"]["Type"] = "Reinforcement Learning / Continuous"
 e["Problem"]["Environment Function"] = env
 e["Problem"]["Actions Between Policy Updates"] = 1
-e["Problem"]["Training Reward Threshold"] = 400
+e["Problem"]["Training Reward Threshold"] = 50
 e["Problem"]["Policy Testing Episodes"] = 10
 
 ### Defining state variables
@@ -36,7 +36,8 @@ e["Variables"][4]["Exploration Sigma"] = 0.35
 ### Defining Agent Configuration 
 
 e["Solver"]["Type"] = "Agent / Continuous / GFPT"
-e["Solver"]["Episodes Per Generation"] = 5
+e["Solver"]["Agent Count"] = 5
+e["Solver"]["Experiences Per Generation"] = 500
 e["Solver"]["Experiences Between Policy Updates"] = 1
 e["Solver"]["Cache Persistence"] = 10
 
@@ -98,7 +99,7 @@ e["Solver"]["Policy"]["Neural Network"]["Layers"][3]["Activation Function"]["Typ
 
 ### Defining Termination Criteria
 
-e["Solver"]["Termination Criteria"]["Target Average Testing Reward"] = 450
+e["Solver"]["Termination Criteria"]["Target Average Testing Reward"] = 50
 
 ### Setting file output configuration
 

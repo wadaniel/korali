ba5f2539584522b7c7f60a347ea4587994ed6a36
Sergio Martin <martiser@ethz.ch>
2020-12-12 19:25:36 +0100

    Fixing fish script

--- examples/study.cases/fish/run-korali.cpp
+++ examples/study.cases/fish/run-korali.cpp
@@ -46,7 +46,7 @@ int main(int argc, char *argv[])
 
   // Configuring Experiment
   e["Problem"]["Environment Function"] = &runEnvironment;
-  e["Problem"]["Training Reward Threshold"] = -12.0;
+  e["Problem"]["Training Reward Threshold"] = 3.0;
   e["Problem"]["Policy Testing Episodes"] = 1;
   e["Problem"]["Actions Between Policy Updates"] = 1;
 

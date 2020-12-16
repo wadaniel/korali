a6365e87a8a1e4687a0e6d66ae71a705c48101d9
wadaniel <wadaniel@ethz.ch>
2020-12-16 21:45:01 +0100

    cmairl with mpi

--- examples/study.cases/CMAIRL/run-cmaes.py
+++ examples/study.cases/CMAIRL/run-cmaes.py
@@ -31,7 +31,7 @@ for i in range(dim):
 
 # Configuring CMA-ES parameters
 e["Solver"]["Type"] = "Optimizer/CMAES"
-e["Solver"]["Population Size"] = 4
+e["Solver"]["Population Size"] = 8
 e["Solver"]["Termination Criteria"]["Min Value Difference Threshold"] = 0.1
 e["Solver"]["Termination Criteria"]["Max Generations"] = 100
 
@@ -41,6 +41,9 @@ e["File Output"]["Enabled"] = True
 e["File Output"]["Frequency"] = 1
 
 e["Console Output"]["Verbosity"] = "Detailed"
+
+k["Conduit"]["Type"] = "Concurrent"
+k["Conduit"]["Concurrent Jobs"] = 8
 
 # Running Korali
 k.run(e)

6924d54249db89b1a9aeaa040905b42e07568ef4
Sergio Martin <martiser@ethz.ch>
2020-12-17 18:39:16 +0100

    Progress with rnn, almost there

--- examples/learning/supervised/run-sin.py
+++ examples/learning/supervised/run-sin.py
@@ -35,7 +35,7 @@ e["Solver"]["Learning Rate"] = 0.005
 
 ### Defining the shape of the neural network
 
-e["Solver"]["Neural Network"]["Engine"] = "OneDNN"
+e["Solver"]["Neural Network"]["Engine"] = "CuDNN"
 
 e["Solver"]["Neural Network"]["Layers"][0]["Type"] = "Layer/Input"
 e["Solver"]["Neural Network"]["Layers"][0]["Node Count"] = 1

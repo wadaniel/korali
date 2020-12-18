b25d19e75b3bcb4291c4f0a6f00d189aedd33b73
G. Arampatzis <gtarabat@gmail.com>
2020-12-18 13:54:04 +0100

    add propagation of uncertainty

--- examples/propagation/_model/work.py
+++ examples/propagation/_model/work.py
@@ -10,8 +10,8 @@ def prepareOutputDir():
 
 
 def put_normal_rnds(theta, Ns, fileName):
-  mu = theta["Parameters"][0]
-  var = theta["Parameters"][1]
+  mu = theta['Parameters'][0]
+  var = theta['Parameters'][1]
 
   y = np.random.normal(mu, var, Ns)
 
@@ -22,3 +22,45 @@ def put_normal_rnds(theta, Ns, fileName):
 
   else:
     sys.exit('put_normal_rnds: dir \'_executor_output\' does not exist! exit..')
+
+
+# This is a linear regression model with two params (slope and intercept)
+def model(s, X):
+  a = s['Parameters'][0]
+  b = s['Parameters'][1]
+  sig = s['Parameters'][2]
+
+  s['Reference Evaluations'] = []
+  s['Standard Deviation'] = []
+  for x in X:
+    s['Reference Evaluations'] += [a * x + b]
+    s['Standard Deviation'] += [sig]
+
+def model_propagation(s, X):
+  a = s['Parameters'][0]
+  b = s['Parameters'][1]
+
+  s['sigma'] = s['Parameters'][2]
+  s['X'] = X.tolist()
+  s['Evaluations'] = []
+  for x in X:
+    s['Evaluations'] += [a * x + b]
+
+
+def getReferenceData():
+  y = []
+  y.append(3.21)
+  y.append(4.14)
+  y.append(4.94)
+  y.append(6.06)
+  y.append(6.84)
+  return y
+
+def getReferencePoints():
+  x = []
+  x.append(1.0)
+  x.append(2.0)
+  x.append(3.0)
+  x.append(4.0)
+  x.append(5.0)
+  return x
---
+++ examples/propagation/run-uncertainty-propagation.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+
+# In this example, we sample the posterior of the parameters in a linear model.
+# Next we evaluate the model for all the parameters in the sample database and
+# different input data. Finally, the credible intervals can be plotted.
+
+# Importing the computational model
+import json
+import sys
+sys.path.append('./_model')
+from work import *
+
+data = {}
+data['X'] = getReferencePoints()
+data['Y'] = getReferenceData()
+
+# Sample the posterior of the parameters
+import korali
+e = korali.Experiment()
+
+# Setting up the reference likelihood for the Bayesian Problem
+e['Problem']['Type'] = 'Bayesian/Reference'
+e['Problem']['Likelihood Model'] = 'Normal'
+e['Problem']['Reference Data'] = data['Y']
+e['Problem']['Computational Model'] = lambda sampleData: model(sampleData, data['X'])
+
+# Configuring TMCMC parameters
+e['Solver']['Type'] = 'Sampler/TMCMC'
+e['Solver']['Population Size'] = 1000
+e['Solver']['Target Coefficient Of Variation'] = 0.8
+e['Solver']['Covariance Scaling'] = 0.04
+
+# Configuring the problem's random distributions
+e['Distributions'][0]['Name'] = 'Uniform 0'
+e['Distributions'][0]['Type'] = 'Univariate/Uniform'
+e['Distributions'][0]['Minimum'] = -5.0
+e['Distributions'][0]['Maximum'] = +5.0
+
+e['Distributions'][1]['Name'] = 'Uniform 1'
+e['Distributions'][1]['Type'] = 'Univariate/Uniform'
+e['Distributions'][1]['Minimum'] = -5.0
+e['Distributions'][1]['Maximum'] = +5.0
+
+e['Distributions'][2]['Name'] = 'Uniform 2'
+e['Distributions'][2]['Type'] = 'Univariate/Uniform'
+e['Distributions'][2]['Minimum'] = 0.0
+e['Distributions'][2]['Maximum'] = +5.0
+
+# Configuring the problem's variables and their prior distributions
+e['Variables'][0]['Name'] = 'a'
+e['Variables'][0]['Prior Distribution'] = 'Uniform 0'
+
+e['Variables'][1]['Name'] = 'b'
+e['Variables'][1]['Prior Distribution'] = 'Uniform 1'
+
+e['Variables'][2]['Name'] = '[Sigma]'
+e['Variables'][2]['Prior Distribution'] = 'Uniform 2'
+
+e['Store Sample Information'] = True
+
+# Configuring output settings
+e['File Output']['Path'] = '_korali_result_samples'
+
+# Starting Korali's Engine and running experiment
+e['Console Output']['Verbosity'] = 'Detailed'
+k = korali.Engine()
+k.run(e)
+
+
+# Evaluate the model for all the parameters from the previous step
+e = korali.Experiment()
+
+x = np.linspace(0,7,100)
+
+e['Problem']['Type'] = 'Propagation'
+e['Problem']['Execution Model'] = lambda modelData: model_propagation(modelData, x)
+
+# load the data from the sampling
+with open('_korali_result_samples/latest') as f: d=json.load(f)
+
+e['Variables'][0]['Name'] = 'a'
+e['Variables'][0]['Precomputed Values'] = [ x[0] for x in d['Results']['Sample Database'] ]
+e['Variables'][1]['Name'] = 'b'
+e['Variables'][1]['Precomputed Values'] = [ x[1] for x in d['Results']['Sample Database'] ]
+e['Variables'][2]['Name'] = 'sigma'
+e['Variables'][2]['Precomputed Values'] = [ x[2] for x in d['Results']['Sample Database'] ]
+
+e['Solver']['Type'] = 'Executor'
+e['Solver']['Executions Per Generation'] = 100
+
+e['Console Output']['Verbosity'] = 'Minimal'
+e['File Output']['Path'] = '_korali_result_propagation'
+e['Store Sample Information'] = True
+
+k = korali.Engine()
+k.run(e)
+
+
+# Uncomment the next two lines to plot the credible intervals
+from plots import *
+plot_credible_intervals('./_korali_result_propagation/latest', data)

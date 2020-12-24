871798839eda00e99f64e97a89eaa1b9d9100af1
wadaniel <wadaniel@ethz.ch>
2020-12-24 10:09:30 +0100

    plotter mocmaes

--- source/modules/solver/optimizer/MOCMAES/MOCMAES.py
+++ source/modules/solver/optimizer/MOCMAES/MOCMAES.py
@@ -7,94 +7,36 @@ from korali.plotter.helpers import hlsColors, drawMulticoloredLine
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-
-# Plot histogram of sampes in diagonal
-def plot_histogram(ax, theta):
-  dim = theta.shape[1]
-  num_bins = 50
-
-  for i in range(dim):
-
-    if (dim == 1):
-      ax_loc = ax
-    else:
-      ax_loc = ax[i, i]
-
-    hist, bins, _ = ax_loc.hist(
-        theta[:, i], num_bins, density=True, color='lightgreen', ec='black')
-
-    if i == 0:
-
-      # Rescale hist to scale of theta -> get correct axis titles
-      widths = np.diff(bins)
-      if (dim > 1):
-        hist = hist / np.max(hist) * (
-            ax_loc.get_xlim()[1] - ax_loc.get_xlim()[0])
-        bottom = ax_loc.get_xlim()[0]
-        ax_loc.cla()
-        ax_loc.bar(
-            bins[:-1],
-            hist,
-            widths,
-            color='lightgreen',
-            ec='black',
-            bottom=bottom)
-        ax_loc.set_ylim(ax_loc.get_xlim())
-        ax_loc.set_xticklabels([])
-      else:
-        ax_loc.cla()
-        ax_loc.bar(bins[:-1], hist, widths, color='lightgreen', ec='black')
-
-    elif i == theta.shape[1] - 1:
-      ax_loc.set_yticklabels([])
-
-    else:
-      ax_loc.set_xticklabels([])
-      ax_loc.set_yticklabels([])
-    ax_loc.tick_params(axis='both', which='both', length=0)
-
+plotSamples = True
 
 #Plot scatter plot in upper triangle of figure
-def plot_upper_triangle(ax, theta, lik=None):
+def plot_upper_triangle(ax, theta, f=None):
   dim = theta.shape[1]
-  if (dim == 1):
-    return
-
   for i in range(dim):
     for j in range(i + 1, dim):
-      if lik:
+      if f:
         ax[i, j].scatter(
-            theta[:, j], theta[:, i], marker='o', s=3, alpha=0.5, c=lik)
+            theta[:, i], theta[:, j], marker='o', s=3, alpha=0.5, c=f)
       else:
-        ax[i, j].plot(theta[:, j], theta[:, i], '.', markersize=3)
-      ax[i, j].set_xticklabels([])
-      ax[i, j].set_yticklabels([])
+        ax[i, j].plot(theta[:, i], theta[:, j], '.', markersize=3)
       ax[i, j].grid(b=True, which='both')
+      ax[i, j].set_xlabel("F"+str(i))
+      ax[i, j].set_ylabel("F"+str(j))
 
 
-#Plot 2d histogram in lower triangle of figure
-def plot_lower_triangle(ax, theta):
+#Plot scatter plot in lower triangle of figure
+def plot_lower_triangle(ax, theta, f=None):
   dim = theta.shape[1]
-  if (dim == 1):
-    return
-
   for i in range(dim):
-    for j in range(i):
-      # returns bin values, bin edges and bin edges
-      H, xe, ye = np.histogram2d(theta[:, j], theta[:, i], 10, density=True)
-      # plot and interpolate data
-      ax[i, j].imshow(
-          H.T,
-          aspect="auto",
-          interpolation='spline16',
-          origin='lower',
-          extent=np.hstack((ax[j, j].get_xlim(), ax[i, i].get_xlim())),
-          cmap=plt.get_cmap('jet'))
-
-      if i < theta.shape[1] - 1:
-        ax[i, j].set_xticklabels([])
-      if j > 0:
-        ax[i, j].set_yticklabels([])
+    for j in range(0, i):
+      if f:
+        ax[i, j].scatter(
+            theta[:, i], theta[:, j], marker='o', s=3, alpha=0.5, c=f)
+      else:
+        ax[i, j].plot(theta[:, i], theta[:, j], '.', markersize=3)
+      ax[i, j].grid(b=True, which='both')
+      ax[i, j].set_xlabel("F"+str(i))
+      ax[i, j].set_ylabel("F"+str(j))
 
 
 def plotGen(genList, idx):
@@ -105,34 +47,32 @@ def plotGen(genList, idx):
     if genList[i]['Current Generation'] > lastGen:
       lastGen = genList[i]['Current Generation']
 
-  numdim = len(genList[lastGen]['Variables'])
-  samples = np.array(genList[lastGen]['Solver']['Sample Collection'])
+  numdim = genList[lastGen]['Solver']['Num Objectives']
+  sampleVals = np.array(genList[lastGen]['Solver']['Sample Value Collection'])
 
-  isFinite = [~np.isnan(s - s).any() for s in samples]  # Filter trick
-  samples = samples[isFinite]
+  isFinite = [~np.isnan(s - s).any() for s in sampleVals]  # Filter trick
+  sampleVals = sampleVals[isFinite]
 
-  numentries = len(samples)
+  numentries = len(sampleVals)
 
   fig, ax = plt.subplots(numdim, numdim, figsize=(8, 8))
-  samplesTmp = np.reshape(samples, (numentries, numdim))
+  samplesTmp = np.reshape(sampleVals, (numentries, numdim))
   plt.suptitle(
       'MO-CMA-ES Plotter - \nNumber of Samples {0}'.format(str(numentries)),
       fontweight='bold',
       fontsize=12)
-  #plot_histogram(ax, samplesTmp)
-  plot_upper_triangle(ax, samplesTmp)
-  #plot_lower_triangle(ax, samplesTmp)
-  
-  if numdim > 1:
-    for i in range(numdim):
-      ax[i, 0].set_ylabel(genList[idx]['Variables'][i]['Name'])
-      ax[-1, i].set_xlabel(genList[idx]['Variables'][i]['Name'])
-  else:
-    ax.set_ylabel(genList[idx]['Variables'][0]['Name'])
-    ax.set_xlabel(genList[idx]['Variables'][0]['Name'])
+
+  if plotSamples and numdim > 1:
+      plot_upper_triangle(ax, samplesTmp)
+      plot_lower_triangle(ax, samplesTmp)
+    
+      for i in range(numdim):
+        ax[i, i].set_xticklabels([])
+        ax[i, i].set_yticklabels([])
 
 
-def plot(genList, args):
+def plot(genList, args, addons):
+  print(addons)
   numgens = len(genList)
 
   plotAll = args.all

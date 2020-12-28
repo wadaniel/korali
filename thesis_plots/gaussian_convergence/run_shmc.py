e869e626fac6c29c6ae1efcab89b37f2ac5faff5
Isidor Schoch <isschoch@ethz.ch>
2020-12-28 13:09:11 +0100

    New plotting etc

--- thesis_plots/gaussian_convergence/run_shmc.py
+++ thesis_plots/gaussian_convergence/run_shmc.py
@@ -7,28 +7,35 @@ import numpy as np
 from scipy.stats import t
 from models import * 
 
-distr = "Laplace"
+distr = "Gaussian"
 mu = 0.0
 sigma = 1.0
 y_max = 1.0
+x_range = 1.0
 if distr == "Gaussian":
     model = lambda x: lGaussianModel(x, mu, sigma)
-    pdf = lambda x: gaussianDensity(x, mu, sigma)  
+    pdf = lambda x: gaussianDensity(x, mu, sigma)
+    x_range = 5.0 * sigma  
     y_max = 0.5 / sigma
 elif distr == "Laplace":
     model = lambda x: lLaplaceModel(x, mu, sigma)
     pdf = lambda x: laplaceDensity(x, mu, sigma)
+    x_range = 5.0 * sigma
     y_max = 0.8 / sigma
+elif distr == "Uniform":
+    model = lambda x: lUniformModel(x, mu, sigma)
+    pdf = lambda x: uniformDensity(x, mu, sigma) 
+    x_range = 0.8 * np.ceil(10 * ((np.sqrt(12) * sigma) + 0.05)) / 10.0
+    y_max = np.ceil(10 * (1.0 / (np.sqrt(12) * sigma) + 0.05)) / 10.0
 
 dim = 1
-numSamples = 5000
-numExperiments = 30
+numSamples = 10000
+numExperiments = 1
 samples = np.zeros(shape=(numExperiments, numSamples))
 for experiment in range(numExperiments):
     # Set Random Seed
     e = korali.Experiment()
     e["Random Seed"] = 0xC0FFEE + experiment
-
     #######################################################################################
     ############################# File/Console output settings ############################
     #######################################################################################
@@ -56,7 +63,7 @@ for experiment in range(numExperiments):
     #######################################################################################
     ############################# Number of samples settings ##############################
     #######################################################################################
-    e["Solver"]["Burn In"] = 200
+    e["Solver"]["Burn In"] = 1000
     e["Solver"]["Termination Criteria"]["Max Samples"] = numSamples
     e["Solver"]["Initial Fast Adaption Interval"] = 75
     e["Solver"]["Final Fast Adaption Interval"] = 50
@@ -66,7 +73,7 @@ for experiment in range(numExperiments):
     ############################# Integration length settings #############################
     #######################################################################################
     # General Step Size settings
-    e["Solver"]["Step Size"] = 0.05
+    e["Solver"]["Step Size"] = 0.10
     e["Solver"]["Step Size Jitter"] = 0.0
     e["Solver"]["Num Integration Steps"] = 3
     # Adaptive Step Size settings
@@ -77,8 +84,8 @@ for experiment in range(numExperiments):
     e["Solver"]["Adaptive Step Size Stabilization Constant"] = 10.0 #for Use Adaptive Step Size = True
     e["Solver"]["Adaptive Step Size Schedule Constant"] = 0.75 #for Use Adaptive Step Size = True
     # NUTS settings
-    e["Solver"]["Use NUTS"] = False
-    e["Solver"]["Max Depth"] = 10 #for Use NUTS = True
+    e["Solver"]["Use NUTS"] = True
+    e["Solver"]["Max Depth"] = 4 #for Use NUTS = True
     # Implicit leapfrog settings
     e["Solver"]["Max Integration Steps"] = 100 #for Version = Riemannian
     
@@ -97,12 +104,13 @@ for experiment in range(numExperiments):
 #######################################################################################
 
 # Plot a single histogram #############################################################
+fig, ax = plt.subplots()
+ax.set_axisbelow(True)
+
 suffixStr = distr+"_experiments_"+str(numExperiments)+"_samples_"+str(numSamples)
-x_range = 5.0
-plt.grid(linestyle=":")
-plt.xlim(-x_range*sigma, x_range*sigma)
+plt.xlim(mu-x_range, mu+x_range)
 plt.ylim(0.0, y_max)
-plt.hist(samples[0, :], bins=50, density=True)
+ax.hist(samples[0, :], bins=50, density=True, zorder=1)
 
 numPoints = 1000
 xVals = np.linspace(-5.0 * sigma, 5.0 * sigma, numPoints)
@@ -111,11 +119,13 @@ stdNormalDistrDensity = np.array([pdf(x) for x in xVals])
 plt.xlabel("Position $q$", fontsize=16)
 plt.ylabel("Rel. Occurrence", fontsize=16)
 plt.tick_params(axis='both', labelsize=14)
-plt.plot(xVals, stdNormalDistrDensity, 'r-')
-plt.legend(["PDF", "Samples"], prop={"size": 14})
+ax.plot(xVals, stdNormalDistrDensity, 'r-')
 
-plt.savefig("samples_"+suffixStr+".svg", bbox_inches="tight")
-plt.savefig("samples_"+suffixStr, dpi=200, bbox_inches="tight")
+
+plt.grid(linestyle=":", zorder=-1)
+plt.legend(["PDF", "Samples"], prop={"size": 14})
+plt.savefig("./shmc/samples_"+suffixStr+".svg", bbox_inches="tight")
+plt.savefig("./shmc/samples_"+suffixStr, dpi=200, bbox_inches="tight")
 
 plt.clf()
 
@@ -133,47 +143,25 @@ averageErrorExpectation = np.mean(errorExpectation, axis=0)
 print("errorExpectation = ", errorExpectation)
 errorIdeal = np.array([1.0/np.sqrt(n) for n in numSamplesRange])
 
-# Least Squares fit
-# A = np.zeros(shape=(numExperiments*numSamples, 2))
-# y = np.zeros(shape=(numExperiments*numSamples, 1))
-# for i in range(numExperiments):
-#     A[i*numSamples, 0] = 1.0
-#     for j in range(numSamples):
-#         A[i*numSamples + j, 1] = np.log10(j+1)
-#         y[i*numSamples + j] = np.log10(errorExpectation[i, j])
-
-# M = np.matmul(np.transpose(A), A)
-# ATy = np.matmul(np.transpose(A), y)
-# print("A = ", A)
-# print("y = ", y)
-# print("M = ", M)
-# print("ATy =", ATy)
-# x = np.linalg.solve(M, ATy)
-# print("x = ", x)
-
-# LSQFit = np.array([(10**x[0] * n**x[1]) for n in numSamplesRange])
-
 # Calculate confidence interval
 alpha = 0.05
 confidenceValue = 1.0 - alpha / 2.0
 studentTCoeff = t.ppf(confidenceValue, df=numExperiments-1)
 ci = studentTCoeff * np.std(errorExpectation, axis=0) / np.sqrt(numExperiments)
 
-# for experiment in range(numExperiments):
-#     plt.loglog(numSamplesRange, errorExpectation[experiment, :])
-# plt.loglog(numSamplesRange, averageErrorExpectation)
-
 fig, ax = plt.subplots()
+plt.xlim(numSamplesRange[0], numSamplesRange[-1])
+plt.ylim(1.0/5.0 * errorIdeal[-1], 5*errorIdeal[0])
+ax.loglog(numSamplesRange, errorExpectation[0, :], 'silver', linewidth=0.3, zorder=-1)
 ax.loglog(numSamplesRange, errorIdeal, 'g--')
 # ax.loglog(numSamplesRange, LSQFit, 'r--')
-ax.loglog(numSamplesRange, errorExpectation[0, :], 'r-')
 ax.loglog(numSamplesRange, averageErrorExpectation)
 print("averageErrorExpectation = ", averageErrorExpectation)
 ax.fill_between(numSamplesRange, (averageErrorExpectation-ci), (averageErrorExpectation+ci), color='b', alpha=.1)
 ax.tick_params(axis='both', labelsize=14)
-plt.grid(linestyle=":")
+plt.grid(linestyle=":", zorder=-2)
 plt.xlabel("Number of Samples $N$", fontsize=16)
 plt.ylabel("Error $|\\mathbb{E}[Q]-\hat{E}[Q]|$", fontsize=16)
-plt.legend(["$N^{-\\frac{1}{2}}$", "Single Run", "$\hat{E}[Error]$", str((100 * (1.0 - alpha))) + "% CI"], loc="lower left", prop={"size": 14})
-plt.savefig("convergence_"+suffixStr+".svg", bbox_inches="tight")
-plt.savefig("convergence_"+suffixStr+".png", dpi=200, bbox_inches="tight")
+plt.legend(["Single Run", "$N^{-\\frac{1}{2}}$", "$\hat{E}[Error]$" ,str((100 * (1.0 - alpha))) + "% CI"], loc="lower left", prop={"size": 14})
+plt.savefig("./shmc/convergence_"+suffixStr+".svg", bbox_inches="tight")
+plt.savefig("./shmc/convergence_"+suffixStr+".png", dpi=200, bbox_inches="tight")

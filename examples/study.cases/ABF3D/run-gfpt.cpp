f8c593d488d55eecf877f2ae1f8b74faea9ce327
Sergio Martin <martiser@ethz.ch>
2020-12-31 16:17:47 +0100

    Improvements to GFPT

--- examples/study.cases/ABF3D/run-gfpt.cpp
+++ examples/study.cases/ABF3D/run-gfpt.cpp
@@ -53,32 +53,32 @@ int main(int argc, char *argv[])
   e["Variables"][14]["Lower Bound"] = lowerBounds[0];
   e["Variables"][14]["Upper Bound"] = upperBounds[0];
   e["Variables"][14]["Exploration Sigma"]["Initial"] = (upperBounds[0] - lowerBounds[0]) * 0.20;
-  e["Variables"][14]["Exploration Sigma"]["Final"] = (upperBounds[0] - lowerBounds[0]) * 0.02;
-  e["Variables"][14]["Exploration Sigma"]["Annealing Rate"] = 1e-5;
+  e["Variables"][14]["Exploration Sigma"]["Final"] = (upperBounds[0] - lowerBounds[0]) * 0.04;
+  e["Variables"][14]["Exploration Sigma"]["Annealing Rate"] = 5e-6;
 
   e["Variables"][15]["Name"] = "Rotation X";
   e["Variables"][15]["Type"] = "Action";
   e["Variables"][15]["Lower Bound"] = lowerBounds[1];
   e["Variables"][15]["Upper Bound"] = upperBounds[1];
   e["Variables"][15]["Exploration Sigma"]["Initial"] = (upperBounds[1] - lowerBounds[1]) * 0.20;
-  e["Variables"][15]["Exploration Sigma"]["Final"] = (upperBounds[1] - lowerBounds[1]) * 0.02;
-  e["Variables"][15]["Exploration Sigma"]["Annealing Rate"] = 1e-5;
+  e["Variables"][15]["Exploration Sigma"]["Final"] = (upperBounds[1] - lowerBounds[1]) * 0.04;
+  e["Variables"][15]["Exploration Sigma"]["Annealing Rate"] = 5e-6;
 
   e["Variables"][16]["Name"] = "Rotation Y";
   e["Variables"][16]["Type"] = "Action";
   e["Variables"][16]["Lower Bound"] = lowerBounds[2];
   e["Variables"][16]["Upper Bound"] = upperBounds[2];
   e["Variables"][16]["Exploration Sigma"]["Initial"] = (upperBounds[2] - lowerBounds[2]) * 0.20;
-  e["Variables"][16]["Exploration Sigma"]["Final"] = (upperBounds[2] - lowerBounds[2]) * 0.02;
-  e["Variables"][16]["Exploration Sigma"]["Annealing Rate"] = 1e-5;
+  e["Variables"][16]["Exploration Sigma"]["Final"] = (upperBounds[2] - lowerBounds[2]) * 0.04;
+  e["Variables"][16]["Exploration Sigma"]["Annealing Rate"] = 5e-6;
 
   e["Variables"][17]["Name"] = "Rotation Z";
   e["Variables"][17]["Type"] = "Action";
   e["Variables"][17]["Lower Bound"] = lowerBounds[3];
   e["Variables"][17]["Upper Bound"] = upperBounds[3];
   e["Variables"][17]["Exploration Sigma"]["Initial"] = (upperBounds[3] - lowerBounds[3]) * 0.20;
-  e["Variables"][17]["Exploration Sigma"]["Final"] = (upperBounds[3] - lowerBounds[3]) * 0.02;
-  e["Variables"][17]["Exploration Sigma"]["Annealing Rate"] = 1e-5;
+  e["Variables"][17]["Exploration Sigma"]["Final"] = (upperBounds[3] - lowerBounds[3]) * 0.04;
+  e["Variables"][17]["Exploration Sigma"]["Annealing Rate"] = 5e-6;
 
   /// Defining Agent Configuration
 
@@ -87,12 +87,12 @@ int main(int argc, char *argv[])
   e["Solver"]["Experiences Per Generation"] = 243;
   e["Solver"]["Experiences Between Policy Updates"] = 10;
   e["Solver"]["Cache Persistence"] = 200;
-  e["Solver"]["Learning Rate"] = 0.001;
+  e["Solver"]["Learning Rate"] = 0.0001;
 
   /// Defining the configuration of replay memory
 
-  e["Solver"]["Experience Replay"]["Start Size"] = 4096;
-  e["Solver"]["Experience Replay"]["Maximum Size"] = 65536;
+  e["Solver"]["Experience Replay"]["Start Size"] = 16384;
+  e["Solver"]["Experience Replay"]["Maximum Size"] = 131072;
   e["Solver"]["Experience Replay"]["Serialization Frequency"] = 100;
 
   /// Configuring the Remember-and-Forget Experience Replay algorithm
@@ -105,13 +105,13 @@ int main(int argc, char *argv[])
 
   /// Configuring Mini Batch
 
-  e["Solver"]["Mini Batch Size"] = 128;
+  e["Solver"]["Mini Batch Size"] = 256;
   e["Solver"]["Mini Batch Strategy"] = "Uniform";
 
   /// Defining Critic and Policy Configuration
 
   e["Solver"]["Critic"]["Advantage Function Population"] = 16;
-  e["Solver"]["Policy"]["Learning Rate Scale"] = 0.1;
+  e["Solver"]["Policy"]["Learning Rate Scale"] = 1.0;
   e["Solver"]["Policy"]["Target Accuracy"] = 0.01;
   e["Solver"]["Policy"]["Optimization Candidates"] = 32;
 
@@ -120,13 +120,13 @@ int main(int argc, char *argv[])
   e["Solver"]["Neural Network"]["Engine"] = "OneDNN";
 
   e["Solver"]["Neural Network"]["Hidden Layers"][0]["Type"] = "Layer/Linear";
-  e["Solver"]["Neural Network"]["Hidden Layers"][0]["Output Channels"] = 64;
+  e["Solver"]["Neural Network"]["Hidden Layers"][0]["Output Channels"] = 128;
 
   e["Solver"]["Neural Network"]["Hidden Layers"][1]["Type"] = "Layer/Activation";
   e["Solver"]["Neural Network"]["Hidden Layers"][1]["Function"] = "Elementwise/Tanh";
 
   e["Solver"]["Neural Network"]["Hidden Layers"][2]["Type"] = "Layer/Linear";
-  e["Solver"]["Neural Network"]["Hidden Layers"][2]["Output Channels"] = 64;
+  e["Solver"]["Neural Network"]["Hidden Layers"][2]["Output Channels"] = 128;
 
   e["Solver"]["Neural Network"]["Hidden Layers"][3]["Type"] = "Layer/Activation";
   e["Solver"]["Neural Network"]["Hidden Layers"][3]["Function"] = "Elementwise/Tanh";

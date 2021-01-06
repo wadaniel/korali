3dc7ff5b9a9e497089ecd4cfd5720542fd398c0d
wadaniel <wadaniel@ethz.ch>
2021-01-06 11:45:16 +0100

    added naf script

--- examples/study.cases/CMAIRL/_optimization_model/objfunc.py
+++ examples/study.cases/CMAIRL/_optimization_model/objfunc.py
@@ -24,7 +24,7 @@ def rl_cartpole_vracer(p):
     e["Problem"]["Type"] = "Reinforcement Learning / Continuous"
     e["Problem"]["Environment Function"] = envp
     e["Problem"]["Testing Frequency"] = 500
-    e["Problem"]["Training Reward Threshold"] = 1e12
+    e["Problem"]["Training Reward Threshold"] = 450
     e["Problem"]["Policy Testing Episodes"] = 20
     e["Problem"]["Actions Between Policy Updates"] = 5
     e["Problem"]["Custom Settings"]["Record Observations"] = "False"
@@ -88,8 +88,8 @@ def rl_cartpole_vracer(p):
 
     ### Defining Termination Criteria
 
-    e["Solver"]["Termination Criteria"]["Testing"]["Target Average Reward"] = 1e12
-    e["Solver"]["Termination Criteria"]["Max Generations"] = 10000
+    e["Solver"]["Termination Criteria"]["Testing"]["Target Average Reward"] = 450
+    e["Solver"]["Termination Criteria"]["Max Generations"] = 7000
 
     ## Setting file output configuration
 
@@ -104,13 +104,118 @@ def rl_cartpole_vracer(p):
     
     e["Solver"]["Mode"] = "Testing"
     e["Solver"]["Testing"]["Sample Ids"] = [0]
+    e["Solver"]["Discount Factor"] = 1.0
     e["Problem"]["Policy Testing Episodes"] = 1
     e["Problem"]["Environment Function"] = evalenv
 
     k.run(e)
 
+    suml2error = e["Solver"]["Testing"]["Average Reward"]
+    
     print("[Korali] Finished testing (p {0} error {1}.".format(target, suml2error))
+   
+    p["F(x)"] = -suml2error # maximize
+
+
+# rl cartpole with unknown reward
+def rl_cartpole_naf(p):
+
+    k = korali.Engine()
+    e = korali.Experiment()
+
+    # environment with parametrized reward function
+    target = p["Parameters"][0]
+    envp = lambda s : env(s,target)
+
+    ### Defining the Cartpole problem's configuration
+
+    e["Problem"]["Type"] = "Reinforcement Learning / Continuous"
+    e["Problem"]["Environment Function"] = envp
+    e["Problem"]["Testing Frequency"] = 500
+    e["Problem"]["Training Reward Threshold"] = 450
+    e["Problem"]["Policy Testing Episodes"] = 20
+    e["Problem"]["Actions Between Policy Updates"] = 5
+    e["Problem"]["Custom Settings"]["Record Observations"] = "False"
+
+    e["Variables"][0]["Name"] = "Cart Position"
+    e["Variables"][0]["Type"] = "State"
+
+    e["Variables"][1]["Name"] = "Cart Velocity"
+    e["Variables"][1]["Type"] = "State"
+
+    e["Variables"][2]["Name"] = "Pole Angle"
+    e["Variables"][2]["Type"] = "State"
+
+    e["Variables"][3]["Name"] = "Pole Angular Velocity"
+    e["Variables"][3]["Type"] = "State"
+
+    e["Variables"][4]["Name"] = "Force"
+    e["Variables"][4]["Type"] = "Action"
+    e["Variables"][4]["Lower Bound"] = -10.0
+    e["Variables"][4]["Upper Bound"] = +10.0
+
+    ## Defining Agent Configuration 
+    
+    e["Solver"]["Type"] = "Agent / Continuous / NAF"
+    e["Solver"]["Mode"] = "Training"
+    e["Solver"]["Target Learning Rate"] = 0.01
+    e["Solver"]["Experiences Between Policy Updates"] = 5
+    e["Solver"]["Covariance Scaling"] = 0.01
+    e["Solver"]["Mini Batch Strategy"] = "Prioritized"
+
+    ### Defining the configuration of replay memory
+
+    e["Solver"]["Experience Replay"]["Start Size"] =   2048
+    e["Solver"]["Experience Replay"]["Maximum Size"] = 32768
+
+    ## Defining Neural Network Configuration for Policy and Critic into Critic Container
+
+    e["Solver"]["Discount Factor"] = 0.99
+    e["Solver"]["Learning Rate"] = 1e-4
+    e["Solver"]["Mini Batch Size"] = 32
+
+    ### Configuring the neural network and its hidden layers
+
+    e["Solver"]["Neural Network"]["Engine"] = "OneDNN"
+
+    e["Solver"]["Neural Network"]["Hidden Layers"][0]["Type"] = "Layer/Linear"
+    e["Solver"]["Neural Network"]["Hidden Layers"][0]["Output Channels"] = 32
+
+    e["Solver"]["Neural Network"]["Hidden Layers"][1]["Type"] = "Layer/Activation"
+    e["Solver"]["Neural Network"]["Hidden Layers"][1]["Function"] = "Elementwise/Tanh"
+
+    e["Solver"]["Neural Network"]["Hidden Layers"][2]["Type"] = "Layer/Linear"
+    e["Solver"]["Neural Network"]["Hidden Layers"][2]["Output Channels"] = 32
+
+    e["Solver"]["Neural Network"]["Hidden Layers"][3]["Type"] = "Layer/Activation"
+    e["Solver"]["Neural Network"]["Hidden Layers"][3]["Function"] = "Elementwise/Tanh"
+
+    ### Defining Termination Criteria
+
+    e["Solver"]["Termination Criteria"]["Testing"]["Target Average Reward"] = 490
+    e["Solver"]["Termination Criteria"]["Max Generations"] = 5000
+
+    ## Setting file output configuration
+
+    e["File Output"]["Enabled"] = False
+    e["Console Output"]["Verbosity"] = "Silent"
+
+    ### Running Experiment
+    
+    k.run(e)
+
+    ### Evaluate Policy
+    
+    e["Solver"]["Mode"] = "Testing"
+    e["Solver"]["Testing"]["Sample Ids"] = [0]
+    e["Solver"]["Discount Factor"] = 1.0
+    e["Problem"]["Policy Testing Episodes"] = 1
+    e["Problem"]["Environment Function"] = evalenv
+
+    k.run(e)
 
     suml2error = e["Solver"]["Testing"]["Average Reward"]
+    
+    print("[Korali] Finished testing (p {0} error {1}.".format(target, suml2error))
    
     p["F(x)"] = -suml2error # maximize
---
+++ examples/study.cases/CMAIRL/run-cmaes-naf.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+## In this example, we demonstrate how Korali finds values for the
+## variables that maximize the objective function, given by a
+## user-provided computational model.
+
+# Importing computational model
+import sys
+sys.path.append('./_optimization_model')
+from objfunc import *
+
+# Starting Korali's Engine
+import korali
+k = korali.Engine()
+
+# Creating new experiment
+e = korali.Experiment()
+
+# Configuring Problem
+e["Random Seed"] = 0xC0FEE
+e["Problem"]["Type"] = "Optimization"
+e["Problem"]["Objective Function"] = rl_cartpole_naf
+
+dim = 1
+
+# Defining the problem's variables.
+for i in range(dim):
+    e["Variables"][i]["Name"] = "X" + str(i)
+    e["Variables"][i]["Lower Bound"] = -1.0 # -57 degrees
+    e["Variables"][i]["Upper Bound"] = +1.0 # +57 degrees
+
+# Configuring CMA-ES parameters
+e["Solver"]["Type"] = "Optimizer/CMAES"
+e["Solver"]["Population Size"] = 8
+e["Solver"]["Termination Criteria"]["Min Value Difference Threshold"] = 0.1
+e["Solver"]["Termination Criteria"]["Max Generations"] = 500
+
+# Configuring results path
+e["File Output"]["Path"] = '_korali_result_cmaes'
+e["File Output"]["Enabled"] = True
+e["File Output"]["Frequency"] = 1
+
+e["Console Output"]["Verbosity"] = "Detailed"
+
+k["Conduit"]["Type"] = "Concurrent"
+k["Conduit"]["Concurrent Jobs"] = 8
+
+# Running Korali
+k.run(e)

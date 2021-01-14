b6b2bf610da863cbf46f91dc0c8e61ce00b512fd
Pascal Weber <webepasc@student.ethz.ch>
2021-01-14 09:45:12 +0100

    updated config

--- examples/study.cases/fish/run.sh
+++ examples/study.cases/fish/run.sh
@@ -5,9 +5,9 @@ mpiflags="mpirun -n 2"
 
 if [ ! -z $SLURM_NNODES ]; then
  N=$SLURM_NNODES
- mpiflags="srun -N $N -n $N -c 12"
+ mpiflags="srun -N 2 -n 2 -c 12"
 fi
 
 set -x
 
-$mpiflags ./run-vracer -poissonType cosine -muteAll 1 -tdump 0 -nu 0.000018 -tend 0 -shapes 'halfDisk_radius=.06_angle=20_xpos=.1_bForced=1_bFixed=1_xvel=0.15_tAccel=5,stefanfish_L=.2_xpos=.4_bFixedy=1' -bpdx 8 -bpdy 4 -levelMax 4 -Rtol 0.5 -Ctol 0.1
+$mpiflags ./run-vracer -bpdx 8 -bpdy 4 -poissonType cosine -muteAll 1 -tdump 0 -nu 0.000018 -tend 0 -poissonType cosine -shapes 'halfDisk_radius=.06_angle=20_xpos=.1_bForced=1_bFixed=1_xvel=0.15_tAccel=5,stefanfish_L=.2_xpos=.4_bFixedy=1' 

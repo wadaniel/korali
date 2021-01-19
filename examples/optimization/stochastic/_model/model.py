a33991891bf9906a5e9ee38eefb444c04cb36f11
wadaniel <wadaniel@ethz.ch>
2021-01-19 14:50:22 +0100

    examples

--- examples/optimization/stochastic/_model/model.py
+++ examples/optimization/stochastic/_model/model.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import numpy as np
 
 # 1-d problem
 def model(p):
@@ -30,4 +31,32 @@ def negative_rosenbrock(p):
         grad[i+1] -= 200.*(x[i+1]-x[i])
 
     p["F(x)"] = -res
+    p["Gradient"] = grad
+
+# multi dimensional problem (ackley)
+def negative_ackley(p):
+    x = p["Parameters"]
+    a = 20.
+    b = 0.2
+    c = 2.*np.pi
+    dim = len(x)
+
+    sum1 = 0.
+    sum2 = 0.
+    for i in range(dim):
+        sum1 += x[i]*x[i]
+        sum2 += np.cos(c*x[i])
+
+    sum1 /= dim
+    sum2 /= dim
+    r1 = a*np.exp(-b*np.sqrt(sum1))
+    r2 = np.exp(sum2)
+
+    p["F(x)"] = r1 + r2 - a - np.exp(1)
+    
+    grad = [0.]*dim
+    for i in range(dim):
+      grad[i] = r1*-1*b*0.5/np.sqrt(sum1)*1.0/dim*2.0*x[i]
+      grad[i] -= r2*1.0/dim*np.sin(c*x[i])*c
+
     p["Gradient"] = grad

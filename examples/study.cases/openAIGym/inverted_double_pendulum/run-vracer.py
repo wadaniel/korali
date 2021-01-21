53aa5bd329edf4059a9253c712c95bf8a2b4e4d7
Sergio Martin <martiser@ethz.ch>
2021-01-21 16:17:35 +0100

    Organizing folder and creating vracer eaxmple

--- examples/study.cases/openAIGym/inverted_double_pendulum/run-vracer.py
+++ examples/study.cases/openAIGym/inverted_double_pendulum/run-vracer.py
@@ -56,49 +56,62 @@ e["Variables"][11]["Type"] = "Action"
 e["Variables"][11]["Lower Bound"] = -1.0
 e["Variables"][11]["Upper Bound"] = +1.0
 
-### Configuring NAF hyperparameters
+### Defining Agent Configuration 
 
-e["Solver"]["Type"] = "Agent / Continuous / NAF"
+e["Solver"]["Type"] = "Agent / Discrete / DVRACER"
 e["Solver"]["Mode"] = "Training"
-e["Solver"]["Target Learning Rate"] = 0.0001
-e["Solver"]["Experiences Between Policy Updates"] = 5
-e["Solver"]["Mini Batch Strategy"] = "Prioritized"
+e["Solver"]["Experiences Between Policy Updates"] = 1
+e["Solver"]["Cache Persistence"] = 250
+e["Solver"]["Episodes Per Generation"] = 1
 
-### Defining Experience Replay configuration
+### Defining the configuration of replay memory
 
-e["Solver"]["Experience Replay"]["Start Size"] =   1000
-e["Solver"]["Experience Replay"]["Maximum Size"] = 10000
+e["Solver"]["Experience Replay"]["Start Size"] = 4096
+e["Solver"]["Experience Replay"]["Maximum Size"] = 65536
 
-## Defining Critic Info
+### Configuring the Remember-and-Forget Experience Replay algorithm
+
+e["Solver"]["Experience Replay"]["REFER"]["Enabled"] = True
+e["Solver"]["Experience Replay"]["REFER"]["Cutoff Scale"] = 4.0
+e["Solver"]["Experience Replay"]["REFER"]["Target"] = 0.1
+e["Solver"]["Experience Replay"]["REFER"]["Initial Beta"] = 0.6
+e["Solver"]["Experience Replay"]["REFER"]["Annealing Rate"] = 5e-7
+
+### Defining probability of taking a random action (epsilon)
 
 e["Solver"]["Discount Factor"] = 0.99
 e["Solver"]["Learning Rate"] = 1e-4
-e["Solver"]["Mini Batch Size"] = 32
+e["Solver"]["Mini Batch Size"] = 256
 
 ### Configuring the neural network and its hidden layers
 
 e["Solver"]["Neural Network"]["Engine"] = "OneDNN"
 
 e["Solver"]["Neural Network"]["Hidden Layers"][0]["Type"] = "Layer/Linear"
-e["Solver"]["Neural Network"]["Hidden Layers"][0]["Output Channels"] = 32
+e["Solver"]["Neural Network"]["Hidden Layers"][0]["Output Channels"] = 128
 
 e["Solver"]["Neural Network"]["Hidden Layers"][1]["Type"] = "Layer/Activation"
 e["Solver"]["Neural Network"]["Hidden Layers"][1]["Function"] = "Elementwise/Tanh"
 
 e["Solver"]["Neural Network"]["Hidden Layers"][2]["Type"] = "Layer/Linear"
-e["Solver"]["Neural Network"]["Hidden Layers"][2]["Output Channels"] = 32
+e["Solver"]["Neural Network"]["Hidden Layers"][2]["Output Channels"] = 128
 
 e["Solver"]["Neural Network"]["Hidden Layers"][3]["Type"] = "Layer/Activation"
 e["Solver"]["Neural Network"]["Hidden Layers"][3]["Function"] = "Elementwise/Tanh"
 
 ### Defining Termination Criteria
 
-e["Solver"]["Termination Criteria"]["Target Average Testing Reward"] = 900
+e["Solver"]["Termination Criteria"]["Testing"]["Target Average Reward"] = 750
 
 ### Setting file output configuration
 
-e["File Output"]["Enabled"] = False
+e["Solver"]["Experience Replay"]["Serialize"] = True
+e["Console Output"]["Verbosity"] = "Detailed"
+e["File Output"]["Enabled"] = True
+e["File Output"]["Frequency"] = 10
+e["File Output"]["Path"] = "_result_dvracer"
 
 ### Running Experiment
 
 k.run(e)
+

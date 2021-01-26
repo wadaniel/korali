c98993b176d9d81c8aa19eca7cd5721310240389
G. Arampatzis <gtarabat@gmail.com>
2021-01-26 14:06:38 +0100

    python files in one place

--- python/plotter/__main__.py
+++ python/plotter/__main__.py
@@ -65,42 +65,42 @@ def main(path, check, test, output, args):
 
   del genList[0]
 
-  solverName = js['Solver']['Type'].lower() 
+  solverName = js['Solver']['Type'].lower()
   solverDir = ""
   moduleName = ""
-  
+
   if ("cmaes" in solverName):
-   solverDir = curdir + '/../modules/solver/optimizer/CMAES'
+   solverDir = curdir + '/CMAES'
    moduleName = 'CMAES'
-   
+
   if ("dea" in solverName):
-   solverDir = curdir + '/../modules/solver/optimizer/DEA'
+   solverDir = curdir + '/DEA'
    moduleName = 'DEA'
 
   if ("lmcmaes" in solverName):
-   solverDir = curdir + '/../modules/solver/optimizer/LMCMAES'
+   solverDir = curdir + '/LMCMAES'
    moduleName = 'LMCMAES'
-   
+
   if ("mocmaes" in solverName):
-   solverDir = curdir + '/../modules/solver/optimizer/MOCMAES'
+   solverDir = curdir + '/MOCMAES'
    moduleName = 'MOCMAES'
-   
+
   if ("mcmc" in solverName):
-   solverDir = curdir + '/../modules/solver/sampler/MCMC'
+   solverDir = curdir + '/MCMC'
    moduleName = 'MCMC'
-   
+
   if ("nested" in solverName):
-   solverDir = curdir + '/../modules/solver/sampler/Nested'
+   solverDir = curdir + '/Nested'
    moduleName = 'Nested'
-   
+
   if ("tmcmc" in solverName):
-   solverDir = curdir + '/../modules/solver/sampler/TMCMC'
+   solverDir = curdir + '/TMCMC'
    moduleName = 'TMCMC'
 
   if (solverDir == ""):
    print("[Korali] Solver '{0}' does not provide support for plotting.".format(solverName))
    exit(0)
-   
+
   sys.path.append(solverDir)
   solverLib = importlib.import_module(moduleName, package=None)
   solverLib.plot(genList, args)

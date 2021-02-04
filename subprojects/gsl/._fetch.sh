9eff66a582877c2dc624b7c6050f3b6405e27961
Fabian Wermelinger <fabianw@mavt.ethz.ch>
2021-02-04 16:32:48 +0100

    Use curl instead of wget to download data

--- subprojects/gsl/._fetch.sh
+++ subprojects/gsl/._fetch.sh
@@ -5,6 +5,6 @@ tarball="gsl-${VERSION}.tar.gz"
 
 if [[ ! -f ${tarball} ]]; then
     git clean -xdf .
-    wget "ftp://ftp.gnu.org/gnu/gsl/${tarball}"
+    curl -L -o ${tarball} "ftp://ftp.gnu.org/gnu/gsl/${tarball}"
     tar --strip-components=1 -xzvf ${tarball}
 fi

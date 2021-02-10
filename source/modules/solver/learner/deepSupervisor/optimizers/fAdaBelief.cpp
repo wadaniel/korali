8c376503e8e73614ebc5c60e7ee32e6e0c44206d
Sergio Martin <martiser@ethz.ch>
2021-02-10 21:29:19 +0100

    Speeding up adam optimizers

--- source/modules/solver/learner/deepSupervisor/optimizers/fAdaBelief.cpp
+++ source/modules/solver/learner/deepSupervisor/optimizers/fAdaBelief.cpp
@@ -1,13 +1,13 @@
 #include "fAdaBelief.hpp"
 #include <cmath>
 #include <stdio.h>
+#include <cstdlib>
 
 namespace korali
 {
 fAdaBelief::fAdaBelief(size_t nVars) : fAdam(nVars)
 {
   _secondCentralMoment.resize(nVars);
-  _biasCorrectedSecondCentralMoment.resize(nVars);
 
   reset();
 }
@@ -17,49 +17,37 @@ void fAdaBelief::reset()
   fAdam::reset();
 
   for (size_t i = 0; i < _nVars; i++)
-  {
     _secondCentralMoment[i] = 0.0f;
-    _biasCorrectedSecondCentralMoment[i] = 0.0f;
-  }
 }
 
 void fAdaBelief::processResult(float evaluation, std::vector<float> &gradient)
 {
   _modelEvaluationCount++;
-  _currentEvaluation = evaluation;
 
-  _currentEvaluation = -_currentEvaluation; //minimize
-  _gradientNorm = 0.0;
-
-  _gradient = gradient;
-
-  if (_gradient.size() != _nVars)
-    fprintf(stderr, "Size of sample's gradient evaluations vector (%lu) is different from the number of problem variables defined (%lu).\n", _gradient.size(), _nVars);
-
-  for (size_t i = 0; i < _nVars; i++)
+  if (gradient.size() != _nVars)
   {
-    _gradient[i] = -_gradient[i]; // minimize
-    _gradientNorm += _gradient[i] * _gradient[i];
+    fprintf(stderr, "Size of sample's gradient evaluations vector (%lu) is different from the number of problem variables defined (%lu).\n", _gradient.size(), _nVars);
+    std::abort();
   }
-  _gradientNorm = std::sqrt(_gradientNorm);
 
-  if (_currentEvaluation < _bestEvaluation)
-    _bestEvaluation = _currentEvaluation;
+  const float secondCentralMomentFactor = 1.0f / (1.0f - std::pow(_beta2, (float)_modelEvaluationCount));
+  const float firstCentralMomentFactor = 1.0f / (1.0f - std::pow(_beta1, (float)_modelEvaluationCount));
+  const float notBeta1 = 1.0f - _beta1;
+  const float notBeta2 = 1.0f - _beta2;
 
   // update first and second moment estimators and bias corrected versions
   for (size_t i = 0; i < _nVars; i++)
   {
-    _firstMoment[i] = _beta1 * _firstMoment[i] + (1 - _beta1) * _gradient[i];
-    _biasCorrectedFirstMoment[i] = _firstMoment[i] / (1 - std::pow(_beta1, _modelEvaluationCount));
-    _secondCentralMoment[i] = _beta2 * _secondCentralMoment[i] + (1 - _beta2) * (_gradient[i] - _firstMoment[i]) * (_gradient[i] - _firstMoment[i]);
-    _biasCorrectedSecondCentralMoment[i] = _secondCentralMoment[i] / (1 - std::pow(_beta2, _modelEvaluationCount));
+    _firstMoment[i] = _beta1 * _firstMoment[i] + notBeta1 * -gradient[i];
+
+    const float biasCorrectedFirstMoment = _firstMoment[i] * firstCentralMomentFactor;
+    const float secondMomentGradientDiff = -gradient[i] -_firstMoment[i];
+    _secondCentralMoment[i] = _beta2 * _secondCentralMoment[i] + notBeta2 * secondMomentGradientDiff * secondMomentGradientDiff;
+
+    const float biasCorrectedSecondCentralMoment = _secondCentralMoment[i] * secondCentralMomentFactor;
+    _currentValue[i] -= _eta / (std::sqrt(biasCorrectedSecondCentralMoment) + _epsilon) * biasCorrectedFirstMoment;
   }
 
-  // update parameters
-  for (size_t i = 0; i < _nVars; i++)
-  {
-    _currentValue[i] -= _eta / (std::sqrt(_biasCorrectedSecondCentralMoment[i]) + _epsilon) * _biasCorrectedFirstMoment[i];
-  }
 }
 
 } // namespace korali

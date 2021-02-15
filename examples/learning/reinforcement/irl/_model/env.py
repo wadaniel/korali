98b63c7e2fdec6b08ff892a802c739532b79da11
wadaniel <wadaniel@ethz.ch>
2021-02-15 16:54:27 +0100

    irl started

---
+++ examples/learning/reinforcement/irl/_model/env.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+from cartpole import *
+
+######## Defining Environment Storage
+
+cart = CartPole()
+maxSteps = 500
+
+def env(s):
+
+ # Initializing environment
+ cart.reset()
+ s["State"] = cart.getState().tolist()
+ step = 0
+ done = False
+
+ while not done and step < maxSteps:
+
+  # Getting new action
+  s.update()
+  
+  # Performing the action
+  done = cart.advance(s["Action"])
+  #print(s["Action"]) 
+  
+  # Getting Reward
+  s["Reward"] = cart.getReward()
+   
+  # Storing New State
+  s["State"] = cart.getState().tolist()
+  
+  # Advancing step counter
+  step = step + 1
+
+ # Setting finalization status
+ if (cart.isOver()):
+  s["Termination"] = "Terminal"
+ else:
+  s["Termination"] = "Truncated"
---
+++ examples/learning/reinforcement/irl/run-vracer-continuous.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+import os
+import sys
+sys.path.append('./_model')
+from env import *
+
+####### Defining Korali Problem
+
+import korali
+k = korali.Engine()
+e = korali.Experiment()
+
+### Defining the Cartpole problem's configuration
+
+e["Problem"]["Type"] = "Reinforcement Learning / Inverse"
+e["Problem"]["Environment Function"] = env
+e["Problem"]["Training Reward Threshold"] = 400
+e["Problem"]["Policy Testing Episodes"] = 20
+e["Problem"]["Actions Between Policy Updates"] = 5
+e["Problem"]["Observation File"] = "./observations.json"
+
+e["Variables"][0]["Name"] = "Cart Position"
+e["Variables"][0]["Type"] = "State"
+
+e["Variables"][1]["Name"] = "Cart Velocity"
+e["Variables"][1]["Type"] = "State"
+
+e["Variables"][2]["Name"] = "Pole Angle"
+e["Variables"][2]["Type"] = "State"
+
+e["Variables"][3]["Name"] = "Pole Angular Velocity"
+e["Variables"][3]["Type"] = "State"
+
+e["Variables"][4]["Name"] = "Force"
+e["Variables"][4]["Type"] = "Action"
+e["Variables"][4]["Lower Bound"] = -10.0
+e["Variables"][4]["Upper Bound"] = +10.0
+e["Variables"][4]["Initial Exploration Noise"] = 1.0
+
+### Defining Agent Configuration 
+
+e["Solver"]["Type"] = "Agent / Continuous / VRACER"
+e["Solver"]["Mode"] = "Training"
+e["Solver"]["Experiences Between Policy Updates"] = 10
+e["Solver"]["Episodes Per Generation"] = 1
+e["Solver"]["Cache Persistence"] = 100
+
+### Defining the configuration of replay memory
+
+e["Solver"]["Experience Replay"]["Start Size"] = 1000
+e["Solver"]["Experience Replay"]["Maximum Size"] = 10000
+
+## Defining Neural Network Configuration for Policy and Critic into Critic Container
+
+e["Solver"]["Discount Factor"] = 0.99
+e["Solver"]["Learning Rate"] = 1e-3
+e["Solver"]["Mini Batch Size"] = 32
+
+### Configuring the neural network and its hidden layers
+
+e["Solver"]["Neural Network"]["Engine"] = "OneDNN"
+
+e["Solver"]["Neural Network"]["Hidden Layers"][0]["Type"] = "Layer/Linear"
+e["Solver"]["Neural Network"]["Hidden Layers"][0]["Output Channels"] = 32
+
+e["Solver"]["Neural Network"]["Hidden Layers"][1]["Type"] = "Layer/Activation"
+e["Solver"]["Neural Network"]["Hidden Layers"][1]["Function"] = "Elementwise/Tanh"
+
+e["Solver"]["Neural Network"]["Hidden Layers"][2]["Type"] = "Layer/Linear"
+e["Solver"]["Neural Network"]["Hidden Layers"][2]["Output Channels"] = 32
+
+e["Solver"]["Neural Network"]["Hidden Layers"][3]["Type"] = "Layer/Activation"
+e["Solver"]["Neural Network"]["Hidden Layers"][3]["Function"] = "Elementwise/Tanh"
+
+### Defining Termination Criteria
+
+e["Solver"]["Termination Criteria"]["Testing"]["Target Average Reward"] = 450
+
+### Setting file output configuration
+
+e["File Output"]["Enabled"] = False
+
+### Running Experiment
+
+k.run(e)

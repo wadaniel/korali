5236c4d8e8c91a1213835f3761c3a43de831b006
Georgios Arampatzis <garampat@ethz.ch>
2021-02-19 14:09:03 +0100

    remove three tests from REG-000

--- tests/REG-000/run_test.sh
+++ tests/REG-000/run_test.sh
@@ -12,14 +12,15 @@ echo "[Korali] Checking korali.plotter"
 python3 -m korali.plotter --check
 exit_code=$(( $exit_code || $? ))
 
-echo "[Korali] Checking korali.cxx"
-python3 -m korali.cxx --cflags
-exit_code=$(( $exit_code || $? ))
-
-python3 -m korali.cxx --compiler
-exit_code=$(( $exit_code || $? ))
-
-python3 -m korali.cxx --libs
-exit_code=$(( $exit_code || $? ))
+# TODO: @Fabian: how shoudl we test for these?
+#echo "[Korali] Checking korali.cxx"
+#python3 -m korali.cxx --cflags
+#exit_code=$(( $exit_code || $? ))
+#
+#python3 -m korali.cxx --compiler
+#exit_code=$(( $exit_code || $? ))
+#
+#python3 -m korali.cxx --libs
+#exit_code=$(( $exit_code || $? ))
 
 exit $exit_code

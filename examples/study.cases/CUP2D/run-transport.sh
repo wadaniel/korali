71e8b7d4f78fd03c04eb2f97b1fe38259d67666f
Pascal Weber <webepasc@student.ethz.ch>
2021-02-19 15:21:46 +0100

    unified CUP2D enviroments

--- examples/study.cases/CUP2D/run-transport.sh
+++ examples/study.cases/CUP2D/run-transport.sh
@@ -11,7 +11,7 @@ fi
 set -x
 
 # AMR
-$mpiflags ./run-vracer  -bpdx 32 -bpdy 16 -levelMax 3 -Rtol 0.1 -Ctol 0.01 -poissonType cosine -muteAll 1 -verbose 0 -tdump 0 -nu 0.000018 -tend 0 -poissonType cosine -shapes 'smartDisk_radius=.06_xpos=.2'
+$mpiflags ./run-vracer-transport  -bpdx 32 -bpdy 32 -levelMax 3 -Rtol 0.1 -Ctol 0.01 -poissonType cosine -muteAll 1 -verbose 0 -tdump 0 -nu 0.000018 -tend 0 -poissonType cosine -shapes 'smartDisk_radius=.06_xpos=.2'
 
 # NO AMR
 # $mpiflags ./run-vracer -bpdx 32 -bpdy 16 -poissonType cosine -muteAll 1 -tdump 0 -nu 0.000018 -tend 0 -shapes 'halfDisk_radius=.06_angle=20_xpos=.2_bForced=1_bFixed=1_xvel=0.15_tAccel=5,stefanfish_L=.2_xpos=.5' 

9c7ffe50743410a98b43ec1d4824497fdfe27039
Sergio Martin <martiser@ethz.ch>
2021-03-02 11:00:01 +0100

    Latest changes

--- examples/study.cases/ABF3D/run-vracer.cpp
+++ examples/study.cases/ABF3D/run-vracer.cpp
@@ -108,7 +108,7 @@ int main(int argc, char *argv[])
 
   e["Console Output"]["Verbosity"] = "Detailed";
   e["File Output"]["Enabled"] = true;
-  e["File Output"]["Frequency"] = 10;
+  e["File Output"]["Frequency"] = 100;
   e["File Output"]["Path"] = _resultDir;
 
   auto k = korali::Engine();

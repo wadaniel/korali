7389b8fe110ad704e4dd76bf092491fbb2e7a0b0
G. Arampatzis <gtarabat@gmail.com>
2021-03-02 13:55:54 +0100

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 rm -rf build
 rm -rf test_install
-meson setup build --buildtype release --prefix $(pwd -P)/test_install
+CXX=clang++ CC=clang meson setup build --buildtype release --prefix $(pwd -P)/test_install  
+# --force-fallback-for=gsl
 # meson compile -C build
 # meson test -C build
 # meson install -C build

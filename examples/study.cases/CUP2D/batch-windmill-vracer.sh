14dbd7d4fba02834997a73cb4f014e4996e3d327
Pascal Weber <webepasc@student.ethz.ch>
2021-03-18 13:27:59 +0100

    minor in windmill batch

--- examples/study.cases/CUP2D/batch-windmill-vracer.sh
+++ examples/study.cases/CUP2D/batch-windmill-vracer.sh
@@ -5,44 +5,39 @@ mpiflags="mpirun -n 2"
 
 if [ ! -z $SLURM_NNODES ]; then
  N=$SLURM_NNODES
- mpiflags="ddt srun -N $N -n $(($N+1)) -c 12"
+ mpiflags="srun -N $N -n $(($N)) -c 12"
 fi
 
 set -x
 
 # Defaults for Options
-BPDX=${BPDX:-8} # number of blocks in x direction, 8 cells per block in compilation
+BPDX=${BPDX:-8}
 BPDY=${BPDY:-8}
-LEVELS=${LEVELS:-5} # each block can be refined twice, double number of points per refinement. 
+LEVELS=${LEVELS:-5}
 RTOL=${RTOL-0.1}
 CTOL=${CTOL-0.01}
-EXTENT=${EXTENT:-1} # direction with most grid points is of size 1, (0, 0) point in bottom left
+EXTENT=${EXTENT:-1}
 CFL=${CFL:-0.5}
+# Defaults for Objects
+XPOS=${XPOS:-0.2}
+YPOS1=${YPOS:-0.2}
+YPOS2=${YPOS2:-0.4}
+YPOS3=${YPOS3:-0.6}
+YPOS4=${YPOS4:-0.8}
 
-# XPOS=${XPOS:-0.2}
+XVEL=${XVEL:-0.15}
 
-# YPOS=${YPOS:-0.2}
-# YPOS2=${YPOS2:-0.4}
-# YPOS3=${YPOS3:-0.6}
-# YPOS4=${YPOS4:-0.8}
-
-# XVEL=${XVEL:-0.15}
-
-# MAAXIS=${MAAXIS:-0.0375}
-# MIAXIS=${MIAXIS:-0.01}
+MAAXIS=${MAAXIS:-0.0375}
+MIAXIS=${MIAXIS:-0.01}
 
 NU=${NU:-0.0001125}
 
-# OBJECTS="windmill semiAxisX=$MAAXIS semiAxisY=$MIAXIS xpos=$XPOS ypos=$YPOS bForced=1 bFixed=1 xvel=$XVEL tAccel=0 bBlockAng=0
-# windmill semiAxisX=$MAAXIS semiAxisY=$MIAXIS xpos=$XPOS ypos=$YPOS2 bForced=1 xvel=$XVEL tAccel=0 bBlockAng=0
-# windmill semiAxisX=$MAAXIS semiAxisY=$MIAXIS xpos=$XPOS ypos=$YPOS3 bForced=1 xvel=$XVEL tAccel=0 bBlockAng=0
-# windmill semiAxisX=$MAAXIS semiAxisY=$MIAXIS xpos=$XPOS ypos=$YPOS4 bForced=1 xvel=$XVEL tAccel=0 bBlockAng=0
-# "
+OPTIONS="-bpdx $BPDX -bpdy $BPDY -levelMax $LEVELS -Rtol $RTOL -Ctol $CTOL -extent $EXTENT -CFL $CFL -tdump 0.1 -nu $NU -tend 50 -poissonType dirichlet -iterativePensalization 0 -muteAll 0 -verbose 1"
+# bForced, tAccel is needed here!
+OBJECTS="windmill semiAxisX=$MAAXIS semiAxisY=$MIAXIS xpos=$XPOS ypos=$YPOS1 bForced=1 bFixed=1 xvel=$XVEL tAccel=0 bBlockAng=0
+windmill semiAxisX=$MAAXIS semiAxisY=$MIAXIS xpos=$XPOS ypos=$YPOS2 bForced=1 xvel=$XVEL tAccel=0 bBlockAng=0
+windmill semiAxisX=$MAAXIS semiAxisY=$MIAXIS xpos=$XPOS ypos=$YPOS3 bForced=1 xvel=$XVEL tAccel=0 bBlockAng=0
+windmill semiAxisX=$MAAXIS semiAxisY=$MIAXIS xpos=$XPOS ypos=$YPOS4 bForced=1 xvel=$XVEL tAccel=0 bBlockAng=0
+"
 
-$mpiflags ./run-vracer-windmill -bpdx 8 -bdpy 8 -levelMax 3 -Rtol 1 -Ctol 0.1 -extent 1 -CFL 0.3 -tdump 0.1 -nu 0.0001125 -tend 50 -poissonType dirichlet -iterativePensalization 0 -muteAll 0 -verbose 1 "windmill_semiAxisX=0.0375_semiAxisY=0.01_xpos=0.2_ypos=0.2_bForced=1_bFixed=1_xvel=0.15_tAccel=0_bBlockAng=0,windmill_semiAxisX=0.0375_semiAxisY=0.01_xpos=0.2_ypos=0.4_bForced=1_bFixed=1_xvel=0.15_tAccel=0_bBlockAng=0,windmill_semiAxisX=0.0375_semiAxisY=0.01_xpos=0.2_ypos=0.6_bForced=1_bFixed=1_xvel=0.15_tAccel=0_bBlockAng=0,windmill_semiAxisX=0.0375_semiAxisY=0.01_xpos=0.2_ypos=0.8_bForced=1_bFixed=1_xvel=0.15_tAccel=0_bBlockAng=0"
-
-# AMR
-#$mpiflags ./run-vracer-transport  -bpdx 32 -bpdy 32 -levelMax 3 -Rtol 0.1 -Ctol 0.01 -poissonType dirichlet -muteAll 1 -verbose 0 -tdump 0 -nu 0.000018 -tend 0 -shapes ''
-
-# NO AMR
-# $mpiflags ./run-vracer -bpdx 32 -bpdy 16 -poissonType cosine -muteAll 1 -tdump 0 -nu 0.000018 -tend 0 -shapes 'halfDisk_radius=.06_angle=20_xpos=.2_bForced=1_bFixed=1_xvel=0.15_tAccel=5,stefanfish_L=.2_xpos=.5' 
+$mpiflags ./run-vracer-windmill ${OPTIONS} -shapes "${OBJECTS}"

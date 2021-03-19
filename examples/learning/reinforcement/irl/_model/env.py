3b4c41f87b2ba7caa49c5489f26ffdc1b211b248
wadaniel <wadaniel@ethz.ch>
2021-03-19 11:33:30 +0100

    added cartpole cos irl example and code

--- examples/learning/reinforcement/irl/_model/env.py
+++ examples/learning/reinforcement/irl/_model/env.py
@@ -32,8 +32,48 @@ def env(s):
   s["State"] = cart.getState().tolist()
   
   # Getting Features
-  s["Features"] = [ np.cos(state[2]), state[1]*state[1] ]
-  #s["Features"] = [ np.cos(state[2]), state[1]*state[1], np.random.normal(0.0, 0.1) ]
+  # Pole angle and cart velocity^2
+  #s["Features"] = [ np.cos(state[2]), state[1]*state[1] ]
+  # Pole angle, cart velocity^2 and dummy
+  s["Features"] = [ np.cos(state[2]), state[1]*state[1], np.random.normal(0.0, 0.1) ]
+  # for COSREWARD setup: feature = angle
+  s["Features"] = [ state[2] ]
+  
+  # Advancing step counter
+  step = step + 1
+
+ # Setting finalization status
+ if (cart.isOver()):
+  s["Termination"] = "Terminal"
+ else:
+  s["Termination"] = "Truncated"
+
+def cosenv(s):
+
+ # Initializing environment
+ cart.reset()
+ s["State"] = cart.getState().tolist()
+ step = 0
+ done = False
+
+ while not done and step < maxSteps:
+
+  # Getting new action
+  s.update()
+  
+  # Performing the action
+  done = cart.advance(s["Action"])
+  #print(s["Action"]) 
+  
+  # Getting Reward
+  s["Reward"] = cart.getReward()
+  
+  # Storing New State
+  state = cart.getState().tolist()
+  s["State"] = cart.getState().tolist()
+  
+  # Getting Features
+  s["Features"] = [ state[2] ]
   
   # Advancing step counter
   step = step + 1

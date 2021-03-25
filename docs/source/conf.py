da247d6d249c8f177e222a2040ff6df7a43684e2
G. Arampatzis <gtarabat@gmail.com>
2021-03-25 15:57:45 +0100

    Update conf.py in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,8 +31,6 @@ extensions = [
   'sphinx.ext.todo',
   'sphinx.ext.mathjax',
   'sphinx_rtd_theme',
-  'sphinxcontrib.bibtex',
-  'breathe',
 ]
 
 # Add any paths that contain templates here, relative to this directory.

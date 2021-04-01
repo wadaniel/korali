9fa2bab6bef76557bc880b126dc5f0c1056a2470
Sergio Martin <martiser@ethz.ch>
2021-04-01 09:51:50 +0200

    Adding cartpole dvracer

---
+++ examples/learning/reinforcement/cartpole/run-dvracer.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+import os
+import sys
+sys.path.append('./_model')
+from env import *
+
+####### Defining Korali Problem
+
+import korali
+k = korali.Engine()
+e = korali.Experiment()
+
+### Defining the Cartpole problem's configuration
+
+e["Problem"]["Type"] = "Reinforcement Learning / Discrete"
+e["Problem"]["Possible Actions"] = [ [ -10.0 ], [  10.0 ] ]
+e["Problem"]["Environment Function"] = env
+e["Problem"]["Training Reward Threshold"] = 400
+e["Problem"]["Policy Testing Episodes"] = 20
+e["Problem"]["Actions Between Policy Updates"] = 5
+
+e["Variables"][0]["Name"] = "Cart Position"
+e["Variables"][0]["Type"] = "State"
+
+e["Variables"][1]["Name"] = "Cart Velocity"
+e["Variables"][1]["Type"] = "State"
+
+e["Variables"][2]["Name"] = "Pole Angle"
+e["Variables"][2]["Type"] = "State"
+
+e["Variables"][3]["Name"] = "Pole Angular Velocity"
+e["Variables"][3]["Type"] = "State"
+
+e["Variables"][4]["Name"] = "Force"
+e["Variables"][4]["Type"] = "Action"
+
+
+### Defining Agent Configuration 
+
+e["Solver"]["Type"] = "Agent / Discrete / dVRACER"
+e["Solver"]["Mode"] = "Training"
+e["Solver"]["Episodes Per Generation"] = 10
+e["Solver"]["Experiences Between Policy Updates"] = 1
+e["Solver"]["Learning Rate"] = 1e-3
+e["Solver"]["Mini Batch"]["Size"] = 32
+
+### Defining Experience Replay configuration
+
+e["Solver"]["Experience Replay"]["Start Size"] = 4096
+e["Solver"]["Experience Replay"]["Maximum Size"] = 65536
+
+### Setting Experience Replay and REFER settings
+
+e["Solver"]["Experience Replay"]["Off Policy"]["Annealing Rate"] = 5.0e-8
+e["Solver"]["Experience Replay"]["Off Policy"]["Cutoff Scale"] = 5.0
+e["Solver"]["Experience Replay"]["Off Policy"]["REFER Beta"] = 0.3
+e["Solver"]["Experience Replay"]["Off Policy"]["Target"] = 0.1
+
+e["Solver"]["State Rescaling"]["Enabled"] = True
+e["Solver"]["Reward Rescaling"]["Enabled"] = True
+e["Solver"]["Reward Rescaling"]["Frequency"] = 1000
+  
+### Configuring the neural network and its hidden layers
+
+e["Solver"]["Neural Network"]["Engine"] = "OneDNN"
+e["Solver"]["L2 Regularization"]["Enabled"] = False
+e["Solver"]["L2 Regularization"]["Importance"] = 1.0
+
+### Configuring the neural network and its hidden layers
+
+e["Solver"]["Neural Network"]["Engine"] = "OneDNN"
+
+e["Solver"]["Neural Network"]["Hidden Layers"][0]["Type"] = "Layer/Linear"
+e["Solver"]["Neural Network"]["Hidden Layers"][0]["Output Channels"] = 32
+
+e["Solver"]["Neural Network"]["Hidden Layers"][1]["Type"] = "Layer/Activation"
+e["Solver"]["Neural Network"]["Hidden Layers"][1]["Function"] = "Elementwise/Tanh"
+
+e["Solver"]["Neural Network"]["Hidden Layers"][2]["Type"] = "Layer/Linear"
+e["Solver"]["Neural Network"]["Hidden Layers"][2]["Output Channels"] = 32
+
+e["Solver"]["Neural Network"]["Hidden Layers"][3]["Type"] = "Layer/Activation"
+e["Solver"]["Neural Network"]["Hidden Layers"][3]["Function"] = "Elementwise/Tanh"
+
+### Defining Termination Criteria
+
+e["Solver"]["Termination Criteria"]["Testing"]["Target Average Reward"] = 450
+
+### Setting file output configuration
+
+e["File Output"]["Enabled"] = False
+e["Console Output"]["Verbosity"] = "Detailed"
+
+### Running Experiment
+
+k.run(e)

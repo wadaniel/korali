abd289e08514bbbaecc9efe138565d2f253064b3
Sergio Martin <martiser@ethz.ch>
2021-04-20 11:35:25 +0200

    Adding mnist example

---
+++ examples/learning/supervised/mnist/get_data.sh
@@ -0,0 +1,10 @@
+rm -rf _data
+mkdir _data
+pushd _data
+
+wget https://storage.googleapis.com/cvdf-datasets/mnist/train-images-idx3-ubyte.gz
+wget https://storage.googleapis.com/cvdf-datasets/mnist/train-labels-idx1-ubyte.gz
+wget https://storage.googleapis.com/cvdf-datasets/mnist/t10k-images-idx3-ubyte.gz
+wget https://storage.googleapis.com/cvdf-datasets/mnist/t10k-labels-idx1-ubyte.gz
+
+popd

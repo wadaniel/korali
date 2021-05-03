668e8da98e1a49449d9c6cbb5783863914618e6a
Fabian Wermelinger <fabianw@mavt.ethz.ch>
2021-05-03 14:44:01 +0200

    Clean-up

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf build
-rm -rf test_install
-CXX=clang++ CC=clang meson setup build --buildtype release --prefix $(pwd -P)/test_install  
-# --force-fallback-for=gsl
-# meson compile -C build
-# meson test -C build
-# meson install -C build

f93ae29813ee18f6acb955a939e15dede6b999a2
Sergio Martin <martiser@ethz.ch>
2021-05-10 12:53:44 +0200

    Fixing tests

--- examples/learning/supervised/sineWave/.test-run.py
+++ examples/learning/supervised/sineWave/.test-run.py
@@ -1,11 +1,11 @@
 #! /usr/bin/env python3
 from subprocess import call
 
-r = call(["python3", "run-ffn.py"])
+r = call(["python3", "run-ffn.py", "--test"])
 if r!=0:
   exit(r)
 
-r = call(["python3", "run-rnn.py"])
+r = call(["python3", "run-rnn.py", "--test"])
 if r!=0:
   exit(r)
 

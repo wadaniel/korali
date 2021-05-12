9aa1f4520661e52a307107cb3ae6bb026cab0d0d
Sergio Martin <martiser@ethz.ch>
2021-05-12 13:10:18 +0200

    Fixing cxx script

--- python/korali/cxx/__main__.py
+++ python/korali/cxx/__main__.py
@@ -5,18 +5,19 @@ import ast
 import sysconfig
 import os
 import subprocess
+import korali
+import pybind11
 
 import libkorali as lk
 
 def main():
   fileDir = os.path.dirname(os.path.realpath(__file__))
-  koraliDir = os.path.abspath(fileDir + '/../')
 
   configFile = open(fileDir + '/flags.in', "r")
   configFileContents = configFile.read()
   configDict = ast.literal_eval(configFileContents)
   configFile.close()
-
+  
   if (len(sys.argv) != 2):
     print('[Korali] Syntax error on call to korali.cxx module: Exactly one argument is required (--cflags, --libs, or --compiler).')
     exit(-1)
@@ -24,8 +25,10 @@ def main():
   if (sys.argv[1] == '--cflags'):
     correctSyntax = True
     pythonCFlagsCommand = subprocess.Popen("python3-config --includes", shell=True, stdout=subprocess.PIPE)
-    pythonCFlags = pythonCFlagsCommand.stdout.read().decode()
-    flags = '-std=c++17' + ' -I' + koraliDir + '/../../source/' + ' -I' + koraliDir + '/include' + ' -I' + sysconfig.get_path("include") + ' -I' + sysconfig.get_path("platinclude") + ' -I' + configDict['PYBIND11_INCLUDES'] + ' ' + pythonCFlags
+    pythonCFlags = pythonCFlagsCommand.stdout.read().decode().rstrip("\n")
+    pybind11Includes=pybind11.get_include()
+    koraliIncludes=' -I' + os.path.dirname(korali.__file__) + '/include'
+    flags = '-std=c++17' + koraliIncludes + ' -I' + sysconfig.get_path("include") + ' -I' + sysconfig.get_path("platinclude") + ' -I' + pybind11Includes + ' ' + pythonCFlags
 
   if (sys.argv[1] == '--libs'):
     correctSyntax = True
@@ -38,11 +41,7 @@ def main():
     
     # Looking for Korali library
     koraliLib = os.path.realpath(lk.__file__)
-    flags = '-L' + koraliDir + ' -L' + koraliDir + '/../../../../lib' + ' -L' + koraliDir + '/../../../../lib64' + ' ' + koraliLib + ' ' + pythonLibs
-
-  if (sys.argv[1] == '--compiler'):
-    correctSyntax = True
-    flags = configDict['CXX']
+    flags = koraliLib + ' ' + pythonLibs
 
   if (correctSyntax == False):
    print('[Korali] Syntax error on call to korali.cxx module: Argument \'{0}\' not recognized (--cflags, --libs, or --compiler).'.format(sys.argv[1]))

557a8df70a33bf578705f52ea2bea2f7bc670ced
wadaniel <wadaniel@ethz.ch>
2021-05-13 17:04:32 +0200

    added checkes, new run script

--- examples/learning/reinforcement/irl/plot_logpartitionfunction_new.py
+++ examples/learning/reinforcement/irl/plot_logpartitionfunction_new.py
@@ -14,6 +14,7 @@ if __name__ == '__main__':
   parser.add_argument('--resultdir','--d', required=True, type=str, help='directory to read')
   parser.add_argument('--logy', action='store_true', help='logscale y axis')
   parser.add_argument('--intermediate', '--i', action='store_true', help='show intermediate plots')
+  parser.add_argument('--last', '--l', action='store_true', help='show last estimate')
   parser.add_argument('--fusion', '--f', action='store_true', help='plot fusion distribution')
   parser.add_argument('--both', '--b', action='store_true', help='plot single and fusion distribution')
   parser.add_argument('--returns', '--r', action='store_true', help='plot returns')
@@ -21,6 +22,7 @@ if __name__ == '__main__':
   args = parser.parse_args()
   resultdir = args.resultdir
   showIntermediate = args.intermediate
+  showOnlyLast = args.last
   plotFusion = args.fusion
   plotBoth = args.both
   plotReturns = args.returns
@@ -56,11 +58,13 @@ if __name__ == '__main__':
         label = r"Total Trajectories {} ($\theta$ = {:.3f}, Single)".format(bsize, thetas[i][0])
         labelFusion = r"Total Trajectories {} ($\theta$ = {:.3f}, Mixture)".format(bsize, thetas[i][0])
  
+    if showOnlyLast == True and i < len(stats)-1:
+        continue
+
     if plotBoth:
         plt.plot(xbatchsize, means, label=label, linestyle='-')
         plt.plot(xbatchsize, statsFusion[i], label=labelFusion, linestyle='--')
     else:
-
         if plotFusion:
             plt.plot(xbatchsize, statsFusion[i], label=labelFusion, linestyle='--')
         else:
@@ -68,18 +72,17 @@ if __name__ == '__main__':
  
     plt.xlabel('Number of Trajectories M')
     plt.ylabel(r'log($Z_\theta$)')
-    if(showIntermediate):
+    if(showIntermediate or showOnlyLast == True):
         plt.title(r'Estimate Log Partition Function $Z_\theta$ ($\theta = {:.3f}$, Total Trajectories {})'.format(thetas[i][0], bsize))
         if plotReturns:
             plt.plot(xbatchsize, returns[i], label=labelRewards, linestyle='None', marker='x', color='r', alpha=0.5)
         plt.legend()
         plt.show()
  
-  if plotReturns:
-    plt.plot(xbatchsize, returns[-1], label=labelRewards, linestyle='None', marker='x', color='r', alpha=0.5)
-
-  if(showIntermediate == False):
+  if(not (showIntermediate == True or showOnlyLast == True)):
     plt.title(r'Estimate Log Partition Function $Z_\theta$')
+    if plotReturns:
+        plt.plot(xbatchsize, returns[-1], label=labelRewards, linestyle='None', marker='x', color='r', alpha=0.5)
     plt.legend()
     plt.show()
     

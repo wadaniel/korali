fbd11b752e293a21cb42769827611f37db12032b
wadaniel <wadaniel@ethz.ch>
2021-05-19 09:25:07 +0200

    added file

---
+++ examples/learning/reinforcement/cartpole/run-es.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+import os
+import sys
+sys.path.append('./_model')
+from env import *
+
+####### Defining Korali Problem
+
+import korali
+k = korali.Engine()
+e = korali.Experiment()
+
+### Defining the Cartpole problem's configuration
+
+e["Problem"]["Type"] = "Reinforcement Learning / Continuous"
+e["Problem"]["Environment Function"] = env
+e["Problem"]["Training Reward Threshold"] = 400
+e["Problem"]["Policy Testing Episodes"] = 20
+e["Problem"]["Actions Between Policy Updates"] = 1
+
+e["Variables"][0]["Name"] = "Cart Position"
+e["Variables"][0]["Type"] = "State"
+
+e["Variables"][1]["Name"] = "Cart Velocity"
+e["Variables"][1]["Type"] = "State"
+
+e["Variables"][2]["Name"] = "Pole Angle"
+e["Variables"][2]["Type"] = "State"
+
+e["Variables"][3]["Name"] = "Pole Angular Velocity"
+e["Variables"][3]["Type"] = "State"
+
+e["Variables"][4]["Name"] = "Force"
+e["Variables"][4]["Type"] = "Action"
+e["Variables"][4]["Lower Bound"] = -10.0
+e["Variables"][4]["Upper Bound"] = +10.0
+e["Variables"][4]["Initial Exploration Noise"] = 1.0 # no use
+
+### Defining Agent Configuration 
+
+e["Solver"]["Type"] = "Agent / Continuous / ES"
+e["Solver"]["Mode"] = "Training"
+e["Solver"]["Experiences Between Policy Updates"] = 1
+e["Solver"]["Episodes Per Generation"] = 1
+e["Solver"]["Episodes Per Update"] = 32
+
+e["Solver"]["Experience Replay"]["Start Size"] = 8096
+e["Solver"]["Experience Replay"]["Maximum Size"] = 10000
+
+e["Solver"]["Discount Factor"] = 0.99
+e["Solver"]["Learning Rate"] = 1e-4
+e["Solver"]["Noise Parameter"] = 1e-4
+e["Solver"]["Mini Batch"]["Size"] = 32
+
+e["Solver"]["State Rescaling"]["Enabled"] = True
+e["Solver"]["Reward"]["Rescaling"]["Enabled"] = False
+
+### Configuring the neural network and its hidden layers
+
+e["Solver"]["Neural Network"]["Engine"] = "OneDNN"
+e["Solver"]["Neural Network"]["Optimizer"] = "AdaBelief"
+
+e["Solver"]["Neural Network"]["Hidden Layers"][0]["Type"] = "Layer/Linear"
+e["Solver"]["Neural Network"]["Hidden Layers"][0]["Output Channels"] = 4
+
+#e["Solver"]["Neural Network"]["Hidden Layers"][1]["Type"] = "Layer/Activation"
+#e["Solver"]["Neural Network"]["Hidden Layers"][1]["Function"] = "Elementwise/Tanh"
+
+#e["Solver"]["Neural Network"]["Hidden Layers"][2]["Type"] = "Layer/Linear"
+#e["Solver"]["Neural Network"]["Hidden Layers"][2]["Output Channels"] = 32
+
+#e["Solver"]["Neural Network"]["Hidden Layers"][3]["Type"] = "Layer/Activation"
+#e["Solver"]["Neural Network"]["Hidden Layers"][3]["Function"] = "Elementwise/Tanh"
+
+### Defining Termination Criteria
+
+e["Solver"]["Termination Criteria"]["Max Experiences"] = 1e6
+e["Solver"]["Termination Criteria"]["Testing"]["Target Average Reward"] = 450
+
+### Setting file output configuration
+
+e["File Output"]["Enabled"] = True
+
+### Running Experiment
+
+k.run(e)

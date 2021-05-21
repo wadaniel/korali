938d2f7866715f1a9a25587f67924a1b860998fa
Sergio Martin <martiser@ethz.ch>
2021-05-21 11:52:25 +0200

    Improving tests

--- tests/statistical/samplers/mean/run-tmcmc-laplace.py
+++ tests/statistical/samplers/mean/run-tmcmc-laplace.py
@@ -15,19 +15,18 @@ e = korali.Experiment()
 
 # Setting up custom likelihood for the Bayesian Problem
 e["Problem"]["Type"] = "Bayesian/Custom"
-e["Problem"]["Likelihood Model"] = lexponentialCustom
+e["Problem"]["Likelihood Model"] = llaplaceCustom
 
 # Configuring TMCMC parameters
 e["Solver"]["Type"] = "Sampler/TMCMC"
-e["Solver"]["Target Coefficient Of Variation"] = 0.6
-e["Solver"]["Population Size"] = 5000
-e["Solver"]["Per Generation Burn In"] = [3, 3, 3, 3]
+e["Solver"]["Population Size"] = 1000
+e["Solver"]["Default Burn In"] = 3
 
 # Configuring the problem's random distributions
 e["Distributions"][0]["Name"] = "Uniform 0"
 e["Distributions"][0]["Type"] = "Univariate/Uniform"
-e["Distributions"][0]["Minimum"] = 0.0
-e["Distributions"][0]["Maximum"] = 80.0
+e["Distributions"][0]["Minimum"] = -20.0
+e["Distributions"][0]["Maximum"] = +20.0
 
 # Configuring the problem's variables and their prior distributions
 e["Variables"][0]["Name"] = "a"
@@ -36,8 +35,10 @@ e["Variables"][0]["Prior Distribution"] = "Uniform 0"
 e["File Output"]["Frequency"] = 0
 
 # Running Korali
-e["Random Seed"] = 1234
+e["Random Seed"] = 1337
+k["Conduit"]["Type"] = "Concurrent"
+k["Conduit"]["Concurrent Jobs"] = 8
 k.run(e)
 
 verifyMean(e["Solver"]["Sample Database"], [4.0], 0.05)
-verifyStd(e["Solver"]["Sample Database"], [4.0], 0.05)
+verifyStd(e["Solver"]["Sample Database"], [math.sqrt(2)], 0.05)

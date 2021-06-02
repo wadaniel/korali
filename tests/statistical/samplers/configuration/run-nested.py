03d911918ba8ddbc4d3f95a30b01782d0ce480a1
wadaniel <wadaniel@ethz.ch>
2021-06-02 14:06:11 +0200

    added configuration test for nested

--- tests/statistical/samplers/configuration/run-nested.py
+++ tests/statistical/samplers/configuration/run-nested.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Importing computational model
+# Importing the computational model
 import sys
 import numpy as np
 
@@ -12,27 +12,30 @@ def verify(actual, expected, atol=1e-9):
 def lgaussian(s):
   x0 = s["Parameters"][0]
   x1 = s["Parameters"][1]
-  r = - 0.5* (x0*x0 + x1*x1)
+  x2 = s["Parameters"][2]
+  r = - 0.5* (x0*x0 + x1*x1 + x2*x2)
   s["logLikelihood"] = r
+
 
 # Starting Korali's Engine
 import korali
 k = korali.Engine()
 e = korali.Experiment()
 
-# Setting up custom likelihood for the Bayesian Problem
+# Setting up the reference likelihood for the Bayesian Problem
 e["Problem"]["Type"] = "Bayesian/Custom"
 e["Problem"]["Likelihood Model"] = lgaussian
 
-# Configuring TMCMC parameters
-e["Solver"]["Type"] = "Sampler/TMCMC"
-e["Solver"]["Population Size"] = 100
+# Configuring Nested Sampling parameters
+e["Solver"]["Type"] = "Sampler/Nested"
+e["Solver"]["Number Live Points"] = 100
+e["Solver"]["Batch Size"] = 1
 
 # Configuring the problem's random distributions
 e["Distributions"][0]["Name"] = "Uniform 0"
 e["Distributions"][0]["Type"] = "Univariate/Uniform"
 e["Distributions"][0]["Minimum"] = 0.0
-e["Distributions"][0]["Maximum"] = 5.0
+e["Distributions"][0]["Maximum"] = 2.0
 
 # Configuring the problem's variables and their prior distributions
 e["Variables"][0]["Name"] = "a"
@@ -41,14 +44,14 @@ e["Variables"][0]["Prior Distribution"] = "Uniform 0"
 e["Variables"][1]["Name"] = "b"
 e["Variables"][1]["Prior Distribution"] = "Uniform 0"
 
+e["Variables"][2]["Name"] = "c"
+e["Variables"][2]["Prior Distribution"] = "Uniform 0"
+
 e["Solver"]["Termination Criteria"]["Max Generations"] = 1
 
-# Running Korali
+# Starting Korali's Engine and running experiment
 e["File Output"]["Enabled"] = False
 k.run(e)
 
 # Verify Initial Values
-verify(e["Solver"]["Chain Candidates LogPriors"][0], np.log(1./25.))
-verify(e["Solver"]["Chain Leaders LogPriors"][0], np.log(1./25.))
-
-
+verify(e["Solver"]["Live LogPriors"][0], np.log(1./8.))

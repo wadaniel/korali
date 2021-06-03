d8aeb6201a2d671d07a164abe45ff23b45de8a1c
Sergio Martin <martiser@ethz.ch>
2021-06-03 16:48:52 +0200

    Adding more generations to sine test

--- examples/learning/supervised/sineWave/.test-run.py
+++ examples/learning/supervised/sineWave/.test-run.py
@@ -2,51 +2,51 @@
 from subprocess import call
 import os
 
-r = call(["python3", "run-ffn.py", "--optimizer", "AdaBelief", "--learningRate", "0.005", "--maxGenerations", "10"])
+r = call(["python3", "run-ffn.py", "--optimizer", "AdaBelief", "--learningRate", "0.005", "--maxGenerations", "20"])
 if r!=0:
   exit(r)
 
-r = call(["python3", "run-ffn.py", "--optimizer", "Adagrad", "--learningRate", "0.05", "--maxGenerations", "10"])
+r = call(["python3", "run-ffn.py", "--optimizer", "Adagrad", "--learningRate", "0.05", "--maxGenerations", "20"])
 if r!=0:
   exit(r)
 
-r = call(["python3", "run-ffn.py", "--optimizer", "Adam", "--learningRate", "0.005", "--maxGenerations", "10"])
+r = call(["python3", "run-ffn.py", "--optimizer", "Adam", "--learningRate", "0.005", "--maxGenerations", "20"])
 if r!=0:
   exit(r)
 
-r = call(["python3", "run-ffn.py", "--optimizer", "MADGRAD", "--learningRate", "0.005", "--maxGenerations", "10"])
+r = call(["python3", "run-ffn.py", "--optimizer", "MADGRAD", "--learningRate", "0.005", "--maxGenerations", "20"])
 if r!=0:
   exit(r)
       
-r = call(["python3", "run-ffn.py", "--optimizer", "RMSProp", "--learningRate", "0.002", "--maxGenerations", "10"])
+r = call(["python3", "run-ffn.py", "--optimizer", "RMSProp", "--learningRate", "0.002", "--maxGenerations", "20"])
 if r!=0:
   exit(r)
   
-r = call(["python3", "run-ffn.py", "--optimizer", "Adam", "--learningRate", "0.005", "--engine", "Korali", "--maxGenerations", "10"])
+r = call(["python3", "run-ffn.py", "--optimizer", "Adam", "--learningRate", "0.005", "--engine", "Korali", "--maxGenerations", "20"])
 if r!=0:
   exit(r)
     
-r = call(["python3", "run-rnn.py", "--optimizer", "AdaBelief", "--learningRate", "0.001", "--maxGenerations", "10"])
+r = call(["python3", "run-rnn.py", "--optimizer", "AdaBelief", "--learningRate", "0.001", "--maxGenerations", "20"])
 if r!=0:
   exit(r)
 
-r = call(["python3", "run-rnn.py", "--optimizer", "Adagrad", "--learningRate", "0.01", "--maxGenerations", "10"])
+r = call(["python3", "run-rnn.py", "--optimizer", "Adagrad", "--learningRate", "0.01", "--maxGenerations", "20"])
 if r!=0:
   exit(r)
 
-r = call(["python3", "run-rnn.py", "--optimizer", "Adam", "--learningRate", "0.001", "--maxGenerations", "10"])
+r = call(["python3", "run-rnn.py", "--optimizer", "Adam", "--learningRate", "0.001", "--maxGenerations", "20"])
 if r!=0:
   exit(r)
 
-r = call(["python3", "run-rnn.py", "--optimizer", "MADGRAD", "--learningRate", "0.001", "--maxGenerations", "10"])
+r = call(["python3", "run-rnn.py", "--optimizer", "MADGRAD", "--learningRate", "0.002", "--maxGenerations", "30"])
 if r!=0:
   exit(r)
       
-r = call(["python3", "run-rnn.py", "--optimizer", "RMSProp", "--learningRate", "0.001", "--maxGenerations", "10"])
+r = call(["python3", "run-rnn.py", "--optimizer", "RMSProp", "--learningRate", "0.001", "--maxGenerations", "20"])
 if r!=0:
   exit(r)
 
-r = call(["python3", "run-rnn.py", "--optimizer", "Adam", "--learningRate", "0.001", "--rnnType", "LSTM", "--maxGenerations", "10"])
+r = call(["python3", "run-rnn.py", "--optimizer", "Adam", "--learningRate", "0.001", "--rnnType", "LSTM", "--maxGenerations", "20"])
 if r!=0:
   exit(r)
     

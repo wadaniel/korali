2dfffdbfdc6efbbe0512d9373290881874fdcae9
Sergio Martin <martiser@ethz.ch>
2021-06-06 13:37:11 +0200

    Updating unit test for adabelief

--- tests/unit/modules/solver/optimizers.cpp
+++ tests/unit/modules/solver/optimizers.cpp
@@ -8,10 +8,13 @@ namespace
  using namespace korali::solver;
  using namespace korali::solver::optimizer;
 
- TEST(Conduit, SequentialConduit)
+ TEST(optimizers, AdaBelief)
  {
   // Creating base experiment
   Experiment e;
+  auto& experimentJs = e._js.getJson();
+
+  // Creating initial variable
   Variable v;
   e._variables.push_back(&v);
   e["Variables"][0]["Name"] = "Var 1";
@@ -32,32 +35,155 @@ namespace
   // Covering variable functions (no effect)
   ASSERT_NO_THROW(opt->applyVariableDefaults());
 
-  // Setting up optimizer
+  // Backup the correct base configuration
+  auto baseOptJs = optimizerJs;
+  auto baseExpJs = experimentJs;
+
+  // Setting up optimizer correctly
   ASSERT_NO_THROW(opt->setConfiguration(optimizerJs));
- }
 
- TEST(Conduit, VariableFails)
- {
-  // Creating base experiment
-  Experiment e;
-  Variable v;
+  // Testing infinite initial value fail
   v._initialValue = std::numeric_limits<double>::infinity();
-  e._variables.push_back(&v);
-  e["Variables"][0]["Name"] = "Var 1";
-  e["Variables"][0]["Lower Bound"] = 0.0;
-  e["Variables"][0]["Upper Bound"] = 1.0;
-
-
-  // Creating optimizer configuration Json
-  knlohmann::json optimizerJs;
-  optimizerJs["Type"] = "Optimizer/AdaBelief";
-
-  // Creating module
-  AdaBelief* opt;
-  ASSERT_NO_THROW(opt = dynamic_cast<AdaBelief *>(Module::getModule(optimizerJs, &e)));
-
-  // A complaint for initial value to be Infinity should happen now
   ASSERT_ANY_THROW(opt->setInitialConfiguration());
+
+  // Testing initial configuration success
+  v._initialValue = 1.0;
+  ASSERT_NO_THROW(opt->setInitialConfiguration());
+
+  // Testing optional parameters
+  optimizerJs = baseOptJs;
+  experimentJs = baseExpJs;
+  optimizerJs["Current Variable"] = std::vector<double>({ 2.0 });
+  ASSERT_NO_THROW(opt->setConfiguration(optimizerJs));
+
+  optimizerJs = baseOptJs;
+  experimentJs = baseExpJs;
+  optimizerJs["Current Variable"] = 2.0;
+  ASSERT_ANY_THROW(opt->setConfiguration(optimizerJs));
+
+  optimizerJs = baseOptJs;
+  experimentJs = baseExpJs;
+  optimizerJs["Gradient"] = std::vector<double>({ 2.0 });
+  ASSERT_NO_THROW(opt->setConfiguration(optimizerJs));
+
+  optimizerJs = baseOptJs;
+  experimentJs = baseExpJs;
+  optimizerJs["Gradient"] = std::vector<double>({ 2.0 });
+  ASSERT_NO_THROW(opt->setConfiguration(optimizerJs));
+
+  optimizerJs = baseOptJs;
+  experimentJs = baseExpJs;
+  optimizerJs["Gradient"] = 2.0;
+  ASSERT_ANY_THROW(opt->setConfiguration(optimizerJs));
+
+  optimizerJs = baseOptJs;
+  experimentJs = baseExpJs;
+  optimizerJs["Gradient Norm"] = std::vector<double>({ 2.0 });
+  ASSERT_ANY_THROW(opt->setConfiguration(optimizerJs));
+
+  optimizerJs = baseOptJs;
+  experimentJs = baseExpJs;
+  optimizerJs["Gradient Norm"] = 2.0;
+  ASSERT_NO_THROW(opt->setConfiguration(optimizerJs));
+
+  optimizerJs = baseOptJs;
+  experimentJs = baseExpJs;
+  optimizerJs["First Moment"] = std::vector<double>({ 2.0 });
+  ASSERT_NO_THROW(opt->setConfiguration(optimizerJs));
+
+  optimizerJs = baseOptJs;
+  experimentJs = baseExpJs;
+  optimizerJs["First Moment"] = 2.0;
+  ASSERT_ANY_THROW(opt->setConfiguration(optimizerJs));
+
+  optimizerJs = baseOptJs;
+  experimentJs = baseExpJs;
+  optimizerJs["Bias Corrected First Moment"] = std::vector<double>({ 2.0 });
+  ASSERT_NO_THROW(opt->setConfiguration(optimizerJs));
+
+  optimizerJs = baseOptJs;
+  experimentJs = baseExpJs;
+  optimizerJs["Bias Corrected First Moment"] = 2.0;
+  ASSERT_ANY_THROW(opt->setConfiguration(optimizerJs));
+
+  optimizerJs = baseOptJs;
+  experimentJs = baseExpJs;
+  optimizerJs["Second Central Moment"] = std::vector<double>({ 2.0 });
+  ASSERT_NO_THROW(opt->setConfiguration(optimizerJs));
+
+  optimizerJs = baseOptJs;
+  experimentJs = baseExpJs;
+  optimizerJs["Second Central Moment"] = 2.0;
+  ASSERT_ANY_THROW(opt->setConfiguration(optimizerJs));
+
+  optimizerJs = baseOptJs;
+  experimentJs = baseExpJs;
+  optimizerJs["Bias Corrected Second Central Moment"] = std::vector<double>({ 2.0 });
+  ASSERT_NO_THROW(opt->setConfiguration(optimizerJs));
+
+  optimizerJs = baseOptJs;
+  experimentJs = baseExpJs;
+  optimizerJs["Bias Corrected Second Central Moment"] = 2.0;
+  ASSERT_ANY_THROW(opt->setConfiguration(optimizerJs));
+
+  optimizerJs = baseOptJs;
+  experimentJs = baseExpJs;
+  optimizerJs["Beta1"] = std::vector<double>({ 2.0 });
+  ASSERT_ANY_THROW(opt->setConfiguration(optimizerJs));
+
+  optimizerJs = baseOptJs;
+  experimentJs = baseExpJs;
+  optimizerJs["Beta1"] = 2.0;
+  ASSERT_NO_THROW(opt->setConfiguration(optimizerJs));
+
+  optimizerJs = baseOptJs;
+  experimentJs = baseExpJs;
+  optimizerJs["Beta2"] = std::vector<double>({ 2.0 });
+  ASSERT_ANY_THROW(opt->setConfiguration(optimizerJs));
+
+  optimizerJs = baseOptJs;
+  experimentJs = baseExpJs;
+  optimizerJs["Beta2"] = 2.0;
+  ASSERT_NO_THROW(opt->setConfiguration(optimizerJs));
+
+  optimizerJs = baseOptJs;
+  experimentJs = baseExpJs;
+  optimizerJs["Eta"] = std::vector<double>({ 2.0 });
+  ASSERT_ANY_THROW(opt->setConfiguration(optimizerJs));
+
+  optimizerJs = baseOptJs;
+  experimentJs = baseExpJs;
+  optimizerJs["Epsilon"] = 2.0;
+  ASSERT_NO_THROW(opt->setConfiguration(optimizerJs));
+
+  optimizerJs = baseOptJs;
+  experimentJs = baseExpJs;
+  optimizerJs["Termination Criteria"]["Max Min Norm"] = std::vector<double>({ 1.0 });
+  ASSERT_ANY_THROW(opt->setConfiguration(optimizerJs));
+
+  optimizerJs = baseOptJs;
+  experimentJs = baseExpJs;
+  optimizerJs["Termination Criteria"]["Min Gradient Norm"] = 1.0;
+  ASSERT_NO_THROW(opt->setConfiguration(optimizerJs));
+
+  optimizerJs = baseOptJs;
+  experimentJs = baseExpJs;
+  optimizerJs["Termination Criteria"]["Max Gradient Norm"] = std::vector<double>({ 2.0 });
+  ASSERT_ANY_THROW(opt->setConfiguration(optimizerJs));
+
+  optimizerJs = baseOptJs;
+  experimentJs = baseExpJs;
+  optimizerJs["Termination Criteria"]["Max Gradient Norm"] = 2.0;
+  ASSERT_NO_THROW(opt->setConfiguration(optimizerJs));
+
+  // Testing termination criteria
+  e._currentGeneration = 2;
+  opt->_gradientNorm = 0.0;
+  ASSERT_TRUE(opt->checkTermination());
+  opt->_gradientNorm = 1.5;
+  ASSERT_FALSE(opt->checkTermination());
+  opt->_gradientNorm = 3.0;
+  ASSERT_TRUE(opt->checkTermination());
  }
 
 } // namespace

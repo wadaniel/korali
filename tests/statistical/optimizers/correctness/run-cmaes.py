efe5a7460a9418cf627e4cbf2470329f746aec3a
Sergio Martin <martiser@ethz.ch>
2021-06-08 17:31:54 +0200

    Adding DEA tests

--- tests/statistical/optimizers/correctness/run-cmaes.py
+++ tests/statistical/optimizers/correctness/run-cmaes.py
@@ -159,3 +159,29 @@ k = korali.Engine()
 k.run(e)
 
 checkMin(e, 0.22942553779431113, 1e-3)
+
+### Running With Different Mu Types
+
+e = korali.Experiment()
+e["Problem"]["Type"] = "Optimization"
+e["Problem"]["Objective Function"] = evalmodel
+
+e["Variables"][0]["Name"] = "X"
+e["Variables"][0]["Lower Bound"] = -10.0
+e["Variables"][0]["Upper Bound"] = +10.0
+
+e["Solver"]["Type"] = "Optimizer/CMAES"
+e["Solver"]["Population Size"] = 64
+e["Solver"]["Termination Criteria"]["Max Generations"] = 100
+e["Solver"]["Mu Type"] = "Equal"
+
+e["Console Output"]["Frequency"] = 10
+e["Console Output"]["Verbosity"] = "Detailed"
+e["File Output"]["Enabled"] = False
+e["Random Seed"] = 1337
+
+k = korali.Engine()
+k.run(e)
+
+checkMin(e, 0.22942553779431113, 1e-3)
+

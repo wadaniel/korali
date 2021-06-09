7defe4a147a780fd95ba26438d3182c4d150a07c
Sergio Martin <martiser@ethz.ch>
2021-06-09 11:00:34 +0200

    Updating CMAES tests

--- tests/statistical/optimizers/correctness/helpers/helpers.py
+++ tests/statistical/optimizers/correctness/helpers/helpers.py
@@ -6,3 +6,8 @@ def checkMin(k, expectedMinimum, tol):
   minimum = k["Solver"]["Best Ever Value"]
   assert np.isclose(expectedMinimum, minimum, atol = tol), "Minimum {0} "\
           "deviates from true min {1} by more than {2}".format(minimum, expectedMinimum, tol)
+
+def checkInfeasible(k, expectedMinimum):
+  minimum = k["Solver"]["Infeasible Sample Count"]
+  assert np.less(expectedMinimum, minimum, ), "Minimum {0} "\
+          "is not less than {1}".format(minimum, expectedMinimum)

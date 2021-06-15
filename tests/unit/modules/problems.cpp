9e178ac41ebea8e537b1d9240f444c376b9e1eb4
Sergio Martin <martiser@ethz.ch>
2021-06-15 09:23:32 +0200

    updating bayesian tests

--- tests/unit/modules/problems.cpp
+++ tests/unit/modules/problems.cpp
@@ -1071,4 +1071,71 @@ namespace
   problemJs["Likelihood Model"] = 1;
   ASSERT_NO_THROW(pObj->setConfiguration(problemJs));
  }
+
+ TEST(Problem, BayesianReference)
+ {
+  // Creating base experiment
+  Experiment e;
+  auto& experimentJs = e._js.getJson();
+
+  knlohmann::json uniformDistroJs;
+  uniformDistroJs["Type"] = "Univariate/Uniform";
+  uniformDistroJs["Minimum"] = 0.0;
+  uniformDistroJs["Maximum"] = 1.0;
+  auto uniformGenerator = dynamic_cast<korali::distribution::univariate::Uniform*>(korali::Module::getModule(uniformDistroJs, &e));
+  uniformGenerator->applyVariableDefaults();
+  uniformGenerator->applyModuleDefaults(uniformDistroJs);
+  uniformGenerator->setConfiguration(uniformDistroJs);
+  e._distributions.push_back(uniformGenerator);
+  e._distributions[0]->_name = "Uniform";
+
+  // Creating initial variable
+  Variable v;
+  v._precomputedValues = std::vector<double>({0.0});
+  e._variables.push_back(&v);
+
+  // Configuring Problem
+  Reference* pObj;
+  knlohmann::json problemJs;
+  problemJs["Type"] = "Bayesian/Reference";
+  problemJs["Computational Model"] = 0;
+  problemJs["Likelihood Model"] = "Normal";
+  problemJs["Reference Data"] = std::vector<double>({ 0.0 });
+  e["Solver"]["Type"] = "Sampler/TMCMC";
+
+  e["Variables"][0]["Name"] = "Var X";
+  e["Variables"][0]["Number Of Gridpoints"] = 10;
+  e["Variables"][0]["Prior Distribution"] = "Uniform";
+
+  ASSERT_NO_THROW(pObj = dynamic_cast<Reference *>(Module::getModule(problemJs, &e)));
+  e._problem = pObj;
+
+  // Defaults should be applied without a problem
+  ASSERT_NO_THROW(pObj->applyModuleDefaults(problemJs));
+
+  // Covering variable functions (no effect)
+  pObj->applyVariableDefaults();
+  ASSERT_NO_THROW(pObj->applyVariableDefaults());
+
+  // Trying to run unknown operation
+  Sample s;
+  ASSERT_ANY_THROW(pObj->runOperation("Unknown", s));
+
+  // Backup the correct base configuration
+  auto baseProbJs = problemJs;
+  auto baseExpJs = experimentJs;
+
+  // Testing correct configuration
+  ASSERT_NO_THROW(pObj->setConfiguration(problemJs));
+
+  // Testing unrecognized solver
+  problemJs = baseProbJs;
+  experimentJs = baseExpJs;
+  e["Solver"]["Type"] = "";
+  ASSERT_ANY_THROW(pObj->setConfiguration(problemJs));
+
+  // Testing initialization
+  ASSERT_NO_THROW(pObj->initialize());
+
+ }
 } // namespace

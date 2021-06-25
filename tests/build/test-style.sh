b1c5eb41c1d8c0de2ff919506c3ccac4af457dfd
Sergio Martin <martiser@ethz.ch>
2021-06-25 10:06:43 +0200

    Updating ubuntu img

--- tests/build/test-style.sh
+++ tests/build/test-style.sh
@@ -4,6 +4,7 @@ pushd ../../tools/style
 
 ./style_cxx.sh check
 if [ $? -ne 0 ]; then
- echo "[Korali] Error: Could not validate C++ style. Check docs/build.log" 
+ echo "[Korali] Error: Could not validate C++ style. clang-format version used:" 
+ clang-format --version
  exit 1
 fi

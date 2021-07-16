8ee9eaf0c5788bbae8791ef50919a89e86707eb1
Pascal Weber <webepasc@student.ethz.ch>
2021-07-16 15:32:23 +0200

    added evaluation script

---
+++ examples/study.cases/CUP2D/swimmers/eval-vracer-swimmer.cpp
@@ -0,0 +1,60 @@
+// Select which environment to use
+#include "_model/swimmerEnvironment.hpp"
+#include "korali.hpp"
+
+std::string _resultsPath;
+
+int main(int argc, char *argv[])
+{
+  // Gathering actual arguments from MPI
+  int provided;
+  MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &provided);
+  if (provided != MPI_THREAD_FUNNELED)
+  {
+    printf("Error initializing MPI\n");
+    exit(-1);
+  }
+
+  // Storing parameters
+  _argc = argc;
+  _argv = argv;
+
+  // Getting number of workers
+  int N = 1;
+  MPI_Comm_size(MPI_COMM_WORLD, &N);
+  N = N - 1; // Minus one for Korali's engine
+
+  // Initializing CUP
+  _environment = new Simulation(_argc, _argv);
+  _environment->init();
+
+  // Setting results path
+  std::string trainingResultsPath = "_trainingResults/";
+  std::string testingResultsPath = "_testingResults/";
+
+  // Creating Korali experiment
+  auto e = korali::Experiment();
+
+  // Check if there is log files to continue training
+  auto found = e.loadState(trainingResultsPath+"/latest");
+  if (found == true) printf("[Korali] Continuing execution from previous run...\n");
+  else { fprintf(stderr, "[Korali] Error: cannot find previous results\n"); exit(0); } 
+
+  // Creating Korali engine
+  auto k = korali::Engine();
+
+  // Set conduit and pass MPI communicator
+  k["Conduit"]["Type"] = "Distributed";
+  korali::setKoraliMPIComm(MPI_COMM_WORLD);
+
+  // Adding custom setting to run the environment dumping the state files during testing
+  e["Problem"]["Custom Settings"]["Dump Frequency"] = 0.1;
+  e["Problem"]["Custom Settings"]["Dump Path"] = testingResultsPath;
+  e["Problem"]["Environment Function"] = &runEnvironment;
+  e["File Output"]["Path"] = trainingResultsPath;
+  e["Solver"]["Testing"]["Policy"] = e["Solver"]["Best Training Hyperparameters"];
+  e["Solver"]["Mode"] = "Testing";
+  for (int i = 0; i < N; i++) e["Solver"]["Testing"]["Sample Ids"][i] = i;
+
+  k.run(e);
+}

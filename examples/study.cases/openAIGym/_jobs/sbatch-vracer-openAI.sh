ae5ad7a8df94e3d8f240d9be0db57daa72d72fa3
wadaniel <wadaniel@ethz.ch>
2021-07-26 17:48:03 +0200

    cleanup

--- examples/study.cases/openAIGym/_jobs/sbatch-vracer-openAI.sh
+++ examples/study.cases/openAIGym/_jobs/sbatch-vracer-openAI.sh
@@ -22,7 +22,7 @@ cat > run.sh <<EOF
 #SBATCH --constraint=gpu
 #SBATCH --account=s929
 
-RUNPATH=$SCRATCH/OpenAI_cscale_ows0001_small/$ENV/\$SLURM_JOB_ID
+RUNPATH=$SCRATCH/OpenAI_VRACER/$ENV/\$SLURM_JOB_ID
 mkdir -p \$RUNPATH
 
 pushd ..

2e366bcb32c8b298ec2ab3c9a081f155529eb21e
wadaniel <wadaniel@ethz.ch>
2021-08-16 13:07:30 +0200

    doc

--- examples/learning/reinforcement/ABF2D/run-vracer.py
+++ examples/learning/reinforcement/ABF2D/run-vracer.py
@@ -49,7 +49,7 @@ e["Solver"]["Experiences Between Policy Updates"] = 1
 e["Solver"]["Episodes Per Generation"] = 50 
 e["Solver"]["Learning Rate"] = 1e-4
 e["Solver"]["Mini Batch"]["Size"] = 128
-e["Solver"]["Policy"]["Distribution"] = "Squashed Normal"
+e["Solver"]["Policy"]["Distribution"] = "Normal"
 e["Solver"]["Experience Replay"]["Start Size"] = 32768
 e["Solver"]["Experience Replay"]["Maximum Size"] = 131072
 

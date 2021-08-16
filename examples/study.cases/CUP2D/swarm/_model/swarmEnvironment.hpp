3ab9557956d0062a5b33442177959561c228931b
Pascal Weber <webepasc@student.ethz.ch>
2021-08-16 08:40:47 +0200

    ipdated runscripts

--- examples/study.cases/CUP2D/swarm/_model/swarmEnvironment.hpp
+++ examples/study.cases/CUP2D/swarm/_model/swarmEnvironment.hpp
@@ -9,15 +9,20 @@
 #include "Utils/BufferedLogger.h"
 
 // Settings for different setups
-#define NAGENTS 9
-#define XMAX 2
-#define YMIN 0.6
-#define YMAX 1.4
+// #define NAGENTS 4
+// #define XMAX 1.4
+// #define YMIN 0.7
+// #define YMAX 1.3
 
-#define NAGENTS 16
-#define XMAX 2.6
-#define YMIN 0.5
-#define YMAX 1.5
+// #define NAGENTS 9
+// #define XMAX 2
+// #define YMIN 0.6
+// #define YMAX 1.4
+
+// #define NAGENTS 16
+// #define XMAX 2.6
+// #define YMIN 0.5
+// #define YMAX 1.5
 
 #define NAGENTS 25
 #define XMAX 3.2
--- examples/study.cases/CUP2D/swimmers/settings.sh
+++ examples/study.cases/CUP2D/swimmers/settings.sh
@@ -10,12 +10,15 @@ EXTENT=${EXTENT:-4}
 CFL=${CFL:-0.4}
 PT=${PT:-1e-5}
 PTR=${PTR:-1e-2}
-PR=${PR:-0}
+PR=${PR:-5}
 
 # Defaults for follower
 LENGTH=${LENGTH:-0.2}
 PERIOD=${PERIOD:-1} # 0.2 for NACA
 XPOSFOLLOWER=${XPOSFOLLOWER:-0.9}
+
+# stefanfish Re=1'000 <-> NU=0.00001125
+NU=${NU:-0.00004}
 
 # Settings for Obstacle
 OBSTACLE=${OBSTACLE:-halfDisk}
@@ -35,8 +38,6 @@ stefanfish L=$LENGTH T=$PERIOD xpos=$XPOSFOLLOWER"
 	echo $OBJECTS
 	# halfDisk Re=1'000 <-> NU=0.000018
 	# NU=${NU:-0.000018}
-	# stefanfish Re=1'000 <-> NU=0.00001125
-	NU=${NU:-0.00004}
 elif [ "$OBSTACLE" = "NACA" ]
 then
 	echo "###############################"
@@ -52,8 +53,6 @@ stefanfish L=$LENGTH T=$PERIOD xpos=$XPOSFOLLOWER"
 	echo $OBJECTS
 	# NACA Re=1'000 <-> NU=0.000018
 	# NU=${NU:-0.000018}
-	# stefanfish Re=1'000 <-> NU=0.00001125
-	NU=${NU:-0.00004}
 elif [ "$OBSTACLE" = "stefanfish" ]
 then
 	echo "###############################"
@@ -62,8 +61,6 @@ then
 	OBJECTS="stefanfish L=$LENGTH T=$PERIOD xpos=$XPOSLEADER bFixed=1 pid=1
 stefanfish L=$LENGTH T=$PERIOD xpos=$XPOSFOLLOWER"
 	echo $OBJECTS
-	# stefanfish Re=1'000 <-> NU=0.00001125
-	NU=${NU:-0.00004}
 fi
 
 echo "----------------------------"

2a403bc50a76d6ca2c1d0ca978b086568c46efef
wadaniel <wadaniel@ethz.ch>
2021-08-16 09:20:45 +0200

    cleanup

--- examples/study.cases/openAIGym/_jobs/submit_batch.sh
+++ examples/study.cases/openAIGym/_jobs/submit_batch.sh
@@ -1,10 +1,6 @@
-for E in Ant-v2 HalfCheetah-v2 Hopper-v2 Humanoid-v2 HumanoidStandup-v2 Reacher-v2 Swimmer-v2 Walker2d-v2;  # full run
-#for E in Ant-v2 HalfCheetah-v2 Hopper-v2 Walker2d-v2;  # small run 1
-#for E in Humanoid-v2 HumanoidStandup-v2 Reacher-v2 Swimmer-v2;  # small run 2
+for E in Ant-v2 HalfCheetah-v2 Hopper-v2 Humanoid-v2 HumanoidStandup-v2 Reacher-v2 Swimmer-v2 Walker2d-v2;
 do 
-#    for D in "Normal" "Clipped Normal" "Squashed Normal" "Truncated Normal"; 
-#    for D in "Normal" "Clipped Normal" 
-    for D in "Squashed Normal" 
+    for D in "Normal" "Clipped Normal" "Squashed Normal" "Truncated Normal"; 
     do 
         export ENV=$E
         export DIS="$D" 

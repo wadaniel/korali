6d9399805f7d1159b3988e94de33a0948baed6a5
Joan <joanclapesroig17@icloud.com>
2021-09-16 11:14:04 +0200

    run-vracer-cartpole

--- examples/study.cases/modelBasedRL/install_deps.sh
+++ examples/study.cases/modelBasedRL/install_deps.sh
@@ -1,18 +1,18 @@
 git clone git@gitlab.ethz.ch:mavt-cse/modelBasedRL.git
 mkdir Utils/
-mkdir _modelCoord/
+mkdir _model_cartpole/
 cd modelBasedRL
-cp model.py ../model.py
-cp env.py ../_modelCoord/env.py
-cp cartpole.py ../_modelCoord/cartpole.py
-cp grid_search.py ../Utils/grid_search.py
-cp group_results_onlyreal.py ../Utils/group_results_onlyreal.py
-cp group_results.py ../Utils/group_results.py
-cp plot_best_surrogate_based_model.py ../Utils/plot_best_surrogate_based_model.py
-cp plot_results.py ../Utils/plot_results.py
-cp plot_updates.py ../Utils/plot_updates.py
-cp plot_optimize_3d.py ../Utils/plot_optimize_3d.py
-cp render_cartpole.py ../Utils/render_cartpole.py
+cp Cartpole/model.py ../_model_cartpole/model.py
+cp Cartpole/env.py ../_model_cartpole/env.py
+cp Cartpole/cartpole.py ../_model_cartpole/cartpole.py
+cp Cartpole/grid_search.py ../Utils/grid_search.py
+cp Cartpole/group_results_onlyreal.py ../Utils/group_results_onlyreal.py
+cp Cartpole/group_results.py ../Utils/group_results.py
+cp Cartpole/plot_best_surrogate_based_model.py ../Utils/plot_best_surrogate_based_model.py
+cp Cartpole/plot_results.py ../Utils/plot_results.py
+cp Cartpole/plot_updates.py ../Utils/plot_updates.py
+cp Cartpole/plot_optimize_3d.py ../Utils/plot_optimize_3d.py
+cp Cartpole/render_cartpole.py ../Utils/render_cartpole.py
 mkdir ../Results/
 mkdir ../Visualization/
 cd ..

c8497c99f5bcd6b1411cebb53ad3b47d39b53f3e
mzeqiri1717 <mzeqiri@panda.ethz.ch>
2021-09-22 09:37:02 +0200

    pettingzoo

---
+++ examples/study.cases/pettingZoo/_model/agent.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python3
+
+import gym
+#import pyBulletEnvironments
+import math
+from HumanoidWrapper import HumanoidWrapper
+from AntWrapper import AntWrapper
+import pdb
+import numpy as np
+
+def initEnvironment(e, envName, moviePath = ''):
+
+ # Creating environment 
+ if (envName ==  'Waterworld'):
+    from pettingzoo.sisl import waterworld_v3
+    env = waterworld_v3.env()
+    #print(env.action_spaces.values())
+    #pdb.set_trace()
+    stateVariableCount = 242
+    actionVariableCount = 2
+    eps = 10 ** (-20)
+    obs_upper = 2 * math.sqrt(2)
+    obs_low = -1 * math.sqrt(2)
+    ac_upper = 0.01 - eps
+    ac_low = -0.01 + eps
+    numIndividuals = 5
+ 
+ # Handling special cases
+ 
+ if (envName == 'Humanoid-v2'):
+  env = HumanoidWrapper(env)
+  
+ if (envName == 'HumanoidStandup-v2'):
+  env = HumanoidWrapper(env)
+  
+ if (envName == 'Ant-v2'):
+  env = AntWrapper(env)
+
+ 
+  
+ # Re-wrapping if saving a movie
+ if (moviePath != ''):
+  env = gym.wrappers.Monitor(env, moviePath, force=True)
+ 
+ ### Defining problem configuration for openAI Gym environments
+ e["Problem"]["Type"] = "Reinforcement Learning / Continuous"
+ e["Problem"]["Environment Function"] = lambda x : agent(x, env)
+ e["Problem"]["Custom Settings"]["Print Step Information"] = "Disabled"
+ e["Problem"]["Training Reward Threshold"] = math.inf
+ e["Problem"]["Policy Testing Episodes"] = 20
+ e["Problem"]["Agents Per Environment"] = numIndividuals
+ 
+ # Getting environment variable counts
+ #commented this out
+ #stateVariableCount = env.observation_space.shape[0]
+ #actionVariableCount = env.action_space.shape[0]
+ 
+ # Generating state variable index list
+ stateVariablesIndexes = range(stateVariableCount)
+ 
+ # Handling Environment-Specific Configuration
+ 
+ if (envName == 'Ant-v2'):
+  stateVariableCount = 27
+  stateVariablesIndexes = range(stateVariableCount) 
+ 
+ # Defining State Variables
+ 
+ for i in stateVariablesIndexes:
+  e["Variables"][i]["Name"] = "State Variable " + str(i)
+  e["Variables"][i]["Type"] = "State"
+  e["Variables"][i]["Lower Bound"] = float(obs_low)
+  e["Variables"][i]["Upper Bound"] = float(obs_upper)
+  
+ # Defining Action Variables
+ 
+ for i in range(actionVariableCount):
+  e["Variables"][stateVariableCount + i]["Name"] = "Action Variable " + str(i)
+  e["Variables"][stateVariableCount + i]["Type"] = "Action"
+  e["Variables"][stateVariableCount + i]["Lower Bound"] = float(ac_low)
+  e["Variables"][stateVariableCount + i]["Upper Bound"] = float(ac_upper)
+  e["Variables"][stateVariableCount + i]["Initial Exploration Noise"] = math.sqrt(0.2)
+ 
+ ### Defining Termination Criteria
+
+ e["Solver"]["Termination Criteria"]["Testing"]["Target Average Reward"] = math.inf
+ 
+'''
+  for agent in env.agent_iter():
+      observation, reward, done, info = env.last()
+      action = policy(observation, agent)
+      env.step(action)
+'''
+
+def agent(s, env):
+
+ if (s["Custom Settings"]["Print Step Information"] == "Enabled"):
+  printStep = True
+ else:
+  printStep = False
+
+ #s["State"] = env.reset().tolist()
+
+ 
+ env.reset()
+ states = []
+ 
+ for ag in env.agents:
+  state = env.observe(ag).tolist()
+  states.append(state)
+  
+
+ 
+ s["State"] = states
+ step = 0
+ done = False
+
+ # Storage for cumulative reward
+ cumulativeReward = 0.0
+ 
+ overSteps = 0
+  
+ while not done and step < 500:
+
+  # Getting new action
+
+
+  s.update()
+  
+  # Printing step information    
+  if (printStep):  print('[Korali] Frame ' + str(step), end = '')
+  
+  
+  actions = s["Action"]
+  rewards = []
+  for ag in env.agents:
+   observation, reward, done, info = env.last()
+   rewards.append(reward)
+   if done == True:
+    continue
+   action = actions.pop(0)
+   #pdb.set_trace()
+   
+   #print(action)
+   #pdb.set_trace()
+   
+   
+   env.step(np.array(action,dtype= 'float32'))
+
+
+  
+  
+ 
+  # Getting Reward
+  s["Reward"] = rewards
+  
+  # Printing step information
+  #if (printStep):  print(' - State: ' + str(state) + ' - Action: ' + str(action))
+  #cumulativeReward = cumulativeReward + reward 
+  #if (printStep):  print(' - Cumulative Reward: ' + str(cumulativeReward))
+   
+  # Storing New State
+  states = []
+ 
+  for ag in env.agents:
+   state = env.observe(ag).tolist()
+   states.append(state)
+  s["State"] = states
+   
+  # Advancing step counter
+  step = step + 1
+
+ # Setting termination status
+ if (not env.agents):
+  s["Termination"] = "Terminal"
+ else:
+  s["Termination"] = "Truncated"
+

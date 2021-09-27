9d5ad2d2db13198b4f3f37aca8144d045cec7019
Daniel Waelchli <wadaniel@ethz.ch>
2021-09-27 23:17:26 +0200

    fixed sbatch script

--- examples/study.cases/RLCMAES/sbatch-vracer-rlcmaes.sh
+++ examples/study.cases/RLCMAES/sbatch-vracer-rlcmaes.sh
@@ -1,5 +1,29 @@
 #!/bin/bash -l
 
+#dims=(2 4 8 16 32 64)
+#pops=(8 16 32 64 128 256)
+#dim=8
+#pop=32
+#dim=16
+#pop=64
+#dim=32
+#pop=128
+#dim=64
+#pop=256
+dim=128
+pop=512
+
+
+run=0
+noise=0.0
+obj="random"
+exp=5000000
+reps=100
+version=1
+outdir="figures"
+
+objectives=("fsphere" "felli" "fcigar" "ftablet" "fcigtab" "ftwoax" "fdiffpow" "rosenbrock" "fparabr" "fsharpr")
+
 cat > run.sh <<EOF
 #!/bin/bash -l
 #SBATCH --job-name="RLCMAES_VRACER_$dim"
@@ -14,56 +38,34 @@ cat > run.sh <<EOF
 #SBATCH --constraint=gpu
 #SBATCH --account=s929
 
-RUNPATH=$SCRATCH/RLCMAES/$dim/\$SLURM_JOB_ID
+RUNPATH=$SCRATCH/RLCMAES/\$SLURM_JOB_ID
 mkdir -p \$RUNPATH
 
 cat run-vracer.py
 
 cp run-vracer.py \$RUNPATH
+cp run-env-cmaes.py \$RUNPATH
+cp read-history.py \$RUNPATH
 cp -r _environment/ \$RUNPATH
 
 pushd \$RUNPATH
 
-run=0
-noise=0.0
-obj="random"
-exp=10000000
-reps=100
-version=1
-outdir="figures"
-
-
-#dims=(2 4 8 16 32 64)
-#pops=(8 16 32 64 128 256)
-
-#dims=(32)
-#pops=(128)
-
-dims=(64)
-pops=(256)
-
-
-objectives=("fsphere" "felli" "fcigar" "ftablet" "fcigtab" "ftwoax" "fdiffpow" "rosenbrock" "fparabr" "fsharpr")
-
 mkdir -p ${outdir}
 
-for i in "${!dims[@]}";
+python run-vracer.py --noise $noise --obj $obj --dim $dim --pop $pop --run $run --exp $exp --version=$version
+python -m korali.rlview --dir "_vracer_${obj}_${dim}_${pop}_${noise}_${run}/" --out "${outdir}/${obj}_${dim}_${pop}_${run}.png"
+
+python run-vracer.py --noise $noise --obj $obj --dim ${dim} --pop ${pop} --run $run --eval --reps $reps --version=$version
+
+for o in "${objectives[@]}";
 do
-    python run-vracer.py --noise $noise --obj $obj --dim ${dims[i]} --pop ${pops[i]} --run $run --exp $exp --version=$version
-    python -m korali.rlview --dir "_vracer_${obj}_${dims[i]}_${pops[i]}_${noise}_${run}/" --out "${outdir}/${obj}_${dims[i]}_${pops[i]}_${run}.png"
-    
-    python run-vracer.py --noise $noise --obj $obj --dim ${dims[i]} --pop ${pops[i]} --run $run --eval --reps $reps --version=$version
-
-    for o in "${objectives[@]}";
-    do
-        python run-env-cmaes.py --noise $noise --obj $o --dim ${dims[i]} --pop ${pops[i]} --run $run --eval --reps $reps; 
-        vracerfile="history_vracer_${o}_${dims[i]}_${pops[i]}_${noise}_${run}.npz"
-        cmaesfile="history_cmaes_${o}_${dims[i]}_${pops[i]}_${noise}_${run}.npz"
-        outfile="${outdir}/history_${o}_${dims[i]}_${pops[i]}_${noise}_${run}.png"
-        python read-history.py --files ${vracerfile} ${cmaesfile} --out ${outfile}
-    done;
-
+    python run-env-cmaes.py --noise $noise --obj $o --dim ${dim} --pop ${pop} --run $run --eval --reps $reps; 
+    vracerfile="history_vracer_${o}_${dim}_${pop}_${noise}_${run}.npz"
+    cmaesfile="history_cmaes_${o}_${dim}_${pop}_${noise}_${run}.npz"
+    outfile="${outdir}/history_${o}_${dim}_${pop}_${noise}_${run}.png"
+    python read-history.py --files ${vracerfile} ${cmaesfile} --out ${outfile}
 done;
+
 
 popd
 

bcf0d4a3a4c5af011bab9917c0df80e425976b96
Daniel Waelchli <wadaniel@ethz.ch>
2021-10-04 14:57:00 +0200

    openai setup, added softrelu

--- examples/study.cases/openAIGym/settings.sh
+++ examples/study.cases/openAIGym/settings.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Defaults for Options
-#ENV=${ENV:-Ant-v2}
-#DIS=${DIS:-"Normal"}
+ENV=${ENV:-Humanoid-v2}
+DIS=${DIS:-"Clipped Normal"}
 L2=${L2:-0.0}
 OPT=${OPT:-0.1}
 LR=${LR:-0.0001}

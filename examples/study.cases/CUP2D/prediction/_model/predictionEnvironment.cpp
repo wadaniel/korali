07e59f84bdf034cbd1ad2042ea4d41bb81e493e4
sotiris <scatsoul@ethz.ch>
2021-10-19 11:04:14 -0400

    updateRe100

--- examples/study.cases/CUP2D/prediction/_model/predictionEnvironment.cpp
+++ examples/study.cases/CUP2D/prediction/_model/predictionEnvironment.cpp
@@ -8,7 +8,25 @@ int _argc;
 char **_argv;
 std::mt19937 _randomGenerator;
 
-inline std::vector<double> averageBlock(ScalarBlock &scalarBlock, VectorBlock &vectorBlock){
+inline std::vector<double> getBlockCenter(ScalarBlock &scalarBlock, BlockInfo &blockInfo){
+
+  double p[2];
+
+  int ix = ScalarBlock::sizeX/2;
+  int iy = ScalarBlock::sizeY/2;
+
+  blockInfo.pos(p, ix, iy);
+  double xcenter = p[0]-0.5*blockInfo.h;
+  double ycenter = p[1]-0.5*blockInfo.h;
+
+  std::vector<double> center(2, 0);
+  center[0] = xcenter;
+  center[1] = ycenter;
+
+  return center;
+}
+
+inline std::vector<double> getBlockAverage(ScalarBlock &scalarBlock, VectorBlock &vectorBlock){
   
   std::vector<double> blockAverage(3, 0);
   for(int iy=0; iy<VectorBlock::sizeY; ++iy)
@@ -76,7 +94,7 @@ void runEnvironment(korali::Sample &s)
   s["State"] = states;
 
   // Variables for time and step conditions
-  size_t curStep = 0;  // current Step
+  size_t curStep = 0;  // current step
 
   // Setting maximum number of steps before truncation
   size_t maxSteps = 1000;
@@ -84,9 +102,24 @@ void runEnvironment(korali::Sample &s)
   // Starting main environment loop
   bool done = false;
   double t = 0;
-  // const double t_start = 5; // set to tAccel (from Cubism) plus some time to allow information propagation (to predict steady-state)
-  double tNextAct = 0;
+  const double tStart = 30; // set to tAccel (from Cubism) plus some time to allow information propagation (to get to steady-state)
+  double tNextAct = tStart + 0;
   const double dtNextAct = 0.1;
+
+  // Advance simulation to desired time before beginning RL loop
+  while(t < tStart){
+
+    // Computing CFD timestep
+    const double dt = _environment->calcMaxTimestep();
+
+    // Advancing simulation
+    _environment->advance(dt);
+
+    // Stepping forward in time
+    t += dt;
+
+  }
+
   while(curStep < maxSteps && done == false){
 
     // Getting new actions
@@ -98,7 +131,7 @@ void runEnvironment(korali::Sample &s)
     // Updating time to the next action
     tNextAct += dtNextAct;
 
-    // Running simulation until predefined time
+    // Running simulation until next action
     while(t < tNextAct){
 
     // Computing CFD timestep
@@ -107,6 +140,7 @@ void runEnvironment(korali::Sample &s)
     // Advancing simulation
     _environment->advance(dt);
 
+    // Stepping forward in time
     t += dt;
 
     }
@@ -124,16 +158,19 @@ void runEnvironment(korali::Sample &s)
     // Getting number of blocks
     const int nBlocks = presInfo.size();
 
-    // Getting individual block averages for the entire field
+    // Getting individual block averages and centers for the entire field
     std::vector<std::vector<double>> fieldAvg(nBlocks, std::vector<double>(3, 0));
+    // std::vector<std::vector<double>> fieldCenter(nBlocks, std::vector<double>(2, 0));
     for(int b=0; b<nBlocks; ++b){
     
     ScalarBlock & __restrict__ P = *(ScalarBlock*) presInfo[b].ptrBlock;
     VectorBlock & __restrict__ V = *(VectorBlock*)  velInfo[b].ptrBlock;
 
-    auto blockAverage = averageBlock(P, V);
+    auto blockAverage = getBlockAverage(P, V);
+    // auto blockCenter = getBlockCenter(P, presInfo[b]);
 
     fieldAvg[b] = blockAverage;
+    // fieldCenter[b] = blockCenter;
     }
 
     size_t agentID = 0;
@@ -178,11 +215,26 @@ void runEnvironment(korali::Sample &s)
 
       // Computing reward and assigning to container
       for(size_t j = 0; j<3; ++j){
-        rewards[agentID] -= (action[j] - state[j]) * (action[j] - state[j]) / nAgents; // TODO, try different norm & weigths for p,u,v
+        rewards[agentID] -= (action[j] - state[j]) * (action[j] - state[j]); // / nAgents; // TODO, try different norm & weigths for p,u,v
       }
+      /*
+      // Dump p, u, v. One .csv for each curStep, containing all field data for real/predicted quantities.
+      if(s["Mode"] == "Testing" && curStep % 5 == 0){
+        
+        std::string curStepString = std::to_string(curStep);
+        std::string fileID = std::string(4 - curStepString.length(), '0') + curStepString;
+
+        std::ofstream fields;
+        fields.open(fileID + "fields.csv", std::ios_base::app);
+        fields << curStep << "," << t << "," << fieldCenter[a][0] << "," << fieldCenter[a][1] << ","
+               << fieldAvg[a][0] << "," << fieldAvg[a][1] << "," << fieldAvg[a][2] << ","
+               << action[0][0] << "," << action[1][0] << "," << action[2][0] << "\n";
+        fields.close();
+      }
+      */
       agentID++;
     }
-    
+
     s["State"] = states;
     s["Reward"] = rewards;
 
@@ -215,3 +267,5 @@ bool isTerminal()
   
   return false;
 }
+
+// TO DO add randomization, re-run one CFD timestep ahead, add multi-step with own prediction/prediction many steps ahead

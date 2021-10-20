06c2af5410d153f3405b71e96399a3a2f5cbb282
webepasc <webepasc@student.ethz.ch>
2021-10-20 15:39:01 +0200

    run twice for restart

--- examples/features/checkpoint.resume/.test-run.py
+++ examples/features/checkpoint.resume/.test-run.py
@@ -4,7 +4,15 @@ from subprocess import call
 r = call(["python3", "run-sin.py"])
 if r!=0:
  exit(r)
-  
+
+r = call(["python3", "run-sin.py"])
+if r!=0:
+ exit(r)
+
+r = call(["python3", "run-cmaes.py"])
+if r!=0:
+  exit(r)
+
 r = call(["python3", "run-cmaes.py"])
 if r!=0:
   exit(r)
@@ -13,8 +21,16 @@ r = call(["python3", "run-vracer.py"])
 if r!=0:
    exit(r)
 
+r = call(["python3", "run-vracer.py"])
+if r!=0:
+   exit(r)
+
 r = call(["python3", "run-dvracer.py"])
 if r!=0:
    exit(r)
-    
+
+r = call(["python3", "run-dvracer.py"])
+if r!=0:
+   exit(r)
+
 exit(0)

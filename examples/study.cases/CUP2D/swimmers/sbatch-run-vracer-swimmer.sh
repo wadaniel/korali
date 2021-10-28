65268435e9f1aa593d3aefbf60f0ee9735e787df
Pascal Weber <webepasc@student.ethz.ch>
2021-10-28 19:10:12 +0200

    enabled heterogenous jobs

--- examples/study.cases/CUP2D/swimmers/sbatch-run-vracer-swimmer.sh
+++ examples/study.cases/CUP2D/swimmers/sbatch-run-vracer-swimmer.sh
@@ -9,12 +9,12 @@ if [ $# -gt 0 ] ; then
 fi
 
 # number of workers
-# NWORKER=32
-NWORKER=1
+NWORKER=32
+# NWORKER=1
 
 # number of nodes per worker
-# NRANKS=8
-NRANKS=9
+NRANKS=8
+# NRANKS=9
 
 # number of threads per worker
 NUMTHREADS=12
@@ -33,24 +33,28 @@ source settings.sh
 
 cat <<EOF >daint_sbatch
 #!/bin/bash -l
+#SBATCH --account=s929
+#SBATCH --constraint=gpu
 #SBATCH --job-name="${RUNNAME}"
 #SBATCH --output=${RUNNAME}_out_%j.txt
 #SBATCH --error=${RUNNAME}_err_%j.txt
-# #SBATCH --time=24:00:00
-# #SBATCH --partition=normal
-#SBATCH --time=00:30:00
-#SBATCH --partition=debug
+#SBATCH --time=24:00:00
+#SBATCH --partition=normal
+# #SBATCH --time=00:30:00
+# #SBATCH --partition=debug
 #SBATCH --nodes=$((NNODES+1))
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=${NUMTHREADS}
 
-#SBATCH --constraint=gpu
-#SBATCH --account=s929
+## OLD HOMOGENEOUS JOB SETTING ##
+# #SBATCH --ntasks-per-node=1
+# #SBATCH --cpus-per-task=12
+# #SBATCH --threads-per-core=1
+# export OMP_NUM_THREADS=12
+# srun ./run-vracer-swimmer ${OPTIONS} -shapes "${OBJECTS}" -nAgents $NAGENTS -nRanks $NRANKS
+#################################
 
-export OMP_NUM_THREADS=${NUMTHREADS}
-
-srun ./run-vracer-swimmer ${OPTIONS} -shapes "${OBJECTS}" -nAgents $NAGENTS -nRanks $NRANKS
-
+## HETEROGENEOUS JOB SETTING ##
+# Korali engine gets 12 threads and 1 rank, CUP gets 1 threads and 12 ranks
+srun --nodes=$NNODES --ntasks-per-node=12 --cpus-per-task=1 --threads-per-core=1 ./run-vracer-swimmer ${OPTIONS} -shapes "${OBJECTS}" -nAgents $NAGENTS -nRanks $(( $NRANKS * 12 )) : --nodes=1 --ntasks-per-node=1 --cpus-per-task=12 --threads-per-core=1 ./run-vracer-swimmer ${OPTIONS} -shapes "${OBJECTS}" -nAgents $NAGENTS -nRanks $(( $NRANKS * 12 ))
 EOF
 
 echo "Starting ${NWORKER} simulations each using ${NRANKS} ranks and ${NUMTHREADS} threads"

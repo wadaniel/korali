d9d04dfeb8258ed91571b61c8f4a7e7ebf2c2f74
wadaniel <wadaniel@ethz.ch>
2022-01-06 10:37:59 +0100

    fix tests

--- examples/bayesian.inference/reference/run-multinest.py
+++ examples/bayesian.inference/reference/run-multinest.py
@@ -26,9 +26,9 @@ e["Solver"]["Number Live Points"] = 1500
 
 # Configuring the problem's random distributions
 e["Distributions"][0]["Name"] = "Uniform 0"
-e["Distributions"][0]["Type"] = "Univariate/Normal"
-e["Distributions"][0]["Mean"] = 2.0
-e["Distributions"][0]["Standard Deviation"] = +5.0
+e["Distributions"][0]["Type"] = "Univariate/Uniform"
+e["Distributions"][0]["Minimum"] = 0.0
+e["Distributions"][0]["Maximum"] = +5.0
 
 # Configuring the problem's variables and their prior distributions
 e["Variables"][0]["Name"] = "a"
@@ -36,15 +36,9 @@ e["Variables"][0]["Prior Distribution"] = "Uniform 0"
 
 e["Variables"][1]["Name"] = "b"
 e["Variables"][1]["Prior Distribution"] = "Uniform 0"
-e["Variables"][1]["Lower Bound"] = -10
-e["Variables"][1]["Upper Bound"] = +10
-
 
 e["Variables"][2]["Name"] = "[Sigma]"
 e["Variables"][2]["Prior Distribution"] = "Uniform 0"
-e["Variables"][2]["Lower Bound"] = 0
-e["Variables"][2]["Upper Bound"] = +10
-
 
 e["File Output"]["Frequency"] = 5000
 e["Console Output"]["Frequency"] = 500

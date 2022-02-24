dfea072cc4e01f8cc0737f2d8423e63b8824939c
Pascal Weber <webepasc@student.ethz.ch>
2022-02-24 18:50:20 +0100

    back to amr-mpi

--- examples/study.cases/CUP2D/install_cup.sh
+++ examples/study.cases/CUP2D/install_cup.sh
@@ -3,7 +3,7 @@
 rm -rf _deps
 
 # clone CUP
-git clone -b periodicFlows --recursive git@github.com:cselab/CUP2D.git _deps/CUP-2D
+git clone -b amr-mpi --recursive git@github.com:cselab/CUP2D.git _deps/CUP-2D
 
 make -C _deps/CUP-2D/makefiles -j
 

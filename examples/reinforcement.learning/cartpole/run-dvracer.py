cc9f5094561c8351e6c014638ef3a657c4ad0baa
wadaniel <wadaniel@ethz.ch>
2022-03-04 16:54:46 +0100

    added testing frequency to cartpole example

--- examples/reinforcement.learning/cartpole/run-dvracer.py
+++ examples/reinforcement.learning/cartpole/run-dvracer.py
@@ -44,6 +44,7 @@ e = korali.Experiment()
 e["Problem"]["Type"] = "Reinforcement Learning / Discrete"
 e["Problem"]["Possible Actions"] = [ [ -10.0 ], [  10.0 ] ]
 e["Problem"]["Environment Function"] = env
+e["Problem"]["Testing Frequency"] = 100
 
 e["Variables"][0]["Name"] = "Cart Position"
 e["Variables"][0]["Type"] = "State"

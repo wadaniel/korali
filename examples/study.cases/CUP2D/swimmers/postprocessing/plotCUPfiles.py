7999bca39352ea1765e5092a114d013086590ad7
Pascal Weber <webepasc@student.ethz.ch>
2022-03-30 16:11:35 +0200

    added functions to plot the CUP output

--- examples/study.cases/CUP2D/swimmers/postprocessing/plotCUPfiles.py
+++ examples/study.cases/CUP2D/swimmers/postprocessing/plotCUPfiles.py
@@ -3,97 +3,175 @@ import matplotlib.pyplot as plt
 from matplotlib import animation
 from scipy.signal import savgol_filter
 from scipy.ndimage.filters import uniform_filter1d
+import argparse
 
-def plotQuick():
-	root = "/scratch/snx3000/pweber/korali/hydrofoil_largeDomain.eval/_testingResults/sample000/"
+import seaborn as sns
+sns.set_theme()
+sns.set_style("whitegrid")
 
-	velData = np.loadtxt(root+"velocity_1.dat", skiprows=1)
-	plt.plot(velData[:,0], velData[:,2], label='x-coordinate')
-	plt.plot(velData[:,0], velData[:,3], label='y-coordinate')
-	plt.ylim([0.8,1.4])
-	plt.legend()
-	plt.show(block=False)
+def plotTrajectory( root ):
+    data = np.loadtxt(root+"/velocity_1.dat", skiprows=1)
 
-	plt.plot(velData[:,0], velData[:,7], label='x-velocity')
-	plt.plot(velData[:,0], velData[:,8], label='y-velocity')
-	plt.ylim([-1,1])
-	plt.show()
+    fig, ax = plt.subplots(1, 2,sharex=True,figsize=(6,3), dpi=100)
+
+    ax[0].plot(data[:,0], data[:,2], color="C0")
+    ax[0].plot(data[:,0], data[:,3], color="C1")
+    ax[0].set_xlabel("time $t$")
+    ax[0].set_ylabel("position")
+    # plt.ylim([0.8,1.4])
+
+    ax[1].plot(data[:,0], data[:,7], label='x-components', color="C0")
+    ax[1].plot(data[:,0], data[:,8], label='y-components', color="C1")
+    ax[1].set_xlabel("time $t$")
+    ax[1].set_ylabel("velocity")
+    # plt.ylim([-1,1])
+
+    index = data[:,0] > 10
+    print("Average velocity:", np.mean(data[index,7]))
+
+    ax[1].legend(bbox_to_anchor=(-0.5,-0.2, 2, 1), loc="lower left", frameon=False, ncols=2)
+    plt.tight_layout()
+    plt.show()
+
+def plotForces( root ):
+    data = np.loadtxt(root+"/forceValues_1.dat", skiprows=1)
+
+    fig, ax = plt.subplots(1, 2,sharex=True,figsize=(6,3), dpi=100)
+
+    # 1/2 * speed of obstacle^2 * fish length
+    nonDimFactor = 0.15**2/2*0.2
+
+    ax[0].plot(data[:,0], data[:,10]/nonDimFactor, label='thrust', color="C0")
+    ax[0].set_xlabel("time $t$")
+    ax[0].set_ylabel("thrust $T$")
+    ax[1].plot(data[:,0], -data[:,11]/nonDimFactor, label='drag', color="C1")
+    ax[1].set_xlabel("time $t$")
+    ax[1].set_ylabel("drag $D$")
+    # twinAx.plot(data[:,0], data[:,7], label='torque', color="C2")
+
+    # plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+def plotEnergy( root ):
+    data = np.loadtxt(root+"/powerValues_1.dat", skiprows=1)
+
+    fig, ax = plt.subplots(1, 3, sharex=True,figsize=(12,3), dpi=100)
+
+    # 1/2 * speed of obstacle^2 * fish length^2
+    nonDimFactor = 0.15**2/2*0.2**2
+
+    ax[0].plot(data[:,0], data[:,1]/nonDimFactor, color="C0")
+    ax[0].set_xlabel("time $t$")
+    ax[0].set_ylabel("thrust power $P_T$")
+    ax[1].plot(data[:,0], data[:,2]/nonDimFactor, color="C1")
+    ax[1].set_xlabel("time $t$")
+    ax[1].set_ylabel("drag power $P_D$")
+    ax[2].plot(data[:,0], data[:,7]/nonDimFactor, color="C2")
+    ax[2].set_xlabel("time $t$")
+    ax[2].set_ylabel("deformation power $P_S$")
+    plt.tight_layout()
+    plt.show()
+
+
+    fig, ax = plt.subplots(1, 1, sharex=True,figsize=(6,3), dpi=100)
+    ax.plot(data[:,0], data[:,-2], label='efficiency', color="C3") #or -1
+    ax.set_xlabel("time $t$")
+    ax.set_ylabel("efficiency $\eta$")
+    plt.tight_layout()
+    plt.show()
+
 
 def plotEfficiency():
-	rootControlled = "/scratch/snx3000/mchatzim/CUP2D/School_Control_09Fish/"
-	rootUncontrolled = "/scratch/snx3000/mchatzim/CUP2D/School_NoControl_09Fish/"
-	rootSingle = "/scratch/snx3000/mchatzim/CUP2D/single-fish/"
+    rootControlled = "/scratch/snx3000/mchatzim/CUP2D/School_Control_09Fish/"
+    rootUncontrolled = "/scratch/snx3000/mchatzim/CUP2D/School_NoControl_09Fish/"
+    rootSingle = "/scratch/snx3000/mchatzim/CUP2D/single-fish/"
 
-	row = 9
+    row = 9
 
-	powDataSingle = np.loadtxt(rootSingle+"powerValues_0.dat", skiprows=1)
-	powDataSingleAverage = powDataSingle[:,row]
-	powDataSingleSmoothned = savgol_filter(powDataSingleAverage, 51, 3)
-	plt.plot(powDataSingle[:,0], powDataSingleSmoothned, linewidth=1, label='single fish')
-	powDataSingleAveraged = uniform_filter1d(powDataSingleAverage, size=1000)
-	plt.plot(powDataSingle[:,0], powDataSingleAveraged, "--", linewidth=2, label='single fish, average')
+    powDataSingle = np.loadtxt(rootSingle+"powerValues_0.dat", skiprows=1)
+    powDataSingleAverage = powDataSingle[:,row]
+    powDataSingleSmoothned = savgol_filter(powDataSingleAverage, 51, 3)
+    plt.plot(powDataSingle[:,0], powDataSingleSmoothned, linewidth=1, label='single fish')
+    powDataSingleAveraged = uniform_filter1d(powDataSingleAverage, size=1000)
+    plt.plot(powDataSingle[:,0], powDataSingleAveraged, "--", linewidth=2, label='single fish, average')
 
-	numSwimmersUncontrolled = 9
-	powDataUncontrolled = np.loadtxt(rootUncontrolled+"powerValues_0.dat", skiprows=1)
-	powDataUncontrolledAverage = np.zeros(powDataUncontrolled[:,row].shape)
-	for i in range(0,numSwimmersUncontrolled):
-		powDataUncontrolled = np.loadtxt(rootUncontrolled+"powerValues_{}.dat".format(i), skiprows=1)
-		powDataUncontrolledAverage = powDataUncontrolledAverage + powDataUncontrolled[:,row]
-	powDataUncontrolledSmoothened = savgol_filter(powDataUncontrolledAverage, 51, 3)
-	plt.plot(powDataUncontrolled[:,0], powDataUncontrolledSmoothened/numSwimmersUncontrolled, linewidth=1, label='9 fish, uncontrolled')
-	powDataUncontrolledAveraged = uniform_filter1d(powDataUncontrolledAverage, size=1000)
-	plt.plot(powDataUncontrolled[:,0], powDataUncontrolledAveraged/numSwimmersUncontrolled, "--", linewidth=2, label='9 fish, uncontrolled, average')
+    numSwimmersUncontrolled = 9
+    powDataUncontrolled = np.loadtxt(rootUncontrolled+"powerValues_0.dat", skiprows=1)
+    powDataUncontrolledAverage = np.zeros(powDataUncontrolled[:,row].shape)
+    for i in range(0,numSwimmersUncontrolled):
+        powDataUncontrolled = np.loadtxt(rootUncontrolled+"powerValues_{}.dat".format(i), skiprows=1)
+        powDataUncontrolledAverage = powDataUncontrolledAverage + powDataUncontrolled[:,row]
+    powDataUncontrolledSmoothened = savgol_filter(powDataUncontrolledAverage, 51, 3)
+    plt.plot(powDataUncontrolled[:,0], powDataUncontrolledSmoothened/numSwimmersUncontrolled, linewidth=1, label='9 fish, uncontrolled')
+    powDataUncontrolledAveraged = uniform_filter1d(powDataUncontrolledAverage, size=1000)
+    plt.plot(powDataUncontrolled[:,0], powDataUncontrolledAveraged/numSwimmersUncontrolled, "--", linewidth=2, label='9 fish, uncontrolled, average')
 
-	powDataControlled = np.loadtxt(rootControlled+"powerValues_0.dat", skiprows=1)
-	powDataAverage = np.zeros(powDataControlled[:,row].shape)
-	for i in range(0,9):
-		powDataControlled = np.loadtxt(rootControlled+"powerValues_{}.dat".format(i), skiprows=1)
-		powDataAverage = powDataAverage + powDataControlled[:,row]
-	powDataAverageSmoothened = savgol_filter(powDataAverage, 51, 3)
-	plt.plot(powDataControlled[:,0], powDataAverageSmoothened/9, linewidth=1, label='9 fish, controlled')
-	powDataAverageAveraged = uniform_filter1d(powDataAverage, size=1000)
-	plt.plot(powDataControlled[:,0], powDataAverageAveraged/9, "--", linewidth=2, label='9 fish, controlled, average')
+    powDataControlled = np.loadtxt(rootControlled+"powerValues_0.dat", skiprows=1)
+    powDataAverage = np.zeros(powDataControlled[:,row].shape)
+    for i in range(0,9):
+        powDataControlled = np.loadtxt(rootControlled+"powerValues_{}.dat".format(i), skiprows=1)
+        powDataAverage = powDataAverage + powDataControlled[:,row]
+    powDataAverageSmoothened = savgol_filter(powDataAverage, 51, 3)
+    plt.plot(powDataControlled[:,0], powDataAverageSmoothened/9, linewidth=1, label='9 fish, controlled')
+    powDataAverageAveraged = uniform_filter1d(powDataAverage, size=1000)
+    plt.plot(powDataControlled[:,0], powDataAverageAveraged/9, "--", linewidth=2, label='9 fish, controlled, average')
 
-	plt.xlim([0,30])
-	plt.legend()
-	plt.show()
+    plt.xlim([0,30])
+    plt.legend()
+    plt.show()
 
 def animateCoM():
-	path = "/scratch/snx3000/pweber/korali/hydrofoil_largeDomain.eval/_testingResults/sample000/"
-	numFish = 1
-	
-	# Load data, set up figure, axis, and plot element we want to animate
-	fig = plt.figure()
-	fig.patch.set_alpha(0.)
-	ax = plt.axes(xlim=(0.4, 2),ylim=( 0.6, 1.4 ) )
-	ax.set_axis_off()
-	lines = []
-	data = []
-	for i in range(numFish):
-		data.append( np.loadtxt(path+"velocity_{}.dat".format(i), skiprows=1) )
-		lines.append(ax.plot([], [], lw=2, color="C{}".format(i))[0])
+    path = "/scratch/snx3000/pweber/korali/hydrofoil_largeDomain.eval/_testingResults/sample000/"
+    numFish = 1
+    
+    # Load data, set up figure, axis, and plot element we want to animate
+    fig = plt.figure()
+    fig.patch.set_alpha(0.)
+    ax = plt.axes(xlim=(0.4, 2),ylim=( 0.6, 1.4 ) )
+    ax.set_axis_off()
+    lines = []
+    data = []
+    for i in range(numFish):
+        data.append( np.loadtxt(path+"velocity_{}.dat".format(i), skiprows=1) )
+        lines.append(ax.plot([], [], lw=2, color="C{}".format(i))[0])
 
-	# initialization function: plot the background of each frame
-	def init():
-	  for line in lines:
-	    line.set_data([], [])
-	  return lines
+    # initialization function: plot the background of each frame
+    def init():
+      for line in lines:
+        line.set_data([], [])
+      return lines
 
-	# animation function.  This is called sequentially
-	def animate(i):
-	  plt.title("Center of Mass, t={}".format(data[0][i,0]))
-	  for j in range(numFish):
-	  	lines[j].set_data(data[j][:i,2], data[j][:i,3])
-	  return lines
+    # animation function.  This is called sequentially
+    def animate(i):
+      plt.title("Center of Mass, t={}".format(data[0][i,0]))
+      for j in range(numFish):
+        lines[j].set_data(data[j][:i,2], data[j][:i,3])
+      return lines
 
-	# call the animator.  blit=True means only re-draw the parts that have changed.
-	anim = animation.FuncAnimation(fig, animate, init_func=init,
-	                               frames=len(data[0][:,0]), interval=1, blit=True)
+    # call the animator.  blit=True means only re-draw the parts that have changed.
+    anim = animation.FuncAnimation(fig, animate, init_func=init,
+                                   frames=len(data[0][:,0]), interval=1, blit=True)
 
-	# save the animation as an mp4
-	anim.save('CoM_animation_fish.mp4', fps=10)
+    # save the animation as an mp4
+    anim.save('CoM_animation_fish.mp4', fps=10)
 
 if __name__ == '__main__':
-	# plotQuick()
-	animateCoM()
-	# plotEfficiency()
+
+    parser = argparse.ArgumentParser(
+        prog='cup2D.dynamics',
+        description='Plot the dynamical quantities of a CUP2D run.')
+    parser.add_argument(
+        '--dir',
+        help='Path to result files, separated by space',
+        type = str,
+        default = "",
+        required=False)
+    args = parser.parse_args()
+
+
+    plotTrajectory(args.dir)
+    plotForces(args.dir)
+    plotEnergy(args.dir)
+    # animateCoM()
+    # plotEfficiency()

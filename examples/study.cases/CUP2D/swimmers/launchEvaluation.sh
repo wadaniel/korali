8f81be76c709b77da8dd9151798148c143a35f30
Pascal Weber <webepasc@student.ethz.ch>
2022-04-01 15:28:19 +0200

    fixing that past me commited an empty file

---
+++ examples/study.cases/CUP2D/swimmers/launchEvaluation.sh
@@ -0,0 +1,11 @@
+for E in {0..9}; do export EVAL=$E; ./sbatch-eval-vracer-swimmer.sh halfDisk_largeDomain.eval 0; done
+
+for E in {0..9}; do export EVAL=$E; ./sbatch-eval-vracer-swimmer.sh hydrofoil_largeDomain.eval 1; done
+
+for E in {0..9}; do export EVAL=$E; ./sbatch-eval-vracer-swimmer.sh hydrofoil_largeDomain.eval.halfdisk 0; done
+
+for E in {0..9}; do export EVAL=$E; ./sbatch-eval-vracer-swimmer.sh halfDisk_largeDomain.eval.hydrofoil 1; done
+
+for E in {0..9}; do export EVAL=$E; ./sbatch-eval-vracer-swimmer.sh multitask_largeDomain.eval.hydrofoil -1; done
+
+for E in {0..9}; do export EVAL=$E; ./sbatch-eval-vracer-swimmer.sh multitask_largeDomain.eval.halfdisk -1; done

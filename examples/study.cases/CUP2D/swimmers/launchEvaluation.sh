faffb675e9a872a08175d5d710bf116afbc714e7
Pascal Weber <webepasc@student.ethz.ch>
2022-04-05 01:17:11 +0200

    moved order for loops for evaluation

--- examples/study.cases/CUP2D/swimmers/launchEvaluation.sh
+++ examples/study.cases/CUP2D/swimmers/launchEvaluation.sh
@@ -6,6 +6,6 @@ for E in {0..9}; do export EVAL=$E; ./sbatch-eval-vracer-swimmer.sh hydrofoil_la
 
 for E in {0..9}; do export EVAL=$E; ./sbatch-eval-vracer-swimmer.sh halfDisk_largeDomain.eval.hydrofoil 1; done
 
-for E in {0..9}; do export EVAL=$E; ./sbatch-eval-vracer-swimmer.sh multitask_largeDomain.eval.hydrofoil -1; done
-
 for E in {0..9}; do export EVAL=$E; ./sbatch-eval-vracer-swimmer.sh multitask_largeDomain.eval.halfdisk -1; done
+
+for E in {0..9}; do export EVAL=$E; ./sbatch-eval-vracer-swimmer.sh multitask_largeDomain.eval.hydrofoil -1; done

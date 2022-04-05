ed6b23e218051a3463cf2911c94898b74d254209
Michail Chatzimanolakis <mchatzim@daint101.login.cscs.ch>
2022-04-05 18:21:56 +0200

    2D swimmers with 3D testing

---
+++ examples/study.cases/CUP2D/swimmers-3D-compatible/_model2D/swimmerEnvironment2D.hpp
@@ -0,0 +1,23 @@
+//  Korali environment for CubismUP-2D
+//  Copyright (c) 2022 CSE-Lab, ETH Zurich, Switzerland.
+
+#include "korali.hpp"
+#include <algorithm>
+#include <random>
+#include <filesystem>
+#include <iostream>
+#include <fstream>
+#include "Obstacles/StefanFish.h"
+#include "Simulation.h"
+#include "Utils/BufferedLogger.h"
+#include <Cubism/ArgumentParser.h>
+
+#define NACTIONS 3
+
+// command line arguments are read in Korali application
+extern int _argc;
+extern char **_argv;
+
+void runEnvironment(korali::Sample &s);
+bool isTerminal(StefanFish *agent);
+double getReward(StefanFish *agent);
---
+++ examples/study.cases/CUP2D/swimmers-3D-compatible/sbatch-eval-vracer-swimmer.sh
@@ -0,0 +1,47 @@
+#! /usr/bin/env bash
+
+if [ $# -lt 3 ] ; then
+	echo "Usage: ./sbatch-eval-vracer-swimmer.sh RUNNAME TASK DIMENSION"
+	exit 1
+fi
+
+RUNNAME=$1
+TASK=$2
+DIMENSION=$3
+EXECUTABLE=eval-vracer-swimmer-"${DIMENSION}"D
+
+EXTRA=
+if [ $DIMENSION == $3 ]
+then
+source _model3D/settings3D.sh
+echo ${FACTORY}
+EXTRA=${OPTIONS}" -factory-content \""${FACTORY}"\""
+fi
+echo ${EXTRA}
+
+NAGENTS=1   # number of agents
+NWORKER=3   # number of workers
+NRANKS=1    # nodes per worker
+NUMCORES=12 # cores per worker
+NNODES=$(( $NWORKER * $NRANKS )) # number of workers * number of nodes per worker
+
+# setup run directory and copy necessary files
+RUNPATH="${SCRATCH}/korali/${RUNNAME}"
+mkdir -p ${RUNPATH}
+cp $EXECUTABLE ${RUNPATH}
+cd ${RUNPATH}
+
+cat <<EOF >daint_sbatch_testing-"${DIMENSION}"D
+#!/bin/bash -l
+#SBATCH --account=s929
+#SBATCH --constraint=gpu
+#SBATCH --job-name="${RUNNAME}"
+#SBATCH --time=00:30:00
+#SBATCH --partition=debug
+#SBATCH --nodes=$((NNODES+1))
+
+srun --nodes=$NNODES --ntasks-per-node=$NUMCORES --cpus-per-task=1 --threads-per-core=1 ./$EXECUTABLE $EXTRA -task $TASK -nAgents $NAGENTS -nRanks $(( $NRANKS * $NUMCORES )) : --nodes=1 --ntasks-per-node=1 --cpus-per-task=$NUMCORES --threads-per-core=1 ./$EXECUTABLE $EXTRA -task $TASK -nAgents $NAGENTS -nRanks $(( $NRANKS * $NUMCORES ))
+EOF
+
+chmod 755 daint_sbatch_testing-"${DIMENSION}"D
+sbatch daint_sbatch_testing-"${DIMENSION}"D

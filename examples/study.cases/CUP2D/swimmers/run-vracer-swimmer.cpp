7e6fa7e67ff21ab114a32ab9399b0fc10234e8ff
Pascal Weber <webepasc@student.ethz.ch>
2022-05-12 21:22:25 +0200

    back to using single policy and no competition

--- examples/study.cases/CUP2D/swimmers/run-vracer-swimmer.cpp
+++ examples/study.cases/CUP2D/swimmers/run-vracer-swimmer.cpp
@@ -50,7 +50,7 @@ int main(int argc, char *argv[])
   // Configuring Experiment
   e["Problem"]["Environment Function"] = &runEnvironment;
   e["Problem"]["Agents Per Environment"] = nAgents;
-  e["Problem"]["Policies Per Environment"] = nAgents;
+  // e["Problem"]["Policies Per Environment"] = nAgents;
 
   #if 0
   if( task == -1 )
@@ -104,7 +104,7 @@ int main(int argc, char *argv[])
   e["Solver"]["Learning Rate"] = 1e-4;
   e["Solver"]["Discount Factor"] = 0.95;
   e["Solver"]["Mini Batch"]["Size"] =  128;
-  e["Solver"]["Multi Agent Relationship"] = "Competition";
+  // e["Solver"]["Multi Agent Relationship"] = "Competition";
 
   /// Defining the configuration of replay memory
   e["Solver"]["Experience Replay"]["Start Size"] = 1024;

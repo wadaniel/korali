d49c6ac7af036fe7942fa41e6a62c71e26ff52f9
Michail Chatzimanolakis <mchatzim@daint105.login.cscs.ch>
2022-05-16 09:09:39 +0200

    deleted old files from CUP3D cases

--- examples/study.cases/CUP3D/old/002-SingleFish3DMotion/_model2D/configs.hpp
+++
@@ -1,6 +0,0 @@
-//  Korali environment for CubismUP-2D
-//  Copyright (c) 2020 CSE-Lab, ETH Zurich, Switzerland.
-
-std::string OPTIONS = "-bpdx 4 -bpdy 4 -levelMax 7 -levelStart 4 -Rtol 10000.0 -Ctol 100.0 -extent 2 -CFL 0.7 -poissonTol 1e-5 -poissonTolRel 0.0 -bMeanConstraint 1 -bAdaptChiGradient 0 -tdump 0 -nu 0.00004 -tend 0 -muteAll 0 -verbose 1";
-
-std::vector<std::vector<double>> initialPositions{{{1.0, 1.0}}};

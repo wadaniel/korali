01317462023c1b8e1e096e5741ad5ff5f4816dd1
Michail Chatzimanolakis <mchatzim@daint102.login.cscs.ch>
2022-05-31 15:47:36 +0200

    School scripts for 3d gpu solver

--- examples/study.cases/CUP3D/005-School09/sbatch-train.sh
+++ examples/study.cases/CUP3D/005-School09/sbatch-train.sh
@@ -22,21 +22,25 @@ TRAIN=$3
 NODES=$4
 NODES_PER_SAMPLE=$5
 
-if [ $DIMENSION == 2 ] #Use GPU solver with 1 MPI rank & 12 threads per node
-then
-	THREADS=12
-	RANKS=1
-else                   #Use CPU solver with 12 MPI ranks & 1 thread per node
-	THREADS=1
-	RANKS=12
-fi
+#if [ $DIMENSION == 2 ] #Use GPU solver with 1 MPI rank & 12 threads per node
+#then
+#	THREADS=12
+#	RANKS=1
+#else                   #Use CPU solver with 12 MPI ranks & 1 thread per node
+#	THREADS=1
+#	RANKS=12
+#fi
+#Settings for GPU Poisson solver
+THREADS=12
+RANKS=1
+
 RANKS_PER_SAMPLE=$(( $NODES_PER_SAMPLE * $RANKS ))
 
 if [ $TRAIN == 1 ]
 then
 EXECUTABLE=train-swimmer-"${DIMENSION}"D
 BATCH_FILE=daint_sbatch_training-"${DIMENSION}"D
-CLOCK=${CLOCK:-06:00:00}
+CLOCK=${CLOCK:-24:00:00}
 PARTITION=${PARTITION:-normal}
 else
 EXECUTABLE=test-swimmer-"${DIMENSION}"D
--- examples/study.cases/CUP3D/005-School09/swimmerEnvironment.hpp
+++ examples/study.cases/CUP3D/005-School09/swimmerEnvironment.hpp
@@ -7,45 +7,18 @@
 #include <filesystem>
 #include <iostream>
 #include <fstream>
-
 #include "Obstacles/StefanFish.h"
 #include "Simulation.h"
 #include "Utils/BufferedLogger.h"
 #include <Cubism/ArgumentParser.h>
 
-#define STATES 35
-#define AGENTS 9
-#define ACTIONS 8
+#define STATES 23
+#define AGENTS 8
+#define ACTIONS 5
 
-#if modelDIM == 2
-    std::string OPTIONS = "-bpdx 2 -bpdy 2 -levelMax 7 -levelStart 5 -Rtol 5.0 -Ctol 0.1 -extent 2.0 -CFL 0.7 -poissonTol 1e-5 -poissonTolRel 1e-2 -bMeanConstraint 0 -tdump 0 -nu 0.00001 -poissonSolver cuda_iterative";
-    std::vector<std::vector<double>> initialPositions{{
-          {0.60, 1.00, 1.00},
-          {0.90, 0.90, 1.00},
-          {0.90, 1.10, 1.00},
-          {1.20, 0.80, 1.00},
-          {1.20, 1.00, 1.00},
-          {1.20, 1.20, 1.00},
-          {1.50, 0.90, 1.00},
-          {1.50, 1.10, 1.00},
-          {1.80, 1.00, 1.00}
-     }};
-     std::vector<std::vector<double>> actual_initialPositions;
-#else
-    std::string OPTIONS = " -bpdx 2 -bpdy 2 -bpdz 1 -extentx 2.0 -levelMax 7 -levelStart 5 -Rtol 5.0 -Ctol 0.1 -tdump 0 -CFL 0.7 -lambda 1e6 -nu 0.00001 -poissonTol 1e-5 -poissonTolRel 1e-2 -bMeanConstraint 1 ";
-    std::vector<std::vector<double>> initialPositions{{
-          {0.60, 1.00, 1.00},
-          {0.90, 0.90, 1.00},
-          {0.90, 1.10, 1.00},
-          {1.20, 0.80, 1.00},
-          {1.20, 1.00, 1.00},
-          {1.20, 1.20, 1.00},
-          {1.50, 0.90, 1.00},
-          {1.50, 1.10, 1.00},
-          {1.80, 1.00, 1.00}
-     }};
-     std::vector<std::vector<double>> actual_initialPositions;
-    using namespace cubismup3d;
+std::vector<std::vector<double>> actual_initialPositions;
+#if modelDIM == 3
+  using namespace cubismup3d;
 #endif
 
 std::mt19937 _randomGenerator;
@@ -61,16 +34,31 @@ Simulation * initializeEnvironment(korali::Sample &s)
     int rank;
     MPI_Comm_rank(comm,&rank);
 
+    std::vector<std::vector<double>> initialPositions{{
+          {0.6, 1.0, 0.8},
+          {1.0, 0.8, 0.8},
+          {1.0, 1.2, 0.8},
+          {1.4, 1.0, 0.8},
+          {0.6, 1.0, 1.2},
+          {1.0, 0.8, 1.2},
+          {1.0, 1.2, 1.2},
+          {1.4, 1.0, 1.2}}};
+
     // Argument string to inititialize Simulation
     #if modelDIM == 2
-        std::string argumentString = "CUP-RL " + OPTIONS + " -shapes ";
+        std::string OPTIONS = "-bpdx 2 -bpdy 2 -extent 2.0 -CFL 0.7 ";
+	OPTIONS += "-levelMax 7 -levelStart 5 -Rtol 5.0 -Ctol 0.1 -nu 0.00001 ";
+        OPTIONS += "-poissonTol 1e-5 -poissonTolRel 1e-2 -bMeanConstraint 0 -poissonSolver cuda_iterative";
         std::string AGENT = " \n\
         stefanfish L=0.2 T=1 bFixed=1 ";
     #else
-        std::string argumentString = "CUP-RL " + OPTIONS + " -factory-content ";
+        std::string OPTIONS = "-bpdx 2 -bpdy 2 -bpdz 2 -extentx 2.0 -CFL 0.7 ";
+	OPTIONS += "-levelMax 6 -levelStart 4 -Rtol 5.0 -Ctol 0.1 -nu 0.00001 ";
+        OPTIONS += "-poissonTol 1e-5 -poissonTolRel 1e-2 -bMeanConstraint 0 -poissonSolver cuda_iterative";
         std::string AGENT = " \n\
-        StefanFish L=0.2 T=1 bFixToPlanar=1 bFixFrameOfRef=1 ";
+        StefanFish L=0.2 T=1 bFixFrameOfRef=1 ";
     #endif
+    std::string argumentString = "CUP-RL " + OPTIONS + " -shapes ";
 
     // Set initial position for all prey
     for( int a = 0; a < AGENTS; a++ )
@@ -85,17 +73,15 @@ Simulation * initializeEnvironment(korali::Sample &s)
                 std::uniform_real_distribution<double> disZ(-0.05, 0.05);
                 initialData[0] += disX(_randomGenerator);
                 initialData[1] += disY(_randomGenerator);
-                //initialData[2] += disZ(_randomGenerator);
+                initialData[2] += disZ(_randomGenerator);
             }
             MPI_Bcast(initialData.data(), 3, MPI_DOUBLE, 0, comm);
         }
         actual_initialPositions.push_back(initialData);
         // Append agent to argument string
-        argumentString = argumentString + AGENT + " xpos=" + std::to_string(initialData[0]) 
-                                                + " ypos=" + std::to_string(initialData[1]);
+        argumentString += AGENT + " xpos=" + std::to_string(initialData[0]) + " ypos=" + std::to_string(initialData[1]);
         #if modelDIM == 3
-            argumentString = argumentString     + " zpos=" + std::to_string(initialData[2])
-                                                + " heightProfile=danio" + " widthProfile=stefan";
+            argumentString += " zpos=" + std::to_string(initialData[2]) + " heightProfile=danio widthProfile=stefan";
         #endif
     }
 
@@ -114,94 +100,94 @@ Simulation * initializeEnvironment(korali::Sample &s)
     #if modelDIM == 2
         Simulation *_environment = new Simulation(argv.size() - 1, argv.data(), comm);
         _environment->init();
-
-        // Establishing environment's dump frequency
-        _environment->sim.dumpTime = s["Custom Settings"]["Dump Frequency"].get<double>();
-        
     #else
         ArgumentParser parser(argv.size()-1, argv.data());
         Simulation *_environment = new Simulation(comm, parser);
-
-        // Establishing environment's dump frequency
-        _environment->sim.saveTime = s["Custom Settings"]["Dump Frequency"].get<double>();
     #endif
+    // Establishing environment's dump frequency
+    _environment->sim.dumpTime = s["Custom Settings"]["Dump Frequency"].get<double>();
 
     return _environment;
 }
 
 bool isTerminal(StefanFish *agent, const SimulationData & sim, const int agentID, const size_t step)
 {
-    const size_t maxSteps = 1000; // Max steps before truncation
+    const size_t maxSteps = 200; // Max steps before truncation (=100 periods)
 
-    if (step >= maxSteps) return true;
+    if (step >= maxSteps || sim.bCollision) return true;
 
-    if (sim.bCollision) return true;
-
-    const double xMin = 0.1;
-    const double xMax = 1.9;
-    const double yMin = 0.1;
-    const double yMax = 1.9;
-    const double zMin = 0.1;
-    const double zMax = 1.9;
     #if modelDIM == 2
-        const double X = agent->center[0];
-        const double Y = agent->center[1];
-        const double Z = 1.0;
+      const double length = agent->length;
+      const double margin = length;
+      const double X = agent->center[0];
+      const double Y = agent->center[1];
+    #else
+      const auto & myFish = agent->myFish;
+      auto * const cFish = dynamic_cast<CurvatureDefinedFishData*>( myFish );
+      const double length = cFish->length;
+      const double margin = length;
+      const double X = agent->position[0];
+      const double Y = agent->position[1];
+      const double Z = agent->position[2];
+      const double zMin = margin;
+      const double zMax = sim.extents[2]-margin;
+      if (Z < zMin) return true;
+      if (Z > zMax) return true;
     #endif
-    #if modelDIM == 3
-        const double X = agent->position[0];
-        const double Y = agent->position[1];
-        const double Z = agent->position[2];
-    #endif
+    const double xMin = margin;
+    const double yMin = margin;
+    const double xMax = sim.extents[0]-margin;
+    const double yMax = sim.extents[1]-margin;
     if (X < xMin) return true;
     if (X > xMax) return true;
     if (Y < yMin) return true;
     if (Y > yMax) return true;
-    if (Z < zMin) return true;
-    if (Z > zMax) return true;
     return false;
 }
 
 double getReward(std::vector<StefanFish *> & agents, const SimulationData & sim, const int agentID)
 {
-    //Check if this fish collided and return penalty only if it did.
+    // I) Check if this fish collided and return penalty only if it did.
     if (sim.bCollision) 
-    {
         for (size_t i = 0; i < sim.bCollisionID.size(); i++)
             if (sim.bCollisionID[i] == agentID) return -10.0;
-    }
 
-    // Return penalty if fish exited the domain
+    // II) Return penalty if fish exited the domain.
     #if modelDIM == 2
-    const double X = agents[agentID]->center[0];
-    const double Y = agents[agentID]->center[1];
-    const double Z = 1.0;
+      const double length = agents[agentID]->length;
+      const double X = agents[agentID]->center[0];
+      const double Y = agents[agentID]->center[1];
+      const double margin = length;
     #else
-    const double X = agents[agentID]->position[0];
-    const double Y = agents[agentID]->position[1];
-    const double Z = agents[agentID]->position[2];
+      const auto & myFish = agents[agentID]->myFish;
+      auto * const cFish = dynamic_cast<CurvatureDefinedFishData*>( myFish );
+      const double length = cFish->length;
+      const double margin = length;
+      const double X = agents[agentID]->position[0];
+      const double Y = agents[agentID]->position[1];
+      const double Z = agents[agentID]->position[2];
+      const double zMin = margin;
+      const double zMax = sim.extents[2]-margin;
+      if (Z < zMin) return -10;
+      if (Z > zMax) return -10;
     #endif
-    const double xMin = 0.1;
-    const double xMax = 1.9;
-    const double yMin = 0.1;
-    const double yMax = 1.9;
-    const double zMin = 0.1;
-    const double zMax = 1.9;
+    const double xMin = margin;
+    const double yMin = margin;
+    const double xMax = sim.extents[0]-margin;
+    const double yMax = sim.extents[1]-margin;
     if (X < xMin) return -10;
     if (X > xMax) return -10;
     if (Y < yMin) return -10;
     if (Y > yMax) return -10;
-    if (Z < zMin) return -10;
-    if (Z > zMax) return -10;
 
-    // Reward swimming efficiency is fish did not collide and is in the domain
+    // III) Reward swimming efficiency otherwise.
     return agents[agentID]->EffPDefBnd;
 }
 
 std::vector<double> getState(std::vector<StefanFish *> & agents, const SimulationData & sim, const int agentID)
 {
     const StefanFish * agent = agents[agentID];
-    std::vector<double> S(26);
+    std::vector<double> S(14);
     #if modelDIM == 3
 	const auto & myFish = agent->myFish;
         auto * const cFish = dynamic_cast<CurvatureDefinedFishData*>( myFish );
@@ -222,91 +208,7 @@ std::vector<double> getState(std::vector<StefanFish *> & agents, const Simulatio
         S[11] = agent->angVel[0] * Tperiod;
         S[12] = agent->angVel[1] * Tperiod;
         S[13] = agent->angVel[2] * Tperiod;
-        S[14] = cFish->lastTact;
-        S[15] = cFish->lastCurv;
-        S[16] = cFish->oldrCurv;
-        //sensor locations
-        const std::array<Real,3> locFront = {cFish->sensorLocation[0*3+0],cFish->sensorLocation[0*3+1],cFish->sensorLocation[0*3+2    ]};
-        const std::array<Real,3> locUpper = {cFish->sensorLocation[1*3+0],cFish->sensorLocation[1*3+1],cFish->sensorLocation[1*3+2    ]};
-        const std::array<Real,3> locLower = {cFish->sensorLocation[2*3+0],cFish->sensorLocation[2*3+1],cFish->sensorLocation[2*3+2    ]};
-        //compute shear stress force (x,y,z) components
-        std::array<Real,3> shearFront = agent->getShear( locFront );
-        std::array<Real,3> shearUpper = agent->getShear( locLower );
-        std::array<Real,3> shearLower = agent->getShear( locUpper );
-        S[17] = shearFront[0] * Tperiod / length;
-        S[18] = shearFront[1] * Tperiod / length;
-        S[19] = shearFront[2] * Tperiod / length;
-        S[20] = shearUpper[0] * Tperiod / length;
-        S[21] = shearUpper[1] * Tperiod / length;
-        S[22] = shearUpper[2] * Tperiod / length;
-        S[23] = shearLower[0] * Tperiod / length;
-        S[24] = shearLower[1] * Tperiod / length;
-        S[25] = shearLower[2] * Tperiod / length;
-        const double X = agents[agentID]->absPos[0];
-        const double Y = agents[agentID]->absPos[1];
-        const double Z = agents[agentID]->absPos[2];
     #else
-	const double length  = agent->length;
-	const double Tperiod = agent->Tperiod;
-	const auto & myFish = agent->myFish;
-        const CurvatureFish* const cFish = dynamic_cast<CurvatureFish*>( myFish );
-        S[0 ] = agent->center[0];
-        S[1 ] = agent->center[1];
-        S[2 ] = 0.0;//Z=0
-	//S[3 ] = 0.0;//axis x-component = 0
-        //S[4 ] = 0.0;//axis y-component = 0
-        //S[5 ] = 1.0;//axis z-component = 1
-        //S[6 ] = agent->getOrientation();
-	const double theta = agent->getOrientation();
-	S[3] = cos(.5*theta);
-        S[4] = 0;
-	S[5] = 0;
-	S[6] = sin(.5*theta);
-        S[7 ] = agent->getPhase( sim.time );
-        S[8 ] = agent->getU() * Tperiod / length;
-        S[9 ] = agent->getV() * Tperiod / length;
-        S[10] = 0.0;//z linear vel
-        S[11] = 0.0;//x angular vel
-        S[12] = 0.0;//y angular vel
-        S[13] = agent->getW() * Tperiod;
-        S[14] = cFish->lastTact;
-        S[15] = cFish->lastCurv;
-        S[16] = cFish->oldrCurv;
-
-     	//Shear stress computation at three sensors
-        //******************************************
-        // Get fish skin
-        const auto &DU = myFish->upperSkin;
-        const auto &DL = myFish->lowerSkin;
-
- 	// index for sensors on the side of head
-        int iHeadSide = 0;
-        for(int i=0; i<myFish->Nm-1; ++i)
-           if( myFish->rS[i] <= 0.04*length && myFish->rS[i+1] > 0.04*length ) iHeadSide = i;
-        assert(iHeadSide>0);
-
-        //sensor locations
-        const std::array<Real,2> locFront = {DU.xSurf[0]       , DU.ySurf[0]       };
-        const std::array<Real,2> locUpper = {DU.midX[iHeadSide], DU.midY[iHeadSide]};
-        const std::array<Real,2> locLower = {DL.midX[iHeadSide], DL.midY[iHeadSide]};
-
-        //compute shear stress force (x,y) components
-        std::array<Real,2> shearFront = agent->getShear( locFront );
-        std::array<Real,2> shearUpper = agent->getShear( locLower );
-        std::array<Real,2> shearLower = agent->getShear( locUpper );
-        S[17] = shearFront[0] * Tperiod / length;
-        S[18] = shearFront[1] * Tperiod / length;
-        S[19] = 0.0;
-        S[20] = shearLower[0] * Tperiod / length;
-        S[21] = shearLower[1] * Tperiod / length;
-        S[22] = 0.0;
-        S[23] = shearUpper[0] * Tperiod / length;
-        S[24] = shearUpper[1] * Tperiod / length;
-        S[25] = 0.0;
-
-        const double X = agents[agentID]->center[0];
-        const double Y = agents[agentID]->center[1];
-        const double Z = 0;
     #endif
 
     std::vector<double> x;
@@ -317,18 +219,18 @@ std::vector<double> getState(std::vector<StefanFish *> & agents, const Simulatio
     {
         if (i == agentID) continue;
         #if modelDIM == 2
-            const double xfish = agents[i]->center[0];
-            const double yfish = agents[i]->center[1];
-            const double zfish = 0;
+            const double xfish = agents[i]->center[0]-agents[agentID]->center[0];
+            const double yfish = agents[i]->center[1]-agents[agentID]->center[1];
+            const double zfish = 0.0;
         #else
-            const double xfish = agents[i]->absPos[0];
-            const double yfish = agents[i]->absPos[1];
-            const double zfish = agents[i]->absPos[2];
+            const double xfish = agents[i]->position[0]-agents[agentID]->position[0];
+            const double yfish = agents[i]->position[1]-agents[agentID]->position[1];
+            const double zfish = agents[i]->position[2]-agents[agentID]->position[2];
         #endif
-        x.push_back(xfish-X);
-        y.push_back(yfish-Y);
-        z.push_back(zfish-Z);
-        d.push_back(std::sqrt((xfish-X)*(xfish-X)+(yfish-Y)*(yfish-Y)+(zfish-Z)*(zfish-Z)));
+        x.push_back(xfish);
+        y.push_back(yfish);
+        z.push_back(zfish);
+        d.push_back(std::sqrt(xfish*xfish+yfish*yfish+zfish*zfish));
     }
     std::vector<int> indices(d.size());
     std::iota(indices.begin(), indices.end(), 0);
@@ -342,20 +244,19 @@ std::vector<double> getState(std::vector<StefanFish *> & agents, const Simulatio
     }
     if (S.size() != STATES)
     {
-       std::cout << "wrong state size " << S.size() << "->" << STATES << std::endl;
+       std::cerr << "wrong state size " << S.size() << "->" << STATES << std::endl;
        abort();
     }
     return S;
 }
 
-void takeAction(StefanFish *agent, const SimulationData & sim, const int agentID, 
-                const std::vector<double> & action, const double l_tnext)
+void takeAction(StefanFish *agent, const SimulationData & sim, const int agentID, const std::vector<double> & action, const double l_tnext)
 {
     #if modelDIM == 3
-        //eight actions defined for compatibility with 3D motion, only two used for planar motion
         auto * const cFish = dynamic_cast<CurvatureDefinedFishData*>( agent->myFish );
-        cFish->action_curvature(sim.time,l_tnext,action[0]);
-        cFish->action_period   (sim.time,l_tnext,action[1]);
+        cFish->action_curvature(sim.time,l_tnext, action[0]);
+        cFish->action_period   (sim.time,l_tnext, action[1]);
+        cFish->action_torsion  (sim.time,l_tnext,&action[2]);
     #else
         agent->act(l_tnext,action);
     #endif
@@ -388,39 +289,21 @@ void setupRL(korali::Experiment & e)
     curVariable++;
     e["Variables"][curVariable]["Name"] = "Torsion point 0";
     e["Variables"][curVariable]["Type"] = "Action";
-    e["Variables"][curVariable]["Lower Bound"] = -0.2;
-    e["Variables"][curVariable]["Upper Bound"] = +0.2;
-    e["Variables"][curVariable]["Initial Exploration Noise"] = 0.05;
+    e["Variables"][curVariable]["Lower Bound"] = -0.5;
+    e["Variables"][curVariable]["Upper Bound"] = +0.5;
+    e["Variables"][curVariable]["Initial Exploration Noise"] = 0.2;
     curVariable++;
     e["Variables"][curVariable]["Name"] = "Torsion point 1";
     e["Variables"][curVariable]["Type"] = "Action";
-    e["Variables"][curVariable]["Lower Bound"] = -0.2;
-    e["Variables"][curVariable]["Upper Bound"] = +0.2;
-    e["Variables"][curVariable]["Initial Exploration Noise"] = 0.05;
+    e["Variables"][curVariable]["Lower Bound"] = -0.5;
+    e["Variables"][curVariable]["Upper Bound"] = +0.5;
+    e["Variables"][curVariable]["Initial Exploration Noise"] = 0.2;
     curVariable++;
     e["Variables"][curVariable]["Name"] = "Torsion point 2";
     e["Variables"][curVariable]["Type"] = "Action";
-    e["Variables"][curVariable]["Lower Bound"] = -0.2;
-    e["Variables"][curVariable]["Upper Bound"] = +0.2;
-    e["Variables"][curVariable]["Initial Exploration Noise"] = 0.05;
-    curVariable++;
-    e["Variables"][curVariable]["Name"] = "Torsion point 3";
-    e["Variables"][curVariable]["Type"] = "Action";
     e["Variables"][curVariable]["Lower Bound"] = -0.5;
     e["Variables"][curVariable]["Upper Bound"] = +0.5;
-    e["Variables"][curVariable]["Initial Exploration Noise"] = 0.05;
-    curVariable++;
-    e["Variables"][curVariable]["Name"] = "Torsion point 4";
-    e["Variables"][curVariable]["Type"] = "Action";
-    e["Variables"][curVariable]["Lower Bound"] = -0.5;
-    e["Variables"][curVariable]["Upper Bound"] = +0.5;
-    e["Variables"][curVariable]["Initial Exploration Noise"] = 0.05;
-    curVariable++;
-    e["Variables"][curVariable]["Name"] = "Torsion point 5";
-    e["Variables"][curVariable]["Type"] = "Action";
-    e["Variables"][curVariable]["Lower Bound"] = -0.2;
-    e["Variables"][curVariable]["Upper Bound"] = +0.2;
-    e["Variables"][curVariable]["Initial Exploration Noise"] = 0.05;
+    e["Variables"][curVariable]["Initial Exploration Noise"] = 0.2;
 
     e["Solver"]["Experiences Between Policy Updates"] = 1;
     e["Solver"]["Learning Rate"] = 1e-4;

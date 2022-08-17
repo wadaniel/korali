927549f3e050946a1148cba281b025d30ed628f1
wadaniel <wadaniel@ethz.ch>
2022-08-17 16:39:12 +0200

    setup

---
+++ source/modules/problem/reaction/reaction.hpp
@@ -0,0 +1,62 @@
+/** \namespace problem
+* @brief Namespace declaration for modules of type: problem.
+*/
+
+/** \file
+* @brief Header file for module: Reaction.
+*/
+
+/** \dir problem/reaction
+* @brief Contains code, documentation, and scripts for module: Reaction.
+*/
+
+#pragma once
+
+#include "modules/problem/problem.hpp"
+
+namespace korali
+{
+namespace problem
+{
+;
+
+/**
+* @brief Class declaration for module: Reaction.
+*/
+class Reaction : public Problem
+{
+  private:
+  public: 
+  /**
+  * @brief Total duration of stochastic reaction simulation.
+  */
+   double _simulationLength;
+  
+ 
+  /**
+  * @brief Obtains the entire current state and configuration of the module.
+  * @param js JSON object onto which to save the serialized state of the module.
+  */
+  void getConfiguration(knlohmann::json& js) override;
+  /**
+  * @brief Sets the entire state and configuration of the module, given a JSON object.
+  * @param js JSON object from which to deserialize the state of the module.
+  */
+  void setConfiguration(knlohmann::json& js) override;
+  /**
+  * @brief Applies the module's default configuration upon its creation.
+  * @param js JSON object containing user configuration. The defaults will not override any currently defined settings.
+  */
+  void applyModuleDefaults(knlohmann::json& js) override;
+  /**
+  * @brief Applies the module's default variable configuration to each variable in the Experiment upon creation.
+  */
+  void applyVariableDefaults() override;
+  
+
+  void initialize() override;
+};
+
+} //problem
+} //korali
+;

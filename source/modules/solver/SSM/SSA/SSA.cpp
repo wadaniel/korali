ebde8c1e535073d887432d65f3a68b5e473bd6fd
wadaniel <wadaniel@ethz.ch>
2022-08-17 16:34:45 +0200

    setup

---
+++ source/modules/solver/SSM/SSA/SSA.cpp
@@ -0,0 +1,81 @@
+#include "modules/solver/SSM/SSA/SSA.hpp"
+
+namespace korali
+{
+namespace solver
+{
+namespace ssm
+{
+;
+
+void SSA::setInitialConfiguration()
+{
+    //TODO
+}
+
+void SSA::simulateTrajectory()
+{
+    //TODO
+}
+
+void SSA::finalize()
+{
+    //TODO
+}
+
+void SSA::setConfiguration(knlohmann::json& js) 
+{
+ if (isDefined(js, "Results"))  eraseValue(js, "Results");
+
+ if (isDefined(_k->_js.getJson(), "Variables"))
+ for (size_t i = 0; i < _k->_js["Variables"].size(); i++) { 
+ } 
+ SSM::setConfiguration(js);
+ _type = "SSM/SSA";
+ if(isDefined(js, "Type")) eraseValue(js, "Type");
+ if(isEmpty(js) == false) KORALI_LOG_ERROR(" + Unrecognized settings for Korali module: SSA: \n%s\n", js.dump(2).c_str());
+} 
+
+void SSA::getConfiguration(knlohmann::json& js) 
+{
+
+ js["Type"] = _type;
+ for (size_t i = 0; i <  _k->_variables.size(); i++) { 
+ } 
+ SSM::getConfiguration(js);
+} 
+
+void SSA::applyModuleDefaults(knlohmann::json& js) 
+{
+
+ std::string defaultString = "{}";
+ knlohmann::json defaultJs = knlohmann::json::parse(defaultString);
+ mergeJson(js, defaultJs); 
+ SSM::applyModuleDefaults(js);
+} 
+
+void SSA::applyVariableDefaults() 
+{
+
+ std::string defaultString = "{}";
+ knlohmann::json defaultJs = knlohmann::json::parse(defaultString);
+ if (isDefined(_k->_js.getJson(), "Variables"))
+  for (size_t i = 0; i < _k->_js["Variables"].size(); i++) 
+   mergeJson(_k->_js["Variables"][i], defaultJs); 
+ SSM::applyVariableDefaults();
+} 
+
+bool SSA::checkTermination()
+{
+ bool hasFinished = false;
+
+ hasFinished = hasFinished || SSM::checkTermination();
+ return hasFinished;
+}
+
+;
+
+} //ssm
+} //solver
+} //korali
+;
---
+++ source/modules/solver/SSM/SSM.hpp
@@ -0,0 +1,83 @@
+/** \namespace solver
+* @brief Namespace declaration for modules of type: solver.
+*/
+
+/** \file
+* @brief Header file for module: SSM.
+*/
+
+/** \dir solver/SSM
+* @brief Contains code, documentation, and scripts for module: SSM.
+*/
+
+#pragma once
+
+#include "modules/solver/solver.hpp"
+#include "modules/distribution/univariate/exponential/exponential.hpp"
+
+namespace korali
+{
+namespace solver
+{
+;
+
+/**
+* @brief Class declaration for module: SSM.
+*/
+class SSM : public Solver
+{
+  public: 
+  /**
+  * @brief Number of simulations.
+  */
+   size_t _numSimulations;
+  /**
+  * @brief Number of bins to calculate the mean trajectory at termination.
+  */
+   size_t _numBins;
+  /**
+  * @brief [Internal Use] Exponential random number generator.
+  */
+   korali::distribution::univariate::Exponential* _exponentialGenerator;
+  
+ 
+  /**
+  * @brief Determines whether the module can trigger termination of an experiment run.
+  * @return True, if it should trigger termination; false, otherwise.
+  */
+  bool checkTermination() override;
+  /**
+  * @brief Obtains the entire current state and configuration of the module.
+  * @param js JSON object onto which to save the serialized state of the module.
+  */
+  void getConfiguration(knlohmann::json& js) override;
+  /**
+  * @brief Sets the entire state and configuration of the module, given a JSON object.
+  * @param js JSON object from which to deserialize the state of the module.
+  */
+  void setConfiguration(knlohmann::json& js) override;
+  /**
+  * @brief Applies the module's default configuration upon its creation.
+  * @param js JSON object containing user configuration. The defaults will not override any currently defined settings.
+  */
+  void applyModuleDefaults(knlohmann::json& js) override;
+  /**
+  * @brief Applies the module's default variable configuration to each variable in the Experiment upon creation.
+  */
+  void applyVariableDefaults() override;
+  
+
+  /**
+   * @brief Simulates a trajectory for all reactants based on provided reactions.
+   */
+  virtual void simulateTrajectory() = 0;
+
+  void runGeneration() override;
+  void printGenerationBefore() override;
+  void printGenerationAfter() override;
+
+};
+
+} //solver
+} //korali
+;

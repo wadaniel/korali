ff582a8e5fc1ba36b7b502f1b55cf9122426010f
wadaniel <wadaniel@ethz.ch>
2022-08-23 16:18:43 +0200

    fix reaction example

--- examples/reaction/run-laczlacy-tauLeaping.py
+++ examples/reaction/run-laczlacy-tauLeaping.py
@@ -69,13 +69,13 @@ e["Problem"]["Reactions"][16]["Rate"] = 6.42e-5
 e["Problem"]["Reactions"][17]["Equation"] = "RbsLacY->dgrRbsLacY"
 e["Problem"]["Reactions"][17]["Rate"] = 0.3
 
-e["Problem"]["Reactions"][18]["Equation"] = "RbsLacZ->dgrRbcLacZ"
+e["Problem"]["Reactions"][18]["Equation"] = "RbsLacZ->dgrRbsLacZ"
 e["Problem"]["Reactions"][18]["Rate"] = 0.3
 
 e["Problem"]["Reactions"][19]["Equation"] = "LacZ+lactose->LacZLactose"
 e["Problem"]["Reactions"][19]["Rate"] = 9.52e-5
 
-e["Problem"]["Reactions"][20]["Equation"] = "LacZlactose->product+LacZ"
+e["Problem"]["Reactions"][20]["Equation"] = "LacZLactose->product+LacZ"
 e["Problem"]["Reactions"][20]["Rate"] = 431.
 
 e["Problem"]["Reactions"][21]["Equation"] = "LacY->lactose+LacY"
@@ -97,56 +97,59 @@ e["Variables"][3]["Initial Reactant Number"] = 0
 e["Variables"][4]["Name"] = "TrLacZ1"
 e["Variables"][4]["Initial Reactant Number"] = 0
 
-e["Variables"][5]["Name"] = "RbsLacY"
+e["Variables"][5]["Name"] = "TrLacZ2"
 e["Variables"][5]["Initial Reactant Number"] = 0
 
-e["Variables"][6]["Name"] = "RbsLacZ"
+e["Variables"][6]["Name"] = "RbsLacY"
 e["Variables"][6]["Initial Reactant Number"] = 0
 
-e["Variables"][7]["Name"] = "TrLacY1"
+e["Variables"][7]["Name"] = "RbsLacZ"
 e["Variables"][7]["Initial Reactant Number"] = 0
 
-e["Variables"][8]["Name"] = "TrLacY2"
+e["Variables"][8]["Name"] = "TrLacY1"
 e["Variables"][8]["Initial Reactant Number"] = 0
 
-e["Variables"][9]["Name"] = "RbsRibosomeLacY"
+e["Variables"][9]["Name"] = "TrLacY2"
 e["Variables"][9]["Initial Reactant Number"] = 0
 
-e["Variables"][10]["Name"] = "RbsRibosomeLacZ"
+e["Variables"][10]["Name"] = "RbsRibosomeLacY"
 e["Variables"][10]["Initial Reactant Number"] = 0
 
-e["Variables"][11]["Name"] = "TrRbsLacY"
+e["Variables"][11]["Name"] = "RbsRibosomeLacZ"
 e["Variables"][11]["Initial Reactant Number"] = 0
 
-e["Variables"][12]["Name"] = "TrRbsLacZ"
+e["Variables"][12]["Name"] = "TrRbsLacY"
 e["Variables"][12]["Initial Reactant Number"] = 0
 
-e["Variables"][13]["Name"] = "LacY"
+e["Variables"][13]["Name"] = "TrRbsLacZ"
 e["Variables"][13]["Initial Reactant Number"] = 0
 
-e["Variables"][14]["Name"] = "LacZ"
+e["Variables"][14]["Name"] = "LacY"
 e["Variables"][14]["Initial Reactant Number"] = 0
 
-e["Variables"][15]["Name"] = "dgrLacY"
+e["Variables"][15]["Name"] = "LacZ"
 e["Variables"][15]["Initial Reactant Number"] = 0
 
-e["Variables"][16]["Name"] = "dgrLacZ"
+e["Variables"][16]["Name"] = "dgrLacY"
 e["Variables"][16]["Initial Reactant Number"] = 0
 
-e["Variables"][17]["Name"] = "dgrRbsLacY"
+e["Variables"][17]["Name"] = "dgrLacZ"
 e["Variables"][17]["Initial Reactant Number"] = 0
 
-e["Variables"][18]["Name"] = "dgrRbsLacZ"
+e["Variables"][18]["Name"] = "dgrRbsLacY"
 e["Variables"][18]["Initial Reactant Number"] = 0
 
-e["Variables"][19]["Name"] = "lactose"
+e["Variables"][19]["Name"] = "dgrRbsLacZ"
 e["Variables"][19]["Initial Reactant Number"] = 0
 
-e["Variables"][20]["Name"] = "LacZLactose"
+e["Variables"][20]["Name"] = "lactose"
 e["Variables"][20]["Initial Reactant Number"] = 0
 
-e["Variables"][21]["Name"] = "product"
+e["Variables"][21]["Name"] = "LacZLactose"
 e["Variables"][21]["Initial Reactant Number"] = 0
+
+e["Variables"][22]["Name"] = "product"
+e["Variables"][22]["Initial Reactant Number"] = 0
 
 # Configuring TauLeaping parameters
 e["Solver"]["Type"] = "SSM/TauLeaping"

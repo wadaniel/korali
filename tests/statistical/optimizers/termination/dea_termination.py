fa35648646907ba2b6c814a72446269a3d61ddb5
wadaniel <wadaniel@ethz.ch>
2022-09-07 18:55:20 -0400

    fix tests

--- tests/statistical/optimizers/termination/dea_termination.py
+++ tests/statistical/optimizers/termination/dea_termination.py
@@ -28,8 +28,9 @@ def run_dea_with_termination_criterion(criterion, value):
 
   e["Solver"]["Type"] = "Optimizer/DEA"
   e["Solver"]["Population Size"] = 10
+  print(criterion)
   e["Solver"]["Termination Criteria"][criterion] = value
-  e["File Output"]["Enabled"] = False
+  e["File Output"]["Enabled"] = True
   e["Random Seed"] = 1337
 
   k = korali.Engine()

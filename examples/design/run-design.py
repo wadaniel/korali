0c1b7f6ed38d0cd4570ac2ddb19809fa30211cbb
webepasc <webepasc@student.ethz.ch>
2022-09-22 18:53:37 +0200

    added argument to easily enable 2D validation

--- examples/design/run-design.py
+++ examples/design/run-design.py
@@ -10,49 +10,50 @@ e = korali.Experiment()
 e["Problem"]["Type"] = "Design"
 e["Problem"]["Model"] = model
 
+e["Solver"]["Type"] = "Designer"
+numMeasurements = 1
+e["Solver"]["Sigma"] = 1e-2
+
 e["Distributions"][0]["Name"] = "Uniform"
 e["Distributions"][0]["Type"] = "Univariate/Uniform"
 e["Distributions"][0]["Minimum"] = 0.0
 e["Distributions"][0]["Maximum"] = 1.0
 
-## TODO: Also add configuration of distribution for measurement variable
 indx = 0
-
 e["Variables"][indx]["Name"] = "d1"
 e["Variables"][indx]["Type"] = "Design"
 e["Variables"][indx]["Number Of Samples"] = 101
 e["Variables"][indx]["Lower Bound"] = 0.0
 e["Variables"][indx]["Upper Bound"] = 1.0
 e["Variables"][indx]["Distribution"] = "Grid"
-
-# indx += 1
-# e["Variables"][indx]["Name"] = "d2"
-# e["Variables"][indx]["Type"] = "Design"
-# e["Variables"][indx]["Number Of Samples"] = 101
-# e["Variables"][indx]["Lower Bound"] = 0.0
-# e["Variables"][indx]["Upper Bound"] = 1.0
-# e["Variables"][indx]["Distribution"] = "Grid"
-
 indx += 1
+
+if numMeasurements == 2:
+	e["Variables"][indx]["Name"] = "d2"
+	e["Variables"][indx]["Type"] = "Design"
+	e["Variables"][indx]["Number Of Samples"] = 101
+	e["Variables"][indx]["Lower Bound"] = 0.0
+	e["Variables"][indx]["Upper Bound"] = 1.0
+	e["Variables"][indx]["Distribution"] = "Grid"
+	indx += 1
+
 e["Variables"][indx]["Name"] = "theta"
 e["Variables"][indx]["Type"] = "Parameter"
 e["Variables"][indx]["Lower Bound"] = 0.0
 e["Variables"][indx]["Upper Bound"] = 1.0
-e["Variables"][indx]["Number Of Samples"] = 1e3
+e["Variables"][indx]["Number Of Samples"] = 1e2
 e["Variables"][indx]["Distribution"] = "Uniform"
-
 indx += 1
+
 e["Variables"][indx]["Name"] = "y1"
 e["Variables"][indx]["Type"] = "Measurement"
-e["Variables"][indx]["Number Of Samples"] = 1e3
+e["Variables"][indx]["Number Of Samples"] = 1e2
+indx += 1
 
-# indx += 1
-# e["Variables"][indx]["Name"] = "y2"
-# e["Variables"][indx]["Type"] = "Measurement"
-# e["Variables"][indx]["Number Of Samples"] = 1e2
-
-e["Solver"]["Type"] = "Designer"
-e["Solver"]["Sigma"] = 1e-2
+if numMeasurements == 2:
+	e["Variables"][indx]["Name"] = "y2"
+	e["Variables"][indx]["Type"] = "Measurement"
+	e["Variables"][indx]["Number Of Samples"] = 1e2
 
 # Starting Korali's Engine and running experiment
 k = korali.Engine()

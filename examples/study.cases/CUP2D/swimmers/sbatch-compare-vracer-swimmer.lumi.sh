87312a348d269e3acfd1f5e923fe69ab494a9f36
Pascal Weber <webepasc@student.ethz.ch>
2022-10-20 17:26:19 +0300

    current setup for lumi-runs

--- examples/study.cases/CUP2D/swimmers/sbatch-compare-vracer-swimmer.lumi.sh
+++ examples/study.cases/CUP2D/swimmers/sbatch-compare-vracer-swimmer.lumi.sh
@@ -1,23 +1,24 @@
 #! /usr/bin/env bash
 
 if [ $# -lt 2 ] ; then
-	echo "Usage: ./sbatch-run-vracer-swimmer.sh RUNNAME TASK"
+	echo "Usage: ./sbatch-compare-vracer-swimmer.sh RUNNAME TASK"
 	exit 1
 fi
 
 RUNNAME=$1
 TASK=$2
 
+# path to previous run
+RESULTSPATH=/scratch/project_465000158/paweber/korali/100swimmer-old
+
 # number of agents
 NAGENTS=100
 
 # number of workers
 NWORKER=64
-# NWORKER=1
 
 # number of nodes per worker
 NRANKS=1
-# NRANKS=9
 
 # number of cores per worker
 NUMCORES=128
@@ -28,7 +29,7 @@ NNODES=$(( $NWORKER * $NRANKS ))
 # setup run directory and copy necessary files
 RUNPATH="${SCRATCH}/korali/${RUNNAME}"
 mkdir -p ${RUNPATH}
-cp run-vracer-swimmer ${RUNPATH}
+cp compare-vracer-swimmer ${RUNPATH}
 cd ${RUNPATH}
 
 cat <<EOF >daint_sbatch
@@ -44,10 +45,10 @@ cat <<EOF >daint_sbatch
 #SBATCH --partition=standard
 #SBATCH --nodes=1 --ntasks-per-node=1 --cpus-per-task=8
 
-srun  --het-group=0,1 ./run-vracer-swimmer -task $TASK -nAgents $NAGENTS -nRanks $(( $NRANKS * $NUMCORES ))
+srun  --het-group=0,1 ./compare-vracer-swimmer -resultsPath $RESULTSPATH -task $TASK -nAgents $NAGENTS -nRanks $(( $NRANKS * $NUMCORES ))
 EOF
 
-echo "Starting task ${TASK} with ${NWORKER} simulations each using ${NRANKS} nodes with ${NUMCORES} cores"
+echo "Starting task ${TASK} with ${NWORKER} simulations each using ${NRANKS} ranks with ${NUMCORES} cores"
 echo "----------------------------"
 
 chmod 755 daint_sbatch
--- examples/study.cases/CUP2D/swimmers/sbatch-run-vracer-swimmer.daint.sh
+++ examples/study.cases/CUP2D/swimmers/sbatch-run-vracer-swimmer.daint.sh
@@ -9,14 +9,14 @@ RUNNAME=$1
 TASK=$2
 
 # number of agents
-NAGENTS=4
+NAGENTS=100
 
 # number of workers
 NWORKER=64
 # NWORKER=1
 
 # number of nodes per worker
-NRANKS=2
+NRANKS=1
 # NRANKS=9
 
 # number of cores per worker

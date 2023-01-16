48c34b5c13851538ed7adca0adedd1401e90cfe4
wadaniel <wadaniel@ethz.ch>
2023-01-16 16:29:54 +0100

    mino

--- study.cases/openAIGym/_jobs/submit_batch_irl.sh
+++ study.cases/openAIGym/_jobs/submit_batch_irl.sh
@@ -1,17 +1,18 @@
-run=300
+run=400
 
-export ENV="Swimmer-v4"
-export POL="Quadratic"
-export EXP=3000000
+export ENV="HalfCheetah-v4"
+export POL="Linear"
+export EXP=5000000
 
-for D in 1 4 16 64
+for D in 4 16 64
 do 
-    for B in 1 4 16 64
+    for B in 4 16 64
     do 
         run=$(($run+1))
         export RUN=$run
         export DBS=$D
         export BBS=$B
         bsub < bsub-vracer-irl.lsf
+        exit
     done
 done

3c80b232ba405b9baff79ce534ccbc8897903cee
wadaniel <wadaniel@ethz.ch>
2023-01-18 23:13:27 +0100

    Setup

--- study.cases/openAIGym/_jobs/submit_batch_irl.sh
+++ study.cases/openAIGym/_jobs/submit_batch_irl.sh
@@ -1,19 +1,18 @@
-run=400
+run=5000
 
 export ENV="HalfCheetah-v4"
 export POL="Linear"
-export EXP=5000000
+export EXP=2000000
 export DAT=1000
 
-for D in 4 16 64
+for D in 1 4 16 64
 do 
-    for B in 4 16 64
+    for B in 1 4 16 64
     do 
         run=$(($run+1))
         export RUN=$run
         export DBS=$D
         export BBS=$B
         bsub < bsub-vracer-irl.lsf
-        exit
     done
 done
--- study.cases/openAIGym/settings_irl.sh
+++ study.cases/openAIGym/settings_irl.sh
@@ -1,6 +1,6 @@
 # Defaults for Options
 ENV=${ENV:-HalfCheetah-v4}  # environment
-EBRU=${EBRU:-100}       # experiences between reward updates
+EBRU=${EBRU:-5000}       # experiences between reward updates
 DBS=${DBS:-10}          # demonstration batch size
 BBS=${BBS:-50}          # background batch size
 BSS=${BSS:-1000}        # background sample size

e8c2e486ea4093175ba0254f3d67ffd7c4478297
wadaniel <wadaniel@ethz.ch>
2023-02-24 19:46:38 +0100

    job

--- study.cases/openAIGym/_jobs/submit_batch_irl.sh
+++ study.cases/openAIGym/_jobs/submit_batch_irl.sh
@@ -1,4 +1,4 @@
-run=3000
+run=0
 
 export ENV="HalfCheetah-v4"
 #export ENV="Swimmer-v4"
@@ -9,19 +9,22 @@ export POL="Linear"
 export EXP=5000000
 export DAT=100
 
-#for EU in 5000
 for EU in 1000 5000
 do
-    for D in 1 4 16 64
+    for D in 1 4 16
     do 
-        for B in 1 4 16 64
+        for B in 4 16 64
         do 
-            run=$(($run+1))
-            export RUN=$run
-            export DBS=$D
-            export BBS=$B
-            export EBRU=$EU
-            bsub < bsub-vracer-irl.lsf
+            for R in 8 32 64
+            do
+                run=$(($run+1))
+                export RUN=$run
+                export DBS=$D
+                export BBS=$B
+                export EBRU=$EU
+                export RNN=$R
+                bsub < bsub-vracer-irl.lsf
+            done
         done
     done
 done

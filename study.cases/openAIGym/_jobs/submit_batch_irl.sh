7e344a1cc654b2f022ba869e6c9061e8d0837acf
wadaniel <wadaniel@ethz.ch>
2023-03-20 18:34:38 +0100

    openai march

--- study.cases/openAIGym/_jobs/submit_batch_irl.sh
+++ study.cases/openAIGym/_jobs/submit_batch_irl.sh
@@ -1,21 +1,21 @@
 run=0
 
-export ENV="HalfCheetah-v4"
+#export ENV="HalfCheetah-v4"
 #export ENV="Swimmer-v4"
-#export ENV="Hopper-v4"
+export ENV="Hopper-v4"
 #export ENV="Walker2d-v4"
 #export POL="Quadratic"
 export POL="Linear"
 export EXP=5000000
 export DAT=100
 
-for EU in 1000 5000
+for EU in 5000
 do
-    for D in 1 4 16
+    for D in 4 16
     do 
         for B in 4 16 64
         do 
-            for R in 8 32 64
+            for R in 8 32
             do
                 run=$(($run+1))
                 export RUN=$run
@@ -24,6 +24,7 @@ do
                 export EBRU=$EU
                 export RNN=$R
                 bsub < bsub-vracer-irl.lsf
+                exit
             done
         done
     done
